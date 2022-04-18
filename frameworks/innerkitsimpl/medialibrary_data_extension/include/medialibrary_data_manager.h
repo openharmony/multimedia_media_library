ae91bcfabdc40ab8a45362bbf8f2a06383050c16
Lihong Kou <koulihong@huawei.com>
2022-04-18 14:34:21 +0800

    introduce data manager for extension
    Signed-off-by: Qilong Zhang <zhangqilong3@huawei.com>
    Change-Id: I3a8b919e4598872f55104ae4da3b5863ba93aadc

--- frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager.h
+++ frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef FRAMEWORKS_INNERKITSIMPL_MEDIALIBRARY_DATA_ABILITY_INCLUDE_MEDIALIBRARY_DATA_ABILITY_H_
-#define FRAMEWORKS_INNERKITSIMPL_MEDIALIBRARY_DATA_ABILITY_INCLUDE_MEDIALIBRARY_DATA_ABILITY_H_
+#ifndef OHOS_MEDIALIBRARY_DATA_MANAGER_H
+#define OHOS_MEDIALIBRARY_DATA_MANAGER_H
 
 #include <string>
 
@@ -23,30 +23,31 @@
 #include "abs_rdb_predicates.h"
 #include "data_ability_predicates.h"
 #include "device_manager.h"
-#include "distributed_kv_data_manager.h"
 #include "device_manager_callback.h"
-#include "hilog/log.h"
 #include "medialibrary_album_operations.h"
 #include "medialibrary_smartalbum_map_operations.h"
 #include "medialibrary_smartalbum_operations.h"
 #include "media_data_ability_const.h"
-#include "medialibrary_data_ability_utils.h"
+#include "medialibrary_data_manager_utils.h"
 #include "medialibrary_device.h"
-#include "medialibrary_thumbnail.h"
 #include "medialibrary_device_info.h"
 #include "medialibrary_file_operations.h"
 #include "medialibrary_kvstore_operations.h"
 #include "rdb_errno.h"
 #include "rdb_helper.h"
 #include "rdb_open_callback.h"
+#include "foundation/aafwk/standard/frameworks/kits/appkit/native/ability_runtime/context/context.h"
 #include "rdb_store.h"
 #include "rdb_store_config.h"
 #include "rdb_types.h"
 #include "result_set.h"
-#include "timer.h"
 #include "uri.h"
 #include "values_bucket.h"
 #include "want.h"
+#include "hilog/log.h"
+#include "medialibrary_thumbnail.h"
+#include "distributed_kv_data_manager.h"
+#include "timer.h"
 
 namespace OHOS {
 namespace Media {
@@ -66,29 +67,29 @@ namespace Media {
     class MediaLibraryInitCallback;
     class MediaLibraryDeviceStateCallback;
     class MediaLibraryRdbStoreObserver;
-    class MediaLibraryDataAbility : public AppExecFwk::Ability {
+    class MediaLibraryDataManager {
     public:
-        EXPORT MediaLibraryDataAbility();
-        EXPORT ~MediaLibraryDataAbility();
+        EXPORT MediaLibraryDataManager();
+        EXPORT ~MediaLibraryDataManager();
+        static std::shared_ptr<MediaLibraryDataManager> GetInstance();
 
         EXPORT int32_t InitMediaLibraryRdbStore();
         EXPORT void InitialiseKvStore();
-        EXPORT int32_t Insert(const Uri &uri, const NativeRdb::ValuesBucket &value) override;
-        EXPORT int32_t Delete(const Uri &uri, const NativeRdb::DataAbilityPredicates &predicates) override;
-        EXPORT int32_t BatchInsert(const Uri &uri, const std::vector<NativeRdb::ValuesBucket> &values) override;
+        EXPORT int32_t Insert(const Uri &uri, const NativeRdb::ValuesBucket &value);
+        EXPORT int32_t Delete(const Uri &uri, const NativeRdb::DataAbilityPredicates &predicates);
+        EXPORT int32_t BatchInsert(const Uri &uri, const std::vector<NativeRdb::ValuesBucket> &values);
         EXPORT int32_t Update(const Uri &uri, const NativeRdb::ValuesBucket &value,
-                       const NativeRdb::DataAbilityPredicates &predicates) override;
+                       const NativeRdb::DataAbilityPredicates &predicates);
         EXPORT std::shared_ptr<NativeRdb::AbsSharedResultSet> Query(const Uri &uri,
             const std::vector<std::string> &columns,
-            const NativeRdb::DataAbilityPredicates &predicates) override;
-        EXPORT int32_t OpenFile(const Uri &uri, const std::string &mode) override;
-        EXPORT std::string GetType(const Uri &uri) override;
+            const NativeRdb::DataAbilityPredicates &predicates);
+        EXPORT int32_t OpenFile(const Uri &uri, const std::string &mode);
+        EXPORT std::string GetType(const Uri &uri);
 
         std::shared_ptr<NativeRdb::RdbStore> rdbStore_;
 
-    protected:
-        void OnStart(const AAFwk::Want &want) override;
-        void OnStop() override;
+        void InitMediaLibraryMgr(const std::shared_ptr<OHOS::AbilityRuntime::Context> &context);
+        void ClearMediaLibraryMgr();
 
     private:
         static constexpr const char DEVICE_BUNDLENAME[] = "com.ohos.medialibrary.MediaLibraryDataA";
@@ -107,26 +108,6 @@ namespace Media {
         std::string GetClientBundle(int uid);
 
         int32_t PreCheckInsert(const std::string &uri, const NativeRdb::ValuesBucket &value);
-        bool ParseThumbnailInfo(std::string &uriString, Size &size);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> GenThumbnail(std::shared_ptr<MediaLibraryThumbnail> thumbnail,
-            std::string &rowId, Size &size, std::string &networkId);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryBySmartTableType(TableType tabletype,
-            std::string strQueryCondition, NativeRdb::DataAbilityPredicates predicates,
-            std::vector<std::string> columns);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryByViewType(TableType tabletype,
-            std::string strQueryCondition, NativeRdb::DataAbilityPredicates predicates,
-            std::vector<string> columns);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryDeviceInfo(std::string strQueryCondition,
-            NativeRdb::DataAbilityPredicates predicates, std::vector<string> columns);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryAlbum(std::string strQueryCondition,
-            NativeRdb::DataAbilityPredicates predicates, std::vector<std::string> columns, std::string networkId);
-        std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryFile(std::string strQueryCondition,
-            NativeRdb::DataAbilityPredicates predicates, std::vector<std::string> columns, std::string networkId);
-        std::string ObtionCondition(std::string &strQueryCondition, const std::vector<std::string> &whereArgs);
-        void SplitKeyValue(const std::string& keyValue, std::string &key, std::string &value);
-        void SplitKeys(const std::string& query, std::vector<std::string>& keys);
-        void DealWithUriString(std::string &uriString, TableType &tabletype,
-            std::string &strQueryCondition, std::string::size_type &pos, std::string &strRow);
 
         static const std::string PERMISSION_NAME_READ_MEDIA;
         static const std::string PERMISSION_NAME_WRITE_MEDIA;
@@ -138,8 +119,11 @@ namespace Media {
         std::shared_ptr<MediaLibraryInitCallback> deviceInitCallback_;
         std::shared_ptr<MediaLibraryRdbStoreObserver> rdbStoreObs_;
         bool isRdbStoreInitialized;
+        std::shared_ptr<OHOS::AbilityRuntime::Context> context_ = nullptr;
         std::string bundleName_;
         OHOS::sptr<AppExecFwk::IBundleMgr> bundleMgr_;
+        static std::mutex mutex_;
+        static std::shared_ptr<MediaLibraryDataManager> instance_;
 };
 
 class MediaLibraryDataCallBack : public NativeRdb::RdbOpenCallback {
@@ -195,5 +179,4 @@ private:
 };
 } // namespace Media
 } // namespace OHOS
-
-#endif  // FRAMEWORKS_INNERKITSIMPL_MEDIALIBRARY_DATA_ABILITY_INCLUDE_MEDIALIBRARY_DATA_ABILITY_H_
+#endif // OHOS_MEDIALIBRARY_DATA_ABILITY_H
