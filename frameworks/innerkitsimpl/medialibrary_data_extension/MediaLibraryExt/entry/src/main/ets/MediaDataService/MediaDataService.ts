a5b635f6602bd7b561805cb1076a2464aeadd5d3
Lihong Kou <koulihong@huawei.com>
2022-04-22 15:10:25 +0800

    make medialib hap
    Signed-off-by: Qilong Zhang <zhangqilong3@huawei.com>
    Change-Id: Ia91261bab2b28045f1385783ca8724705a1f7dee

--- frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MediaDataService/MediaDataService.ts
+++ frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MediaDataService/MediaDataService.ts
@@ -37,15 +37,15 @@ class StubTest extends rpc.RemoteObject{
 
     //创建相关的实现
     onRemoteRequest(code, data, reply, option) {
-        console.log("[ttt] [DataShareTest] onRemoteRequest 1");
+        console.log("[ttt] [MediaDataTest] onRemoteRequest 1");
         if (code === 1) {
-            console.log("[ttt] [DataShareTest] code 1 begin");
+            console.log("[ttt] [MediaDataTest] code 1 begin");
             let op1 = data.readInt();
             let op2 = data.readInt();
-            console.log("[ttt] [DataShareTest] op1 = " + op1 + ", op2 = " + op2);
+            console.log("[ttt] [MediaDataTest] op1 = " + op1 + ", op2 = " + op2);
             reply.writeInt(op1 + op2);
         } else {
-            console.log("[ttt] [DataShareTest] onRemoteRequest code:" + code);
+            console.log("[ttt] [MediaDataTest] onRemoteRequest code:" + code);
         }
         return true;
     }
@@ -53,24 +53,24 @@ class StubTest extends rpc.RemoteObject{
 
 class MediaDataService extends ServiceExtension{
     onCreate(want) {
-        console.log('[ttt] [DataShareTest] ServiceExtAbility onCreate, want:' + want.abilityName);
+        console.log('[ttt] [MediaDataTest] ServiceExtAbility onCreate, want:' + want.abilityName);
     }
 
     onRequest(want, startId) {
-        console.log('[ttt] [DataShareTest] ServiceExtAbility onRequest, want:' + want.abilityName + ', startId:' + startId);
+        console.log('[ttt] [MediaDataTest] ServiceExtAbility onRequest, want:' + want.abilityName + ', startId:' + startId);
     }
 
     onConnect(want) {
-        console.log('[ttt] [DataShareTest] ServiceExtAbility onConnect , want:' + want.abilityName);
+        console.log('[ttt] [MediaDataTest] ServiceExtAbility onConnect , want:' + want.abilityName);
        // return new StubTest("test");
       return mediaLibrary.getMediaLibrary().getMediaRemoteStub(this.context);
     }
 
     onDisconnect(want) {
-        console.log('[ttt] [DataShareTest] ServiceExtAbility onDisconnect , want:' + want.abilityName);
+        console.log('[ttt] [MediaDataTest] ServiceExtAbility onDisconnect , want:' + want.abilityName);
     }
 
-    onDestroy() {console.log('[ttt] [DataShareTest] ServiceExtAbility onDestroy');}
+    onDestroy() {console.log('[ttt] [MediaDataTest] ServiceExtAbility onDestroy');}
 }
 
 export default MediaDataService
