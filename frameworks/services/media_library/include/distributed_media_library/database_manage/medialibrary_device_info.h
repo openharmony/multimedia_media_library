f2a04b48ff396fd6678aaee4d6c466ad1940cd4b
xianghengliang <xianghengliang1@huawei.com>
2022-04-28 23:02:37 +0800

    register media library version change from int to string
    Signed-off-by: xianghengliang <xianghengliang1@huawei.com>

--- frameworks/services/media_library/include/distributed_media_library/database_manage/medialibrary_device_info.h
+++ frameworks/services/media_library/include/distributed_media_library/database_manage/medialibrary_device_info.h
@@ -24,7 +24,7 @@ typedef struct MediaLibraryDeviceInfo {
     std::string deviceUdid;
     std::string selfId;
     uint16_t deviceTypeId;
-    int32_t versionId;
+    std::string versionId;
 } MediaLibraryDeviceInfo;
 } // namespace Media
 } // namespace OHOS
