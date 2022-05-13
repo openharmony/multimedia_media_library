75431fee1b5ed6b5a62357e98ec4cae6066eeb1f
Qilong Zhang <zhangqilong3@huawei.com>
2022-05-13 10:56:33 +0800

    MediaScanner:rebuidl
    1.Add static subscriber boardcast
    2.remove IPC module
    3.remove rdb helper
    
    Signed-off-by: Qilong Zhang <zhangqilong3@huawei.com>
    Change-Id: Ic3fa7b95a6181198feb39acebaf139594492974a

---
+++ frameworks/services/media_scanner/include/scanner/media_scan_executor.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MEDIA_SCAN_EXECUTOR_H
+#define MEDIA_SCAN_EXECUTOR_H
+
+#include <string>
+#include <iostream>
+#include <queue>
+
+namespace OHOS {
+namespace Media {
+using namespace std;
+
+class ScanRequest {
+public:
+    ScanRequest(string path) : requestId_(0), path_(path), isDir_(false) {}
+    ScanRequest() : ScanRequest("") {}
+    ~ScanRequest() = default;
+
+    int32_t GetRequestId() const
+    {
+        return requestId_;
+    };
+
+    void SetRequestId(int32_t requestId)
+    {
+        requestId_ = requestId;
+    }
+
+    const string &GetPath() const
+    {
+        return path_;
+    }
+
+    void SetIsDirectory(bool isDir)
+    {
+        isDir_ = isDir;
+    }
+
+    bool GetIsDirectory() const
+    {
+        return isDir_;
+    }
+
+private:
+    int32_t requestId_;
+    string path_;
+    bool isDir_;
+};
+
+class MediaScanExecutor {
+typedef void (*callback_func)(ScanRequest);
+public:
+    MediaScanExecutor() = default;
+    ~MediaScanExecutor() = default;
+
+    void ExecuteScan(unique_ptr<ScanRequest> request);
+    void SetCallbackFunction(callback_func cb_function);
+
+private:
+    const size_t MAX_THREAD = 1;
+    size_t activeThread_ = 0;
+    std::queue<unique_ptr<ScanRequest>> scanRequestQueue_;
+    callback_func cb_function_ = nullptr;
+
+    void HandleScanExecution();
+    void PrepareScanExecution();
+};
+} // namespace Media
+} // namespace OHOS
+
+#endif /* MEDIA_SCAN_EXECUTOR_H */
