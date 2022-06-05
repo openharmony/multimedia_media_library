5b48b5086ae66c804793548a7eec0a6736ef0fea
Michael <xianghengliang1@huawei.com>
2022-06-05 16:02:59 +0800

    code check fix and close kv resource after using
    Signed-off-by: Michael <xianghengliang1@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/distributed_media_library/devices_info_interact/devices_info_interact.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/distributed_media_library/devices_info_interact/devices_info_interact.cpp
@@ -30,6 +30,12 @@ DevicesInfoInteract::DevicesInfoInteract() : bundleName_(BUNDLE_NAME)
 
 DevicesInfoInteract::~DevicesInfoInteract()
 {
+    DistributedKv::DistributedKvDataManager kvManager;
+    DistributedKv::AppId appId = { BUNDLE_NAME };
+    if (kvStorePtr_ != nullptr) {
+        kvManager.CloseKvStore(appId, kvStorePtr_);
+        kvStorePtr_ = nullptr;
+    }
     MEDIA_DEBUG_LOG("DevicesInfoInteract::deconstructor");
 }
 
