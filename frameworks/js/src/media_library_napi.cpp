4f3761b1970f129ff7bc1b4aa8215bcd5e395d1e
Lihong Kou <koulihong@huawei.com>
2022-06-10 12:06:40 +0800

    clear media service died code
    Signed-off-by: Lihong Kou <koulihong@huawei.com>

--- frameworks/js/src/media_library_napi.cpp
+++ frameworks/js/src/media_library_napi.cpp
@@ -97,7 +97,6 @@ napi_value MediaLibraryNapi::Init(napi_env env, napi_value exports)
         DECLARE_NAPI_FUNCTION("getAllPeers", JSGetAllPeers),
         DECLARE_NAPI_FUNCTION("storeMediaAsset", JSStoreMediaAsset),
         DECLARE_NAPI_FUNCTION("startImagePreview", JSStartImagePreview),
-        DECLARE_NAPI_FUNCTION("getMediaRemoteStub", JSGetMediaRemoteStub)
     };
     napi_property_descriptor static_prop[] = {
         DECLARE_NAPI_STATIC_FUNCTION("getMediaLibrary", GetMediaLibraryNewInstance),
@@ -3579,37 +3578,6 @@ napi_value MediaLibraryNapi::JSStartImagePreview(napi_env env, napi_callback_inf
         }
     }
     return result;
-}
-napi_value MediaLibraryNapi::JSGetMediaRemoteStub(napi_env env, napi_callback_info info)
-{
-    napi_value remoteStub = nullptr;
-    /*
-    size_t argc = ARGS_ONE;
-    napi_value argv[ARGS_ONE] = {0};
-    napi_value thisVar = nullptr;
-    NAPI_CALL(env, napi_get_cb_info(env, info, &argc, argv, &thisVar, nullptr));
-    napi_status status = OHOS::AbilityRuntime::IsStageContext(env, argv[0], isStageMode_);
-
-    if (status != napi_ok){
-
-    } else {
-        if (isStageMode_) {
-            auto context = OHOS::AbilityRuntime::GetStageModeContext(env, argv[0]);
-            if (context == nullptr) {
-                NAPI_ERR_LOG("Failed to get native context instance");
-                return nullptr;
-            }
-            sptr<MediaDataStubImpl> remoteObject = new (std::nothrow) MediaDataStubImpl(env, context);
-            if (remoteObject == nullptr) {
-                return nullptr;
-	    }
-	    remoteObject->InitMediaLibraryRdbStore();
-
-            remoteStub = NAPI_ohos_rpc_CreateJsRemoteObject(env, remoteObject);
-	}
-    }
-    */
-    return remoteStub;
 }
 } // namespace Media
 } // namespace OHOS
