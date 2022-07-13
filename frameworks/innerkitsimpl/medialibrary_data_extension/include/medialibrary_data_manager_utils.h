b065bbd2a907561bc86cdaecd103ad6104dec810
Qiheng Lin <linqiheng@huawei.com>
2022-07-13 14:15:02 +0800

    medialibrary data_manager unistore
    Signed-off-by: Qiheng Lin <linqiheng@huawei.com>
    Change-Id: I47a788ea8f2de24dd2ae3997fa1d1cea85daa11d

--- frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager_utils.h
+++ frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager_utils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,24 +17,10 @@
 #define MEDIALIBRARY_DATA_ABILITY_UTILS
 
 #include <string>
-#include <sys/stat.h>
-#include <unordered_map>
-
-#include "dir_asset.h"
-#include "media_data_ability_const.h"
-#include "media_lib_service_const.h"
-#include "rdb_store.h"
-#include "medialibrary_album_operations.h"
-#include "fetch_result.h"
-#include "file_asset.h"
-#include "datashare_predicates.h"
-#include "datashare_values_bucket.h"
-#include "datashare_abs_result_set.h"
-#include "result_set_bridge.h"
+#include <vector>
 
 namespace OHOS {
 namespace Media {
-using namespace OHOS::DataShare;
 class MediaLibraryDataManagerUtils {
 public:
     MediaLibraryDataManagerUtils();
@@ -42,64 +28,15 @@ public:
 
     static std::string GetFileName(const std::string &path);
     static std::string GetParentPath(const std::string &path);
-    static int32_t GetParentIdFromDb(const std::string &path, const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
     static bool IsNumber(const std::string &str);
-    static int64_t GetAlbumDateModified(const std::string &albumPath);
     static std::string GetOperationType(const std::string &uri);
     static std::string GetIdFromUri(const std::string &uri);
-    static std::string GetMediaTypeUri(MediaType mediaType);
-    static bool isFileExistInDb(const std::string &relativePath,
-                                const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static std::string GetPathFromDb(const std::string &id, const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static std::string GetRecyclePathFromDb(const std::string &id,
-                                            const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static shared_ptr<FileAsset> GetFileAssetFromDb(const std::string &id,
-                                                    const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static bool checkFilePending(const std::shared_ptr<FileAsset> fileAsset);
-    static bool checkOpenMode(const std::string &mode);
-    static int32_t setFilePending(string &id, bool isPending, const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static NativeAlbumAsset CreateDirectorys(const std::string relativePath,
-                                             const std::shared_ptr<NativeRdb::RdbStore> &rdbStore,
-                                             vector<int32_t> &outIds);
-    static int32_t DeleteDirectorys(vector<int32_t> &outIds,
-                                    const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static NativeAlbumAsset GetAlbumAsset(const std::string &id, const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
     static std::string GetFileTitle(const std::string& displayName);
-    static bool isAlbumExistInDb(const std::string &path,
-                                 const std::shared_ptr<NativeRdb::RdbStore> &rdbStore,
-                                 int32_t &outRow);
-    static NativeAlbumAsset GetLastAlbumExistInDb(const std::string &path,
-                                      const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static int64_t UTCTimeSeconds();
-    static bool CheckDisplayName(std::string displayName);
-    static std::shared_ptr<AbsSharedResultSet> QueryFiles(const std::string &strQueryCondition,
-        const std::shared_ptr<RdbStore> &rdbStore);
-    static std::shared_ptr<AbsSharedResultSet> QueryFavFiles(const std::shared_ptr<RdbStore> &rdbStore);
-    static std::shared_ptr<AbsSharedResultSet> QueryTrashFiles(const std::shared_ptr<RdbStore> &rdbStore);
-    static string GetParentDisplayNameFromDb(const int &id, const shared_ptr<RdbStore> &rdbStore);
     static std::string GetNetworkIdFromUri(const std::string &uri);
-    static std::string GetDistributedAlbumSql(const std::string &strQueryCondition, const std::string &tableName);
-    static int32_t GetAssetRecycle(const int32_t &assetId,
-                                   std::string &outOldPath,
-                                   std::string &outTrashDirPath,
-                                   const std::shared_ptr<NativeRdb::RdbStore> &rdbStore,
-                                   const std::unordered_map <std::string, DirAsset> &dirQuerySetMap);
-    static int32_t MakeRecycleDisplayName(const int32_t &assetId,
-                                          std::string &outDisplayName,
-                                          const std::string &trashDirPath,
-                                          const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-
-    static bool IsColumnValueExist(const std::string &value,
-                                   const std::string &column,
-                                   const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-
-    static bool isRecycleAssetExist(const int32_t &assetId,
-        std::string &outRecyclePath,
-        const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
-    static std::shared_ptr<AbsSharedResultSet> QueryAgeingTrashFiles(const std::shared_ptr<RdbStore> &rdbStore);
     static std::string GetDisPlayNameFromPath(std::string &path);
-    static bool IsAssetExistInDb(const int &id,
-                                 const std::shared_ptr<NativeRdb::RdbStore> &rdbStore);
+    static void SplitKeyValue(const std::string &keyValue, std::string &key, std::string &value);
+    static void SplitKeys(const std::string &query, std::vector<std::string> &keys);
+    static std::string ObtionCondition(std::string &strQueryCondition, const std::vector<std::string> &whereArgs);
 };
 } // namespace Media
 } // namespace OHOS
