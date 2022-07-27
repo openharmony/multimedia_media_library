594549031d7034cf642bd642bc2c92c4074e342b
zuojiangjiang <zuojiangjiang@huawei.com>
2022-07-27 18:55:12 +0800

    fix error Signed-off-by: zuojiangjiang <zuojiangjiang@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/distributed_media_library/devices_info_interact/devices_info_interact.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/distributed_media_library/devices_info_interact/devices_info_interact.cpp
@@ -55,7 +55,7 @@ void DevicesInfoInteract::Init()
         .backup = true,
         .autoSync = true,
         .securityLevel = DistributedKv::SecurityLevel::NO_LABEL,
-        .area = ENCRYPTION_LEVEL_INT,
+        .area = EL2,
         .syncPolicy = DistributedKv::SyncPolicy::HIGH,
         .kvStoreType = DistributedKv::KvStoreType::SINGLE_VERSION,
         .baseDir = context->GetDatabaseDir()
