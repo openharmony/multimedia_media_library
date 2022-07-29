d14e6eae979117c4ba955875ab70ddb1bca3dcfb
Qiheng Lin <linqiheng@huawei.com>
2022-07-29 11:07:58 +0800

    fix open asset of distributed album
    Signed-off-by: Qiheng Lin <linqiheng@huawei.com>
    Change-Id: I8ea70776478ae622e6a6fc586dab46701f322afb

--- frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_command.h
+++ frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_command.h
@@ -63,6 +63,8 @@ public:
     MediaLibraryCommand(const OperationObject &oprnObject, const OperationType &oprnType);
     MediaLibraryCommand(const OperationObject &oprnObject, const OperationType &oprnType,
         const NativeRdb::ValuesBucket &value);
+    MediaLibraryCommand(const OperationObject &oprnObject, const OperationType &oprnType,
+        const std::string &deviceId);
 
     MediaLibraryCommand() = delete;
     ~MediaLibraryCommand();
@@ -80,14 +82,14 @@ public:
     const std::string &GetOprnDevice();
     const Uri &GetUri() const;
 
-    void SetOprnObject(const OperationObject &oprnObject);
-    void SetOprnType(const OperationType &oprnType);
     void SetOprnAssetId(const std::string &oprnId);
-    void SetOprnDevice(const std::string &deviceId);
     void SetValueBucket(const NativeRdb::ValuesBucket &value);
     void SetTableName(const std::string &tableName);
 
 private:
+    void SetOprnObject(const OperationObject &oprnObject);
+    void SetOprnType(const OperationType &oprnType);
+    void SetOprnDevice(const std::string &deviceId);
     void ParseOprnObjectFromUri();
     void ParseOprnTypeFromUri();
     void ParseTableName();
