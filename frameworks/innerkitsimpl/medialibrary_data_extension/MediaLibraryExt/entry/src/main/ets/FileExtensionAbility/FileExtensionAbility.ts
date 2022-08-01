5fccc57b7f957e7813eef0692672625782b3c01e
gf721 <gaofeng105@huawei.com>
2022-08-01 19:23:54 +0800

    解决编译冲突
    Signed-off-by: gf721 <gaofeng105@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/FileExtensionAbility/FileExtensionAbility.ts
+++ frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/FileExtensionAbility/FileExtensionAbility.ts
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// @ts-nocheck
 import Extension from '@ohos.application.FileAccessExtensionAbility'
 
 export default class FileExtAbility extends Extension {
