0d9a4ca4a2fbe5b05a9bac7151deadd8985d1520
wenfei <wenfei9@huawei.com>
2022-09-23 08:05:31 +0800

    media thumbnail helper header
    Signed-off-by: gudehe <gudehe@huawei.com>

---
+++ frameworks/services/media_thumbnail/include/ithumbnail_helper.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_ITHUMBNAIL_HELPER_H_
+#define FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_ITHUMBNAIL_HELPER_H_
+
+#include "ability_connect_callback_stub.h"
+#include "ability_context.h"
+#include "datashare_proxy.h"
+#include "medialibrary_async_worker.h"
+#include "pixel_map.h"
+#include "result_set_bridge.h"
+#include "rdb_helper.h"
+#include "single_kvstore.h"
+#include "thumbnail_utils.h"
+
+namespace OHOS {
+namespace Media {
+class GenerateAsyncTaskData : public AsyncTaskData {
+public:
+    GenerateAsyncTaskData() = default;
+    virtual ~GenerateAsyncTaskData() override = default;
+    ThumbRdbOpt opts;
+    ThumbnailData thumbnailData;
+    std::string thumbnailKey;
+};
+
+class IThumbnailHelper {
+public:
+    IThumbnailHelper() = default;
+    virtual ~IThumbnailHelper() = default;
+    virtual int32_t CreateThumbnail(ThumbRdbOpt &opts, bool isSync = false);
+    virtual int32_t GetThumbnailPixelMap(ThumbRdbOpt &opts,
+        std::shared_ptr<DataShare::ResultSetBridge> &outResultSet);
+    static void DeleteThumbnailKv(ThumbRdbOpt &opts);
+    static void CreateLcd(AsyncTaskData *data);
+    static void CreateThumbnail(AsyncTaskData *data);
+    static void AddAsyncTask(MediaLibraryExecute executor, ThumbRdbOpt &opts, ThumbnailData &data, bool isFront);
+protected:
+    static std::shared_ptr<NativeRdb::AbsSharedResultSet> QueryThumbnailInfo(
+        ThumbRdbOpt &opts, ThumbnailData &outData, int &errorCode);
+    static std::unique_ptr<PixelMap> GetPixelMap(const std::vector<uint8_t> &image, Size &size);
+    static bool DoCreateLcd(ThumbRdbOpt &opts, ThumbnailData &data, bool force = false);
+    static bool DoCreateThumbnail(ThumbRdbOpt &opts, ThumbnailData &data, bool force = false);
+    static bool DoThumbnailSync(ThumbRdbOpt &opts, ThumbnailData &outData);
+};
+
+class ThumbnailConnection : public AAFwk::AbilityConnectionStub {
+public:
+    ThumbnailConnection() = default;
+    virtual ~ThumbnailConnection() = default;
+    void OnAbilityConnectDone(
+        const AppExecFwk::ElementName &element, const sptr<IRemoteObject> &remoteObject, int resultCode) override;
+    void OnAbilityDisconnectDone(const AppExecFwk::ElementName &element, int resultCode) override;
+    int32_t GetRemoteDataShareHelper(ThumbRdbOpt &opts, sptr<AAFwk::IAbilityConnection> &callback);
+
+private:
+    sptr<DataShare::IDataShare> dataShareProxy_;
+    SyncStatus status_;
+};
+} // namespace Media
+} // namespace OHOS
+
+#endif  // FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_ITHUMBNAIL_HELPER_H_
---
+++ frameworks/services/media_thumbnail/include/thumbnail_aging_helper.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_THUMBNAIL_AGING_HELPER_H_
+#define FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_THUMBNAIL_AGING_HELPER_H_
+
+#include "medialibrary_async_worker.h"
+#include "rdb_helper.h"
+#include "single_kvstore.h"
+#include "thumbnail_utils.h"
+
+namespace OHOS {
+namespace Media {
+class AgingAsyncTaskData : public AsyncTaskData {
+public:
+    AgingAsyncTaskData() = default;
+    virtual ~AgingAsyncTaskData() override = default;
+    ThumbRdbOpt opts;
+    ThumbnailData thumbnailData;
+};
+
+class ThumbnailAgingHelper {
+public:
+    ThumbnailAgingHelper() = delete;
+    virtual ~ThumbnailAgingHelper() = delete;
+
+    static int32_t AgingLcdBatch(ThumbRdbOpt &opts);
+
+    static int32_t AgingDistributeLcdBatch(ThumbRdbOpt &opts);
+    static int32_t ClearDistributeBatch(ThumbRdbOpt &opts);
+    static int32_t ClearKeyAndRecordFromMap(ThumbRdbOpt &opts);
+    static int32_t ClearLcdFromFileTable(ThumbRdbOpt &opts);
+    static int32_t ClearRemoteLcdFromFileTable(ThumbRdbOpt &opts);
+private:
+    static int32_t GetLcdCount(ThumbRdbOpt &opts, int &outLcdCount);
+    static int32_t GetAgingLcdData(ThumbRdbOpt &opts, int LcdLimit, std::vector<ThumbnailRdbData> &outDatas);
+    static int32_t GetDistributeLcdCount(ThumbRdbOpt &opts, int &outLcdCount);
+    static int32_t GetAgingDistributeLcdData(ThumbRdbOpt &opts,
+        int LcdLimit, std::vector<ThumbnailRdbData> &outDatas);
+
+    static int32_t AgingLcdKeysFromFiles(ThumbRdbOpt &opts);
+    static int32_t AgingThumbnailKeysFromFiles(ThumbRdbOpt &opts);
+};
+} // namespace Media
+} // namespace OHOS
+
+#endif  // FRAMEWORKS_SERVICES_THUMBNAIL_SERVICE_INCLUDE_THUMBNAIL_AGING_HELPER_H_
