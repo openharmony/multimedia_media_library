976152d9a8748b4577179e0047b50983abc28bf2
zhangdaiyue <zhangdaiyue1@huawei.com>
2022-10-29 14:09:24 +0800

    mtp data header file
    Signed-off-by: zhangdaiyue <zhangdaiyue1@huawei.com>

---
+++ frameworks/services/media_mtp/include/header_data.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_HEADER_DATA_H_
+#define FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_HEADER_DATA_H_
+#include <stdint.h>
+#include <vector>
+#include "mtp_operation_context.h"
+namespace OHOS {
+namespace Media {
+class HeaderData {
+public:
+    HeaderData(std::shared_ptr<MtpOperationContext> &context);
+    HeaderData(uint16_t containerType, uint16_t code, uint32_t transactionID);
+    ~HeaderData();
+    int Parser(std::vector<uint8_t> &buffer, uint32_t readSize);
+    int Maker(std::vector<uint8_t> &outBuffer);
+    uint16_t GetCode() const;
+    void SetCode(uint16_t code);
+    uint32_t GetContainerLength() const;
+    void SetContainerLength(uint32_t containerLength);
+    uint16_t GetContainerType() const;
+    void SetContainerType(uint16_t containerType);
+    uint32_t GetTransactionId() const;
+    void SetTransactionId(uint32_t transactionId);
+    void Reset();
+private:
+    std::shared_ptr<MtpOperationContext> context_;
+    uint32_t containerLength_ {0};
+    uint16_t containerType_ {0};
+    uint16_t code_ {0};
+    uint32_t transactionID_ {0};
+};
+} // namespace Media
+} // namespace OHOS
+#endif  // FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_PACKET_HEADER_H_
---
+++ frameworks/services/media_mtp/include/mtp_file_observer.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_MTP_FILE_OBSERVER_H_
+#define FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_MTP_FILE_OBSERVER_H_
+#include "mtp_event.h"
+#include <map>
+#include <memory>
+#include <mutex>
+#include <sys/inotify.h>
+#include <thread>
+#include <vector>
+#include "singleton.h"
+namespace OHOS {
+namespace Media {
+using ContextSptr = std::shared_ptr<MtpOperationContext>;
+class MtpFileObserver : public Singleton<MtpFileObserver> {
+public:
+    void AddFileInotify(const std::string &path, const std::string &realPath, const ContextSptr &context);
+    bool StartFileInotify();
+    bool StopFileInotify();
+
+private:
+    static bool AddInotifyEvents(const int &inotifyFd, ContextSptr &context);
+    static bool WatchPathThread(const ContextSptr &context);
+    static void SendBattery(ContextSptr &context);
+    static void SendEvent(const inotify_event &event, const std::string &path, ContextSptr &context);
+    static bool isRunning_;
+    static std::map<int, std::string> watchMap_;
+    static int inotifyFd_;
+    static std::mutex eventLock_;
+    bool startThread_;
+    bool inotifySuccess_;
+};
+} // namespace Media
+} // namespace OHOS
+#endif // FRAMEWORKS_SERVICES_MEDIA_MTP_INCLUDE_MTP_FILE_OBSERVER_H_
