9660f5d04bd77377885be6a24648bbc36badf221
zhangdaiyue <zhangdaiyue1@huawei.com>
2022-11-10 17:24:42 +0800

    mtp packet payload factory src
    Signed-off-by: zhangdaiyue <zhangdaiyue1@huawei.com>

---
+++ frameworks/services/media_mtp/src/payload_data/delete_object_data.cpp
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "payload_data/delete_object_data.h"
+#include "media_log.h"
+#include "media_mtp_utils.h"
+#include "mtp_packet_tools.h"
+#include "mtp_storage_manager.h"
+using namespace std;
+namespace OHOS {
+namespace Media {
+namespace {
+static constexpr int PARSER_PARAM_SUM = 1;
+}
+
+DeleteObjectData::DeleteObjectData(std::shared_ptr<MtpOperationContext> &context)
+    : PayloadData(context)
+{
+}
+
+DeleteObjectData::~DeleteObjectData()
+{
+}
+
+int DeleteObjectData::Parser(const std::vector<uint8_t> &buffer, uint32_t readSize)
+{
+    if ((context_ == nullptr) || (!MtpStorageManager::GetInstance()->HasStorage())) {
+        MEDIA_ERR_LOG("DeleteObjectData::parser null or storage");
+        return MTP_ERROR_CONTEXT_IS_NULL;
+    }
+
+    int parameterCount = (readSize - MTP_CONTAINER_HEADER_SIZE) / sizeof(uint32_t);
+    if (parameterCount < PARSER_PARAM_SUM) {
+        MEDIA_ERR_LOG("DeleteObjectData::parser paramCount=%{public}u, needCount=%{public}d",
+            parameterCount, PARSER_PARAM_SUM);
+        return MTP_ERROR_PACKET_INCORRECT;
+    }
+
+    size_t offset = MTP_CONTAINER_HEADER_SIZE;
+    context_->handle = MtpPacketTool::GetUInt32(buffer, offset);
+    return MTP_SUCCESS;
+}
+
+int DeleteObjectData::Maker(std::vector<uint8_t> &outBuffer)
+{
+    return MTP_SUCCESS;
+}
+
+uint32_t DeleteObjectData::CalculateSize()
+{
+    std::vector<uint8_t> tmpuse;
+
+    int res = Maker(tmpuse);
+    if (res != MTP_SUCCESS) {
+        return res;
+    }
+
+    uint32_t size = tmpuse.size();
+    return size;
+}
+} // namespace Media
+} // namespace OHOS
