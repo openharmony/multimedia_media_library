6299af021818854b118c2e2d6861f1488772ec1f
梁家熙 <liangjiaxi2@huawei.com>
2023-04-06 07:57:52 +0000

    add create photo asset tdd in api10
    Signed-off-by: 梁家熙 <liangjiaxi2@huawei.com>
    Change-Id: I393b4ed9182592c8b6564849c5050b6e6c7a9369

---
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_photo_operations_test/src/medialibrary_photo_operations_test.cpp
@@ -0,0 +1,352 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define MLOG_TAG "PhotoOperationsTest"
+
+#include "medialibrary_photo_operations_test.h"
+
+#include <fstream>
+#include <unistd.h>
+#include <unordered_map>
+
+#include "abs_rdb_predicates.h"
+#include "fetch_result.h"
+#include "file_asset.h"
+#include "media_column.h"
+#include "media_file_utils.h"
+#include "media_log.h"
+#include "medialibrary_command.h"
+#include "medialibrary_common_utils.h"
+#include "medialibrary_data_manager.h"
+#include "medialibrary_data_manager_utils.h"
+#include "medialibrary_db_const.h"
+#include "medialibrary_errno.h"
+#include "medialibrary_inotify.h"
+#include "medialibrary_photo_operations.h"
+#include "medialibrary_rdbstore.h"
+#include "medialibrary_type_const.h"
+#include "medialibrary_unistore_manager.h"
+#include "medialibrary_unittest_utils.h"
+#include "result_set_utils.h"
+#include "uri.h"
+#include "userfile_manager_types.h"
+#include "values_bucket.h"
+
+namespace OHOS {
+namespace Media {
+using namespace std;
+using namespace testing::ext;
+using namespace OHOS::NativeRdb;
+using namespace OHOS::DataShare;
+using OHOS::DataShare::DataShareValuesBucket;
+using OHOS::DataShare::DataSharePredicates;
+
+static shared_ptr<MediaLibraryRdbStore> g_rdbStore;
+
+namespace {
+void CleanTestTables()
+{
+    vector<string> dropTableList = {
+        PhotoColumn::PHOTOS_TABLE,
+        AudioColumn::AUDIOS_TABLE,
+        DocumentColumn::DOCUMENTS_TABLE,
+        ASSET_UNIQUE_NUMBER_TABLE
+        // todo: album tables
+    };
+    for (auto &dropTable : dropTableList) {
+        string dropSql = "DROP TABLE " + dropTable + ";";
+        int32_t ret = g_rdbStore->ExecuteSql(dropSql);
+        if (ret != NativeRdb::E_OK) {
+            MEDIA_ERR_LOG("Drop %{public}s table failed", dropTable.c_str());
+            return;
+        }
+        MEDIA_DEBUG_LOG("Drop %{public}s table success", dropTable.c_str());
+    }
+}
+
+struct UniqueMemberValuesBucket {
+    std::string assetMediaType;
+    int32_t startNumber;
+};
+
+void PrepareUniqueNumberTable()
+{
+    if (g_rdbStore == nullptr) {
+        MEDIA_ERR_LOG("can not get g_rdbstore");
+        return;
+    }
+    auto store = g_rdbStore->GetRaw();
+    if (store == nullptr) {
+        MEDIA_ERR_LOG("can not get store");
+        return;
+    }
+    string queryRowSql = "SELECT COUNT(*) as count FROM " + ASSET_UNIQUE_NUMBER_TABLE;
+    auto resultSet = store->QuerySql(queryRowSql);
+    if (resultSet == nullptr || resultSet->GoToFirstRow() != NativeRdb::E_OK) {
+        MEDIA_ERR_LOG("Can not get AssetUniqueNumberTable count");
+        return;
+    }
+    if (GetInt32Val("count", resultSet) != 0) {
+        MEDIA_DEBUG_LOG("AssetUniqueNumberTable is already inited");
+        return;
+    }
+
+    UniqueMemberValuesBucket imageBucket = { IMAGE_ASSET_TYPE, 0 };
+    UniqueMemberValuesBucket videoBucket = { VIDEO_ASSET_TYPE, 0 };
+    UniqueMemberValuesBucket audioBucket = { AUDIO_ASSET_TYPE, 0 };
+
+    vector<UniqueMemberValuesBucket> uniqueNumberValueBuckets = {
+        imageBucket, videoBucket, audioBucket
+    };
+
+    for (const auto& uniqueNumberValueBucket : uniqueNumberValueBuckets) {
+        ValuesBucket valuesBucket;
+        valuesBucket.PutString(ASSET_MEDIA_TYPE, uniqueNumberValueBucket.assetMediaType);
+        valuesBucket.PutInt(UNIQUE_NUMBER, uniqueNumberValueBucket.startNumber);
+        int64_t outRowId = -1;
+        int32_t insertResult = store->Insert(outRowId, ASSET_UNIQUE_NUMBER_TABLE, valuesBucket);
+        if (insertResult != NativeRdb::E_OK || outRowId <= 0) {
+            MEDIA_ERR_LOG("Prepare smartAlbum failed");
+        }
+    }
+}
+
+void SetTables()
+{
+    vector<string> createTableSqlList = {
+        PhotoColumn::CREATE_PHOTO_TABLE,
+        AudioColumn::CREATE_AUDIO_TABLE,
+        DocumentColumn::CREATE_DOCUMENT_TABLE,
+        CREATE_ASSET_UNIQUE_NUMBER_TABLE
+        // todo: album tables
+    };
+    for (auto &createTableSql : createTableSqlList) {
+        int32_t ret = g_rdbStore->ExecuteSql(createTableSql);
+        if (ret != NativeRdb::E_OK) {
+            MEDIA_ERR_LOG("Execute sql %{private}s failed", createTableSql.c_str());
+            return;
+        }
+        MEDIA_DEBUG_LOG("Execute sql %{private}s success", createTableSql.c_str());
+    }
+    PrepareUniqueNumberTable();
+}
+
+void ClearAndRestart()
+{
+    if (!MediaLibraryUnitTestUtils::IsValid()) {
+        MediaLibraryUnitTestUtils::Init();
+    }
+    
+    system("rm -rf /storage/media/local/files/*");
+    for (const auto &dir : TEST_ROOT_DIRS) {
+        string ROOT_PATH = "/storage/media/100/local/files/";
+        bool ret = MediaFileUtils::CreateDirectory(ROOT_PATH + dir + "/");
+        CHECK_AND_PRINT_LOG(ret == E_OK, "make %{public}s dir failed, ret=%{public}d", dir.c_str(), ret);
+    }
+    CleanTestTables();
+    SetTables();
+}
+
+string GetFileAssetValueToStr(FileAsset &fileAsset, const string &column)
+{
+    // judge type
+    auto member = fileAsset.GetMemberValue(column);
+    int type = -1;
+    if (std::get_if<int32_t>(&member)) {
+        type = MEMBER_TYPE_INT32;
+    } else if (std::get_if<int64_t>(&member)) {
+        type = MEMBER_TYPE_INT64;
+    } else if (std::get_if<string>(&member)) {
+        type = MEMBER_TYPE_STRING;
+    } else {
+        MEDIA_ERR_LOG("Can not find this type");
+        return "";
+    }
+
+    switch (type) {
+        case MEMBER_TYPE_INT32: {
+            int32_t resInt = fileAsset.GetInt32Member(column);
+            if (resInt != DEFAULT_INT32) {
+                return to_string(resInt);
+            }
+            break;
+        }
+        case MEMBER_TYPE_INT64: {
+            int64_t resLong = fileAsset.GetInt64Member(column);
+            if (resLong != DEFAULT_INT64) {
+                return to_string(resLong);
+            }
+            break;
+        }
+        case MEMBER_TYPE_STRING: {
+            string resStr = fileAsset.GetStrMember(column);
+            if (!resStr.empty()) {
+                return resStr;
+            }
+            return "";
+        }
+        default: {
+            return "";
+        }
+    }
+    return "0";
+}
+
+bool QueryAndVerifyPhotoAsset(const string &columnName, const string &value,
+    const unordered_map<string, string> &verifyColumnAndValuesMap)
+{
+    string querySql = "SELECT * FROM " + PhotoColumn::PHOTOS_TABLE + " WHERE " +
+        columnName + "='" + value + "';";
+
+    MEDIA_DEBUG_LOG("querySql: %{public}s", querySql.c_str());
+    auto resultSet = g_rdbStore->QuerySql(querySql);
+    if (resultSet == nullptr) {
+        MEDIA_ERR_LOG("Get resultSet failed");
+        return false;
+    }
+    
+    int32_t resultSetCount = 0;
+    int32_t ret = resultSet->GetRowCount(resultSetCount);
+    if (ret != NativeRdb::E_OK || resultSetCount <= 0) {
+        MEDIA_ERR_LOG("resultSet row count is 0");
+        return false;
+    }
+
+    shared_ptr<FetchResult<FileAsset>> fetchFileResult = make_shared<FetchResult<FileAsset>>();
+    if (fetchFileResult == nullptr) {
+        MEDIA_ERR_LOG("Get fetchFileResult failed");
+        return false;
+    }
+    auto fileAsset = fetchFileResult->GetObjectFromRdb(resultSet, 0);
+    if (fileAsset == nullptr || fileAsset->GetId() < 0) {
+        return false;
+    }
+    if (fileAsset != nullptr) {
+        for (const auto &iter : verifyColumnAndValuesMap) {
+            string resStr = GetFileAssetValueToStr(*fileAsset, iter.first);
+            if (resStr.empty()) {
+                MEDIA_ERR_LOG("verify failed! Param %{public}s is empty", resStr.c_str());
+                return false;
+            }
+            if (resStr != iter.second) {
+                MEDIA_ERR_LOG("verify failed! Except %{public}s param %{public}s, actually %{public}s",
+                    iter.first.c_str(), iter.second.c_str(), resStr.c_str());
+                return false;
+            }
+        }
+    }
+    return true;
+}
+} // namespace
+
+void TestPhotoCreateParamsApi10(const string &displayName, int32_t type, int32_t result)
+{
+    MediaLibraryCommand cmd(OperationObject::FILESYSTEM_PHOTO, OperationType::CREATE,
+        MediaLibraryApi::API_10);
+    ValuesBucket values;
+    values.PutString(MediaColumn::MEDIA_NAME, displayName);
+    values.PutInt(MediaColumn::MEDIA_TYPE, type);
+    cmd.SetValueBucket(values);
+    int32_t ret = MediaLibraryPhotoOperations::Create(cmd);
+    EXPECT_EQ(ret, result);
+}
+
+void MediaLibraryPhotoOperationsTest::SetUpTestCase()
+{
+    MediaLibraryUnitTestUtils::Init();
+    g_rdbStore = MediaLibraryUnistoreManager::GetInstance().GetRdbStoreRaw();
+    if (g_rdbStore == nullptr || g_rdbStore->GetRaw() == nullptr) {
+        MEDIA_ERR_LOG("Start MediaLibraryPhotoOperationsTest failed, can not get rdbstore");
+        exit(1);
+    }
+}
+
+void MediaLibraryPhotoOperationsTest::TearDownTestCase()
+{
+    if (!MediaLibraryUnitTestUtils::IsValid()) {
+        MediaLibraryUnitTestUtils::Init();
+    }
+    
+    system("rm -rf /storage/media/local/files/*");
+    CleanTestTables();
+    g_rdbStore = nullptr;
+}
+
+void MediaLibraryPhotoOperationsTest::SetUp()
+{
+    if (g_rdbStore == nullptr || g_rdbStore->GetRaw() == nullptr) {
+        MEDIA_ERR_LOG("Start MediaLibraryPhotoOperationsTest failed, can not get rdbstore");
+        exit(1);
+    }
+    ClearAndRestart();
+}
+
+void MediaLibraryPhotoOperationsTest::TearDown()
+{}
+
+const string CHAR256_ENGLISH =
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const string CHAR256_CHINESE =
+    "中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中"
+    "中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中中";
+
+HWTEST_F(MediaLibraryPhotoOperationsTest, photo_oprn_create_api10_test_001, TestSize.Level0)
+{
+    MEDIA_INFO_LOG("start tdd photo_oprn_create_api10_test_001");
+    MediaLibraryCommand cmd(OperationObject::FILESYSTEM_PHOTO, OperationType::CREATE,
+        MediaLibraryApi::API_10);
+    string name = "photo.jpg";
+    ValuesBucket values;
+    values.PutString(MediaColumn::MEDIA_NAME, name);
+    values.PutInt(MediaColumn::MEDIA_TYPE, MediaType::MEDIA_TYPE_IMAGE);
+    cmd.SetValueBucket(values);
+    int32_t ret = MediaLibraryPhotoOperations::Create(cmd);
+    EXPECT_GE(ret, 0);
+    unordered_map<string, string> verifyMap = {
+        { PhotoColumn::MEDIA_TITLE, "photo" },
+        { PhotoColumn::MEDIA_TYPE, to_string(MediaType::MEDIA_TYPE_IMAGE) }
+    };
+    bool res = QueryAndVerifyPhotoAsset(PhotoColumn::MEDIA_NAME, name, verifyMap);
+    EXPECT_EQ(res, true);
+    MEDIA_INFO_LOG("end tdd photo_oprn_create_api10_test_001");
+}
+
+HWTEST_F(MediaLibraryPhotoOperationsTest, photo_oprn_create_api10_test_002, TestSize.Level0)
+{
+    MEDIA_INFO_LOG("start tdd photo_oprn_create_api10_test_002");
+    string defaultRelativePath = "Pictures/1/";
+    TestPhotoCreateParamsApi10("", MediaType::MEDIA_TYPE_IMAGE, E_INVAVLID_DISPLAY_NAME);
+    TestPhotoCreateParamsApi10("photo\"\".jpg", MediaType::MEDIA_TYPE_IMAGE, E_INVAVLID_DISPLAY_NAME);
+    TestPhotoCreateParamsApi10(".photo.jpg", MediaType::MEDIA_TYPE_IMAGE, E_INVAVLID_DISPLAY_NAME);
+    string englishLongString = CHAR256_ENGLISH + ".jpg";
+    TestPhotoCreateParamsApi10(englishLongString, MediaType::MEDIA_TYPE_IMAGE,
+        E_INVAVLID_DISPLAY_NAME);
+    string chineseLongString = CHAR256_CHINESE + ".jpg";
+    TestPhotoCreateParamsApi10(chineseLongString, MediaType::MEDIA_TYPE_IMAGE,
+        E_INVAVLID_DISPLAY_NAME);
+    
+    TestPhotoCreateParamsApi10("photo", MediaType::MEDIA_TYPE_IMAGE, E_INVALID_VALUES);
+    TestPhotoCreateParamsApi10("photo.", MediaType::MEDIA_TYPE_IMAGE, E_INVALID_VALUES);
+    TestPhotoCreateParamsApi10("photo.abc", MediaType::MEDIA_TYPE_IMAGE,
+        E_CHECK_MEDIATYPE_MATCH_EXTENSION_FAIL);
+    TestPhotoCreateParamsApi10("photo.mp3", MediaType::MEDIA_TYPE_IMAGE,
+        E_CHECK_MEDIATYPE_MATCH_EXTENSION_FAIL);
+    MEDIA_INFO_LOG("end tdd photo_oprn_create_api10_test_002");
+}
+
+} // namespace Media
+} // namespace OHOS
