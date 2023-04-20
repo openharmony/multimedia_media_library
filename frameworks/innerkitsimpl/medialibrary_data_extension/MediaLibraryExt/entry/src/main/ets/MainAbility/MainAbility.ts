d0ab9520f51041f93ec8b539b2629dcfafafe2fb
huweiqi <huweiqi1@huawei.com>
2023-04-20 08:59:40 +0000

    fix js file warning
    Signed-off-by: huweiqi <huweiqi1@huawei.com>
    Change-Id: I4ff0c4494c87c536225e75b86fbe33e89a69f378

--- frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MainAbility/MainAbility.ts
+++ frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MainAbility/MainAbility.ts
@@ -1,34 +1,33 @@
-import Ability from '@ohos.app.ability.UIAbility'
+import Ability from '@ohos.app.ability.UIAbility';
 
 export default class MainAbility extends Ability {
-    onCreate(want, launchParam) {
-        console.log("[Demo] MainAbility onCreate")
-        globalThis.abilityWant = want;
-    }
+  onCreate(want, launchParam): void {
+    console.log('[Demo] MainAbility onCreate');
+    globalThis.abilityWant = want;
+  }
 
-    onDestroy() {
-        console.log("[Demo] MainAbility onDestroy")
-    }
+  onDestroy(): void {
+    console.log('[Demo] MainAbility onDestroy');
+  }
 
-    onWindowStageCreate(windowStage) {
-        // Main window is created, set main page for this ability
-        console.log("[Demo] MainAbility onWindowStageCreate")
+  onWindowStageCreate(windowStage): void {
+    // Main window is created, set main page for this ability
+    console.log('[Demo] MainAbility onWindowStageCreate');
+    windowStage.setUIContent(this.context, 'pages/index', null);
+  }
 
-        windowStage.setUIContent(this.context, "pages/index", null)
-    }
+  onWindowStageDestroy(): void {
+    // Main window is destroyed, release UI related resources
+    console.log('[Demo] MainAbility onWindowStageDestroy');
+  }
 
-    onWindowStageDestroy() {
-        // Main window is destroyed, release UI related resources
-        console.log("[Demo] MainAbility onWindowStageDestroy")
-    }
+  onForeground(): void {
+    // Ability has brought to foreground
+    console.log('[Demo] MainAbility onForeground');
+  }
 
-    onForeground() {
-        // Ability has brought to foreground
-        console.log("[Demo] MainAbility onForeground")
-    }
-
-    onBackground() {
-        // Ability has back to background
-        console.log("[Demo] MainAbility onBackground")
-    }
+  onBackground(): void {
+    // Ability has back to background
+    console.log('[Demo] MainAbility onBackground');
+  }
 };
