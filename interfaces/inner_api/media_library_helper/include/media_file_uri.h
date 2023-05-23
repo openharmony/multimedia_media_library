45d5226c9f10f3ea8013cc3ee165db03a4d5aa0f
jiahaoluo <luojiahao5@huawei.com>
2023-05-23 08:27:33 +0000

    get file path from uri
    Signed-off-by: jiahaoluo <luojiahao5@huawei.com>
    Change-Id: Ibd5a61c9f7e2f64e9660731ad3e2f3c183c8e7d6

--- interfaces/inner_api/media_library_helper/include/media_file_uri.h
+++ interfaces/inner_api/media_library_helper/include/media_file_uri.h
@@ -41,6 +41,7 @@ public:
                           MediaFileUriConstruct(mediaType, fileId, networkId, apiVersion)) {}
     std::string GetNetworkId();
     std::string GetFileId();
+    std::string GetFilePath();
     std::unordered_map<std::string, std::string> &GetQueryKeys();
     bool IsValid();
     ~MediaFileUri() = default;
