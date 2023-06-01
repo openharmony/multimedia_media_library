027ccd781237a858cf8ff653c62d2081221f5d36
梁家熙 <liangjiaxi2@huawei.com>
2023-06-01 07:59:37 +0000

    回退 'Pull Request !1908 : fix bug in api9 trans api10'

--- frameworks/js/src/file_asset_napi.cpp
+++ frameworks/js/src/file_asset_napi.cpp
@@ -424,9 +424,7 @@ napi_value FileAssetNapi::JSSetFileDisplayName(napi_env env, napi_callback_info 
         }
         status = napi_get_value_string_utf8(env, argv[PARAM0], buffer, FILENAME_MAX, &res);
         if (status == napi_ok) {
-            string displayName = string(buffer);
-            obj->fileAssetPtr->SetDisplayName(displayName);
-            obj->fileAssetPtr->SetTitle(MediaLibraryDataManagerUtils::GetFileTitle(displayName));
+            obj->fileAssetPtr->SetDisplayName(string(buffer));
         }
     }
 
@@ -526,12 +524,7 @@ napi_value FileAssetNapi::JSSetTitle(napi_env env, napi_callback_info info)
         }
         status = napi_get_value_string_utf8(env, argv[PARAM0], buffer, FILENAME_MAX, &res);
         if (status == napi_ok) {
-            string title = string(buffer);
-            string oldDisplayName = obj->fileAssetPtr->GetDisplayName();
-            string ext = MediaFileUtils::SplitByChar(oldDisplayName, '.');
-            string newDisplayName = title + "." + ext;
-            obj->fileAssetPtr->SetTitle(title);
-            obj->fileAssetPtr->SetDisplayName(newDisplayName);
+            obj->fileAssetPtr->SetTitle(string(buffer));
         }
     }
     return undefinedResult;
