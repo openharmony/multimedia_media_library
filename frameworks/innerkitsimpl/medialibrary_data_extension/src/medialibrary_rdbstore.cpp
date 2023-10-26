fef2036b7b901c2444a6a01728a466609f5386df
luojunlin <luojunlin5@huawei.com>
2023-10-26 02:48:28 +0000

    add hidden time index
    Signed-off-by: luojunlin <luojunlin5@huawei.com>
    Change-Id: I8d7a3ecef69c47911cc74ec82a69306dcfde6392

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
@@ -842,6 +842,7 @@ static const vector<string> onCreateSqlStrs = {
     PhotoColumn::CREATE_DAY_INDEX,
     PhotoColumn::CREATE_SHPT_MEDIA_TYPE_INDEX,
     PhotoColumn::CREATE_SHPT_DAY_INDEX,
+    PhotoColumn::CREATE_HIDDEN_TIME_INDEX,
     PhotoColumn::CREATE_PHOTOS_DELETE_TRIGGER,
     PhotoColumn::CREATE_PHOTOS_FDIRTY_TRIGGER,
     PhotoColumn::CREATE_PHOTOS_MDIRTY_TRIGGER,
@@ -1444,6 +1445,15 @@ void AddHiddenTimeColumn(RdbStore &store)
     ExecSqls(sqls, store);
 }
 
+void AddHiddenTimeIndex(RdbStore &store)
+{
+    const vector<string> sqls = {
+        PhotoColumn::CREATE_HIDDEN_TIME_INDEX,
+    };
+
+    ExecSqls(sqls, store);
+}
+
 static void UpgradeOtherTable(RdbStore &store, int32_t oldVersion)
 {
     if (oldVersion < VERSION_ADD_PACKAGE_NAME) {
@@ -1500,6 +1510,10 @@ static void UpgradeOtherTable(RdbStore &store, int32_t oldVersion)
 
     if (oldVersion < VERSION_ADD_HIDDEN_TIME) {
         AddHiddenTimeColumn(store);
+    }
+
+    if (oldVersion < VERSION_ADD_HIDDEN_TIME_INDEX) {
+        AddHiddenTimeIndex(store);
     }
 }
 
