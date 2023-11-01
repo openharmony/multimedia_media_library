3ce65da312a82648b4d16c69f9cf69395143bf67
j00586257 <jiangjinpeng3@huawei.com>
2023-11-01 13:18:04 +0000

    支持datetoken迁移
    Signed-off-by: j00586257 <jiangjinpeng3@huawei.com>
    Change-Id: I5acf55de6703c14d59e8e232307593750959fcb6

--- frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MediaBackupExtensionAbility/MediaBackupExtensionAbility.ts
+++ frameworks/innerkitsimpl/medialibrary_data_extension/MediaLibraryExt/entry/src/main/ets/MediaBackupExtensionAbility/MediaBackupExtensionAbility.ts
@@ -20,16 +20,16 @@ export default class MediaBackupExtAbility extends BackupExtensionAbility {
 
   async onRestore(bundleVersion : BundleVersion) : Promise<void> {
     console.log(TAG, `onRestore ok ${JSON.stringify(bundleVersion)}`);
-    console.time(TAG + " RESTORE");
+    console.time(TAG + ' RESTORE');
     if (bundleVersion.name === '0.0.0.0' && bundleVersion.code === 0) {
       await mediabackup.startRestore(UPDATE_RESTORE, galleryAppName, mediaAppName);
     } else {
       await mediabackup.startRestore(CLONE_RESTORE, galleryAppName, mediaAppName);
     }
-    console.timeEnd(TAG + " RESTORE");
-    console.time(TAG + " MOVE REST FILES");
+    console.timeEnd(TAG + ' RESTORE');
+    console.time(TAG + ' MOVE REST FILES');
     await this.moveRestFiles();
-    console.timeEnd(TAG + " MOVE REST FILES");
+    console.timeEnd(TAG + ' MOVE REST FILES');
   }
 
   private isFileExist(filePath : string) : boolean {
