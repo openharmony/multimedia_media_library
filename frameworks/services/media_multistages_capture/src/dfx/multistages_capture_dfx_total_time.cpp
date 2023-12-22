5c379baa869bae6805d5c87aecad6911912c5e92
nlzhi <niliangzhi@huawei.com>
2023-12-22 15:54:22 +0800

    add dfx of multistages capture
    Signed-off-by: nlzhi <niliangzhi@huawei.com>
    Change-Id: I33b3506e8b1fc7df7b9daed87d51818da48053bc

---
+++ frameworks/services/media_multistages_capture/src/dfx/multistages_capture_dfx_total_time.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define MLOG_TAG "MultiStagesCaptureDfxTotalTime"
+
+#include "multistages_capture_dfx_total_time.h"
+
+#include "media_file_utils.h"
+#include "media_log.h"
+#include "post_event_utils.h"
+
+namespace OHOS {
+namespace Media {
+MultiStagesCaptureDfxTotalTime::MultiStagesCaptureDfxTotalTime() {}
+
+MultiStagesCaptureDfxTotalTime::~MultiStagesCaptureDfxTotalTime() {}
+
+MultiStagesCaptureDfxTotalTime& MultiStagesCaptureDfxTotalTime::GetInstance()
+{
+    static MultiStagesCaptureDfxTotalTime instance;
+    
+    return instance;
+}
+
+void MultiStagesCaptureDfxTotalTime::AddStartTime(const std::string &photoId)
+{
+    startTimes_.insert(make_pair(photoId, MediaFileUtils::UTCTimeMilliSeconds()));
+}
+
+void MultiStagesCaptureDfxTotalTime::RemoveStartTime(const std::string &photoId)
+{
+    if (startTimes_.empty() || startTimes_.find(photoId) == startTimes_.end()) {
+        MEDIA_INFO_LOG("RemoveStartTime startTimes_ is empty or photoId is not in startTimes_");
+        return;
+    }
+
+    startTimes_.erase(photoId);
+}
+
+void MultiStagesCaptureDfxTotalTime::Report(const std::string &photoId)
+{
+    if (startTimes_.empty() || startTimes_.find(photoId) == startTimes_.end()) {
+        MEDIA_INFO_LOG("startTimes_ is empty or photoId is not in startTimes_");
+        return;
+    }
+    int64_t startTime = startTimes_[photoId];
+    int64_t totalTime = MediaFileUtils::UTCTimeMilliSeconds() - startTime;
+    startTimes_.erase(photoId);
+    VariantMap map = {{KEY_PHOTO_ID, photoId}, {KEY_TOTAL_TIME_COST, totalTime}};
+    PostEventUtils::GetInstance().PostStatProcess(StatType::MSC_TOTAL_TIME_COST_STAT, map);
+}
+
+} // namespace Media
+} // namespace OHOS
