7fad46577fe9e62934c2142f6e9c637dd638f43c
fangmx <fangmingxiang@huawei.com>
2024-01-03 16:31:35 +0800

    4k视频缩略图生成过大
    Signed-off-by: fangmx <fangmingxiang@huawei.com>

--- frameworks/services/media_thumbnail/src/thumbnail_utils.cpp
+++ frameworks/services/media_thumbnail/src/thumbnail_utils.cpp
@@ -214,7 +214,7 @@ bool ThumbnailUtils::LoadVideoFile(ThumbnailData &data, const bool isThumbnail, 
         VariantMap map = {{KEY_ERR_FILE, __FILE__}, {KEY_ERR_LINE, __LINE__}, {KEY_ERR_CODE, E_THUMBNAIL_UNKNOWN},
             {KEY_OPT_FILE, data.path}, {KEY_OPT_TYPE, OptType::THUMB}};
         PostEventUtils::GetInstance().PostErrorProcess(ErrType::FILE_OPT_ERR, map);
-        MEDIA_ERR_LOG("Av meta data helper fetch frame at time failed");
+        MEDIA_ERR_LOG("data source get failed");
         return false;
     }
     int width = data.source->GetWidth();
@@ -225,6 +225,17 @@ bool ThumbnailUtils::LoadVideoFile(ThumbnailData &data, const bool isThumbnail, 
         MEDIA_ERR_LOG("ResizeThumb failed");
     }
     desiredSize = {width, height};
+    param.dstWidth = width;
+    param.dstHeight = height;
+    data.source = avMetadataHelper->FetchFrameAtTime(AV_FRAME_TIME, AVMetadataQueryOption::AV_META_QUERY_NEXT_SYNC,
+        param);
+    if (data.source == nullptr) {
+        VariantMap map = {{KEY_ERR_FILE, __FILE__}, {KEY_ERR_LINE, __LINE__}, {KEY_ERR_CODE, E_THUMBNAIL_UNKNOWN},
+            {KEY_OPT_FILE, data.path}, {KEY_OPT_TYPE, OptType::THUMB}};
+        PostEventUtils::GetInstance().PostErrorProcess(ErrType::FILE_OPT_ERR, map);
+        MEDIA_ERR_LOG("data source get failed");
+        return false;
+    }
     auto resultMap = avMetadataHelper->ResolveMetadata();
     string videoOrientation = resultMap.at(AV_KEY_VIDEO_ORIENTATION);
     if (!videoOrientation.empty()) {
