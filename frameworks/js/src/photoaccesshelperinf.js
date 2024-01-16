813d55d5371048ff144ad8722768763e3949251a
f00610455 <fanjingyu2@huawei.com>
2024-01-16 15:53:49 +0800

    命名修改
    Signed-off-by: f00610455 <fanjingyu2@huawei.com>

--- frameworks/js/src/photoaccesshelperinf.js
+++ frameworks/js/src/photoaccesshelperinf.js
@@ -322,7 +322,7 @@ function parsePhotoPickerSelectOption(args) {
     config.parameters.isPhotoTakingSupported = option.isPhotoTakingSupported === undefined || option.isPhotoTakingSupported;
     config.parameters.isEditSupported = option.isEditSupported === undefined || option.isEditSupported;
     config.parameters.recommendationOptions = option.recommendationOptions;
-    config.parameters.preselectUris = option.preselectUris;
+    config.parameters.preselectedUris = option.preselectedUris;
   }
 
   return config;
