f8d7841dcd40f75c87a188d0be1f6658ba3287ff
luojunlin <luojunlin5@huawei.com>
2024-02-22 20:00:50 +0800

    修复非系统应用能获取所有相册
    Signed-off-by: luojunlin <luojunlin5@huawei.com>
    Change-Id: I09bf5c288c96346159e91d6832eefb1e91cb625d

--- frameworks/js/src/media_library_napi.cpp
+++ frameworks/js/src/media_library_napi.cpp
@@ -5826,6 +5826,14 @@ static napi_value ParseAlbumTypes(napi_env env, unique_ptr<MediaLibraryAsyncCont
     if (albumSubType == PhotoAlbumSubType::SHOOTING_MODE || albumSubType == PhotoAlbumSubType::GEOGRAPHY_CITY) {
         context->predicates.OrderByDesc(PhotoAlbumColumns::ALBUM_COUNT);
     }
+
+    napi_value result = nullptr;
+    CHECK_ARGS(env, napi_get_boolean(env, true, &result), JS_INNER_FAIL);
+    return result;
+}
+
+static void RestrictAlbumSubtypeOptions(unique_ptr<MediaLibraryAsyncContext> &context)
+{
     if (!MediaLibraryNapiUtils::IsSystemApp()) {
         context->predicates.And()->In(PhotoAlbumColumns::ALBUM_SUBTYPE, vector<string>({
             to_string(PhotoAlbumSubType::USER_GENERIC),
@@ -5835,10 +5843,6 @@ static napi_value ParseAlbumTypes(napi_env env, unique_ptr<MediaLibraryAsyncCont
     } else {
         context->predicates.And()->NotEqualTo(PhotoAlbumColumns::ALBUM_SUBTYPE, to_string(PhotoAlbumSubType::HIDDEN));
     }
-
-    napi_value result = nullptr;
-    CHECK_ARGS(env, napi_get_boolean(env, true, &result), JS_INNER_FAIL);
-    return result;
 }
 
 static napi_value ParseArgsGetPhotoAlbum(napi_env env, napi_callback_info info,
@@ -5875,6 +5879,7 @@ static napi_value ParseArgsGetPhotoAlbum(napi_env env, napi_callback_info info,
         default:
             return nullptr;
     }
+    RestrictAlbumSubtypeOptions(context);
     if (context->isLocationAlbum != PhotoAlbumSubType::GEOGRAPHY_LOCATION &&
         context->isLocationAlbum != PhotoAlbumSubType::GEOGRAPHY_CITY) {
         CHECK_NULLPTR_RET(AddDefaultPhotoAlbumColumns(env, context->fetchColumn));
