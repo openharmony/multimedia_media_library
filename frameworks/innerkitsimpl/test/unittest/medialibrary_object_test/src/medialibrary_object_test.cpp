fa2f4e49bb0a5c0cf40e8b89f0f0a37ecd111ef7
z30051992 <zhangtianmin2@huawei.com>
2024-03-30 21:22:55 +0800

    remove accidental change
    Signed-off-by: z30051992 <zhangtianmin2@huawei.com>
    Change-Id: Idd303dbb822bb139b4a1f2cfa1fae73cddd05c05

--- frameworks/innerkitsimpl/test/unittest/medialibrary_object_test/src/medialibrary_object_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_object_test/src/medialibrary_object_test.cpp
@@ -20,10 +20,8 @@
 #include "medialibrary_data_manager.h"
 #include "medialibrary_object_test.h"
 #define private public
-#define protected public
 #include "medialibrary_object_utils.h"
 #undef private
-#undef protected
 
 using namespace std;
 using namespace OHOS;
