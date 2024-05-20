a458e361556ca1e304806811d8131d505c6aa4bb
caojiajun <caojiajun6@huawei.com>
2024-05-20 22:18:11 +0800

    add log for new asset
    Signed-off-by: caojiajun <caojiajun6@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_inotify.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_inotify.cpp
@@ -87,7 +87,7 @@ void MediaLibraryInotify::WatchCallBack()
             meetEvent = (eventMask & IN_CLOSE_NOWRITE) ? (meetEvent | IN_CLOSE_NOWRITE) : meetEvent;
             if (((meetEvent & IN_CLOSE_WRITE) && (meetEvent & IN_MODIFY)) ||
                 ((meetEvent & IN_CLOSE_NOWRITE) && (meetEvent & IN_MODIFY))) {
-                MEDIA_DEBUG_LOG("path:%s, meetEvent:%x file_id:%s", item.path_.c_str(),
+                MEDIA_INFO_LOG("path:%s, meetEvent:%x file_id:%s", item.path_.c_str(),
                     meetEvent, item.uri_.c_str());
                 string id = MediaFileUtils::GetIdFromUri(item.uri_);
                 string itemPath = ConvertMediaPath(item.path_);
