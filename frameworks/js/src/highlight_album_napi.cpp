18ebeec0036669393ef3385fde9dd7f7c21c7f11
mwx1223061 <mabo25@h-partners.com>
2024-06-24 21:41:51 +0800

    Signed-off-by: mwx1223061 <mabo25@h-partners.com> 安全告警

--- frameworks/js/src/highlight_album_napi.cpp
+++ frameworks/js/src/highlight_album_napi.cpp
@@ -331,7 +331,7 @@ static void JSGetHighlightResourceExecute(napi_env env, void *data)
 
     ssize_t readBytes = read(uniqueFd.Get(), arrayBufferData, fileLen);
     if (readBytes != fileLen) {
-        NAPI_ERR_LOG("read file failed, read bytes is %{public}zu, actual length is %{public}lu, "
+        NAPI_ERR_LOG("read file failed, read bytes is %{public}zu, actual length is %{public}lld, "
             "error: %{public}d", readBytes, fileLen, errno);
         return;
     }
