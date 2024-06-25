ef66b3ab1a085a327f1c0af65d748341334b7fdc
luojunlin <luojunlin5@huawei.com>
2024-06-25 17:04:36 +0800

    fix build error
    Signed-off-by: luojunlin <luojunlin5@huawei.com>
    Change-Id: I577f79be4d76df13a2dc968c42282ab785f1f690

--- frameworks/innerkitsimpl/test/unittest/medialibrary_tool/src/userfile_client_ex.cpp
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_tool/src/userfile_client_ex.cpp
@@ -280,10 +280,11 @@ int UserFileClientEx::Open(const std::string &uri, const std::string &mode, bool
     if (uri.empty()) {
         return Media::E_FAIL;
     }
-    MediaFileUtils::UriAppendKeyValue(uri, IS_TOOL_OPEN,
+    std::string uriWithKey {uri};
+    MediaFileUtils::UriAppendKeyValue(uriWithKey, IS_TOOL_OPEN,
         TOOL_OPEN_TRUE);
-    Uri openUri(uri);
-    MEDIA_INFO_LOG("open. uri:%{public}s, mode:%{public}s", uri.c_str(), mode.c_str());
+    Uri openUri(uriWithKey);
+    MEDIA_INFO_LOG("open. uri:%{public}s, mode:%{public}s", uriWithKey.c_str(), mode.c_str());
     return UserFileClient::OpenFile(openUri, mode);
 }
 
