0c4171842e595717666c512ee1d1cba440cca61c
z30051992 <zhangtianmin2@huawei.com>
2024-07-27 09:20:39 +0800

    Revert "consider duplicate photos"
    This reverts commit 8b85b64bff7740ef7cdb07ae60f642cd8ec95293.
    
    Signed-off-by: z30051992 <zhangtianmin2@huawei.com>
    Change-Id: I6e01efae9353e9ded1239f5aeae1c42e5e2925b5

--- frameworks/services/media_backup_extension/include/backup_const_column.h
+++ frameworks/services/media_backup_extension/include/backup_const_column.h
@@ -29,7 +29,6 @@ const int32_t RENAME_OPERATION_RENAMED = 1;
 const std::string CURRENT_ANALYSIS_VERSION = "1.0"; // update-to-date face analysis version
 const std::string GALLERY_TABLE_MERGE_FACE = "merge_face";
 const std::string GALLERY_TABLE_MERGE_TAG = "merge_tag";
-const std::string GALLERY_TABLE_MEDIA = "gallery_media";
 const std::string GALLERY_TAG_ID = "tag_id";
 const std::string GALLERY_GROUP_TAG = "merge_tag.group_tag";
 const std::string GALLERY_TAG_NAME = "merge_tag.tag_name";
@@ -47,10 +46,8 @@ const std::string GALLERY_TOTAL_FACE = "merge_face.total_face";
 const std::string GALLERY_FACE_ID = "merge_face.face_id";
 const std::string GALLERY_LANDMARKS = "merge_face.landmarks";
 const std::string GALLERY_MERGE_FACE_HASH = GALLERY_TABLE_MERGE_FACE + "." + GALLERY_HASH;
-const std::string GALLERY_MEDIA_HASH = GALLERY_TABLE_MEDIA + "." + GALLERY_HASH;
 const std::string GALLERY_MERGE_FACE_TAG_ID = GALLERY_TABLE_MERGE_FACE + "." + GALLERY_TAG_ID;
 const std::string GALLERY_MERGE_TAG_TAG_ID = GALLERY_TABLE_MERGE_TAG + "." + GALLERY_TAG_ID;
-const std::string GALLERY_MEDIA_ID = GALLERY_TABLE_MEDIA + "." + GALLERY_ID;
 const std::string E_VERSION = "-1";
 const std::string TAG_ID_PREFIX = "ser_";
 const std::string TAG_ID_UNPROCESSED = "-1";
@@ -64,8 +61,6 @@ const std::string GALLERY_PORTRAIT_ALBUM_TABLE = GALLERY_TABLE_MERGE_TAG + " WHE
 const std::string QUERY_GALLERY_PORTRAIT_ALBUM_COUNT = "SELECT count(1) as count FROM " + GALLERY_PORTRAIT_ALBUM_TABLE;
 const std::string GALLERY_FACE_TABLE_JOIN_TAG = GALLERY_TABLE_MERGE_FACE + " INNER JOIN " + GALLERY_TABLE_MERGE_TAG +
     " ON " + GALLERY_MERGE_FACE_TAG_ID + " = " + GALLERY_MERGE_TAG_TAG_ID;
-const std::string GALLERY_FACE_TABLE_FULL = GALLERY_TABLE_MEDIA + " INNER JOIN " + GALLERY_TABLE_MERGE_FACE + " ON " +
-    GALLERY_MEDIA_HASH + " = " + GALLERY_MERGE_FACE_HASH;
 } // namespace Media
 } // namespace OHOS
 
