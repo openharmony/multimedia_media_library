236f97f2fcfbded449c81f271d1cdd44c4ba8be3
caojiajun <caojiajun6@huawei.com>
2024-08-17 17:00:37 +0800

    make MoveFile return int
    Signed-off-by: caojiajun <caojiajun6@huawei.com>
    Change-Id: I09284753aa1330ed9f5a1bf94d0b3a467b3fe853

--- frameworks/services/media_backup_extension/include/backup_file_utils.h
+++ frameworks/services/media_backup_extension/include/backup_file_utils.h
@@ -33,7 +33,7 @@ public:
     static std::string GetFullPathByPrefixType(PrefixType prefixType, const std::string &relativePath);
     static int32_t CreatePath(int32_t mediaType, const std::string &displayName, std::string &path);
     static int32_t PreparePath(const std::string &path);
-    static bool MoveFile(const string &oldPath, const string &newPath, int32_t sceneCode);
+    static int32_t MoveFile(const string &oldPath, const string &newPath, int32_t sceneCode);
     static std::string GetReplacedPathByPrefixType(PrefixType srcPrefixType, PrefixType dstPrefixType,
         const std::string &path);
     static void ModifyFile(const std::string path, int64_t modifiedTime);
