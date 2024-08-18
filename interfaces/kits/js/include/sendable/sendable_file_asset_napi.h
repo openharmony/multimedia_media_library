9ffc1a48ddce9afbe0ccd2e1787da992d4d84b9a
j00586257 <jiangjinpeng3@huawei.com>
2024-08-18 16:23:39 +0800

    媒体库Sendable接口 NAPI实现
    Signed-off-by: j00586257 <jiangjinpeng3@huawei.com>
    Change-Id: I3d50774c9b4f02cf69476448dfbb1d859caf07a8

---
+++ interfaces/kits/js/include/sendable/sendable_file_asset_napi.h
@@ -0,0 +1,141 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_FILE_ASSET_NAPI_SENDABLE_H_
+#define INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_FILE_ASSET_NAPI_SENDABLE_H_
+
+#include <mutex>
+
+#include "file_asset.h"
+#include "medialibrary_type_const.h"
+#include "napi/native_api.h"
+#include "napi/native_node_api.h"
+#include "napi_error.h"
+#include "values_bucket.h"
+#include "napi_remote_object.h"
+#include "datashare_predicates.h"
+#include "datashare_abs_result_set.h"
+#include "datashare_helper.h"
+#include "context.h"
+#include "thumbnail_manager.h"
+
+namespace OHOS {
+namespace Media {
+#define EXPORT __attribute__ ((visibility ("default")))
+static const std::string SENDABLE_FILE_ASSET_NAPI_CLASS_NAME = "SendableFileAsset";
+static const std::string SENDABLE_USERFILEMGR_FILEASSET_NAPI_CLASS_NAME = "SendableUserFileMgrFileAsset";
+static const std::string SENDABLE_PHOTOACCESSHELPER_FILEASSET_NAPI_CLASS_NAME = "SendablePhotoAccessHelperFileAsset";
+
+struct SendableAnalysisSourceInfo {
+    std::string fieldStr;
+    std::string uriStr;
+    std::vector<std::string> fetchColumn;
+};
+
+class SendableFileAssetNapi {
+public:
+    EXPORT SendableFileAssetNapi();
+    EXPORT ~SendableFileAssetNapi();
+
+    EXPORT static napi_value PhotoAccessHelperInit(napi_env env, napi_value exports);
+    EXPORT static napi_value CreateFileAsset(napi_env env, std::unique_ptr<FileAsset> &iAsset);
+    EXPORT static napi_value CreatePhotoAsset(napi_env env, std::shared_ptr<FileAsset> &fileAsset);
+
+    std::string GetFileDisplayName() const;
+    std::string GetRelativePath() const;
+    std::string GetFilePath() const;
+    std::string GetTitle() const;
+    std::string GetFileUri() const;
+    int32_t GetFileId() const;
+    int32_t GetOrientation() const;
+    MediaType GetMediaType() const;
+    std::string GetNetworkId() const;
+    bool IsFavorite() const;
+    void SetFavorite(bool isFavorite);
+    bool IsTrash() const;
+    void SetTrash(bool isTrash);
+    bool IsHidden() const;
+    void SetHidden(bool isHidden);
+    std::string GetAllExif() const;
+    std::string GetFrontCamera() const;
+    std::string GetUserComment() const;
+    std::shared_ptr<FileAsset> GetFileAssetInstance() const;
+
+private:
+    EXPORT static void FileAssetNapiDestructor(napi_env env, void *nativeObject, void *finalize_hint);
+    EXPORT static napi_value FileAssetNapiConstructor(napi_env env, napi_callback_info info);
+
+    EXPORT static napi_value JSGetFileUri(napi_env env, napi_callback_info info);
+    EXPORT static napi_value JSGetFileDisplayName(napi_env env, napi_callback_info info);
+    EXPORT static napi_value JSGetFilePath(napi_env env, napi_callback_info info);
+    EXPORT static napi_value JSGetMediaType(napi_env env, napi_callback_info info);
+    EXPORT static napi_value JSSetFileDisplayName(napi_env env, napi_callback_info info);
+
+    void UpdateFileAssetInfo();
+    EXPORT static napi_value PhotoAccessHelperSet(napi_env env, napi_callback_info info);
+    EXPORT static napi_value PhotoAccessHelperGet(napi_env env, napi_callback_info info);
+    EXPORT static napi_value PhotoAccessHelperGetAnalysisData(napi_env env, napi_callback_info info);
+
+    EXPORT static napi_value PhotoAccessHelperCommitModify(napi_env env, napi_callback_info info);
+    EXPORT static napi_value PhotoAccessHelperGetThumbnail(napi_env env, napi_callback_info info);
+    EXPORT static napi_value PhotoAccessHelperRequestSource(napi_env env, napi_callback_info info);
+    EXPORT static napi_value ConvertFromPhotoAsset(napi_env env, napi_callback_info info);
+    EXPORT static napi_value ConvertToPhotoAsset(napi_env env, napi_callback_info info);
+
+    bool HandleParamSet(const std::string &inputKey, const std::string &value, ResultNapiType resultNapiType);
+    napi_env env_;
+
+    static thread_local napi_ref photoAccessHelperConstructor_;
+    static thread_local FileAsset *sFileAsset_;
+    std::shared_ptr<FileAsset> fileAssetPtr = nullptr;
+    static std::shared_ptr<ThumbnailManager> thumbnailManager_;
+    std::unordered_map<std::string, std::variant<int32_t, int64_t, std::string, double>> member_;
+    std::mutex mutex_;
+};
+struct SendableFileAssetAsyncContext : public NapiError {
+    napi_async_work work;
+    napi_deferred deferred;
+    napi_ref callbackRef;
+    bool status;
+    SendableFileAssetNapi *objectInfo;
+    std::shared_ptr<FileAsset> objectPtr = nullptr;
+    OHOS::DataShare::DataShareValuesBucket valuesBucket;
+    Size size;
+    bool isDirectory;
+    int32_t changedRows;
+    int32_t fd;
+    int32_t analysisType = AnalysisType::ANALYSIS_INVALID;
+    bool isFavorite = false;
+    bool isTrash = false;
+    bool isHidden = false;
+    bool isPending = false;
+    bool hasEdit = false;
+    std::string networkId;
+    std::string analysisData;
+    std::shared_ptr<PixelMap> pixelmap;
+
+    size_t argc;
+    napi_value argv[NAPI_ARGC_MAX];
+    ResultNapiType resultNapiType;
+    std::string userComment;
+    std::string jsonStr;
+    std::string editData;
+    std::string uri;
+    char* editDataBuffer;
+};
+} // namespace Media
+} // namespace OHOS
+
+#endif  // INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_FILE_ASSET_NAPI_H_
---
+++ interfaces/kits/js/include/sendable/sendable_medialibrary_napi_utils.h
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2021-2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_MEDIALIBRARY_NAPI_SENDABLE_UTILS_H_
+#define INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_MEDIALIBRARY_NAPI_SENDABLE_UTILS_H_
+
+#include <memory>
+#include <vector>
+
+#include "datashare_predicates.h"
+#include "datashare_result_set.h"
+#include "file_asset.h"
+#include "location_column.h"
+#include "media_column.h"
+#include "medialibrary_napi_utils.h"
+#include "medialibrary_db_const.h"
+#include "medialibrary_napi_log.h"
+#include "napi/native_api.h"
+#include "napi/native_node_api.h"
+#include "photo_album_column.h"
+#include "rdb_store.h"
+
+namespace OHOS {
+namespace Media {
+struct SendableJSAsyncContextOutput {
+    napi_value error;
+    napi_value data;
+    bool status;
+};
+
+/* Util class used by napi asynchronous methods for making call to js callback function */
+class SendableMediaLibraryNapiUtils {
+public:
+    static const std::unordered_map<std::string, ResultSetDataType> &GetTypeMap()
+    {
+        static const std::unordered_map<std::string, ResultSetDataType> TYPE_MAP = {
+            {MEDIA_DATA_DB_ID, TYPE_INT32},
+            {MEDIA_DATA_DB_FILE_PATH, TYPE_STRING},
+            {MEDIA_DATA_DB_NAME, TYPE_STRING},
+            {MEDIA_DATA_DB_MEDIA_TYPE, TYPE_INT32},
+            {MEDIA_DATA_DB_DATE_ADDED, TYPE_INT64},
+            {MEDIA_DATA_DB_DATE_MODIFIED, TYPE_INT64},
+            {MEDIA_DATA_DB_DATE_TAKEN, TYPE_INT64},
+            {MEDIA_DATA_DB_COUNT, TYPE_INT32},
+        };
+        return TYPE_MAP;
+    }
+
+    static napi_status GetUInt32(napi_env env, napi_value arg, uint32_t &value);
+    static napi_status GetInt32(napi_env env, napi_value arg, int32_t &value);
+    static napi_status GetParamBool(napi_env env, napi_value arg, bool &result);
+    static napi_status GetParamFunction(napi_env env, napi_value arg, napi_ref &callbackRef);
+    static napi_status GetParamStringWithLength(napi_env env, napi_value arg, int32_t maxLen,
+        std::string &str);
+    static napi_status GetParamStringPathMax(napi_env env, napi_value arg, std::string &str);
+    static napi_status GetProperty(napi_env env, const napi_value arg, const std::string &propName,
+        std::string &propValue);
+    static napi_status GetArrayProperty(napi_env env, napi_value arg, const std::string &propName,
+        std::vector<std::string> &array);
+    static napi_status GetStringArray(napi_env env, napi_value arg, std::vector<std::string> &array);
+    static MediaType GetMediaTypeFromUri(const std::string &uri);
+    template <class AsyncContext>
+    static napi_status GetPredicate(napi_env env, const napi_value arg, const std::string &propName,
+        AsyncContext &context, const FetchOptionType &fetchOptType);
+    template <class AsyncContext>
+    static napi_status ParseAlbumFetchOptCallback(napi_env env, napi_callback_info info, AsyncContext &context);
+    template <class AsyncContext>
+    static bool HandleSpecialPredicate(AsyncContext &context,
+        std::shared_ptr<DataShare::DataShareAbsPredicates> &predicate, const FetchOptionType &fetchOptType);
+    template <class AsyncContext>
+    static void UpdateMediaTypeSelections(AsyncContext *context);
+
+    template <class AsyncContext>
+    static napi_status AsyncContextSetObjectInfo(napi_env env, napi_callback_info info, AsyncContext &asyncContext,
+        const size_t minArgs, const size_t maxArgs);
+
+    template <class AsyncContext>
+    static napi_status AsyncContextGetArgs(napi_env env, napi_callback_info info, AsyncContext &asyncContext,
+        const size_t minArgs, const size_t maxArgs);
+
+    template <class AsyncContext>
+    static napi_status GetFetchOption(napi_env env, napi_value arg, const FetchOptionType &fetchOptType,
+        AsyncContext &context);
+
+    template <class AsyncContext>
+    static napi_status GetAlbumFetchOption(napi_env env, napi_value arg, const FetchOptionType &fetchOptType,
+        AsyncContext &context);
+
+    template <class AsyncContext>
+    static napi_status GetParamCallback(napi_env env, AsyncContext &context);
+
+    template <class AsyncContext>
+    static napi_status ParseAssetFetchOptCallback(napi_env env, napi_callback_info info,
+        AsyncContext &context);
+
+    template <class AsyncContext>
+    static napi_status ParseArgsBoolCallBack(napi_env env, napi_callback_info info, AsyncContext &context, bool &param);
+
+    template <class AsyncContext>
+    static napi_status ParseArgsStringCallback(napi_env env, napi_callback_info info, AsyncContext &context,
+        std::string &param);
+    template <class AsyncContext>
+    static napi_status ParseArgsStringArrayCallback(napi_env env, napi_callback_info info,
+    AsyncContext &context, std::vector<std::string> &array);
+
+    template <class AsyncContext>
+    static napi_status ParseArgsNumberCallback(napi_env env, napi_callback_info info, AsyncContext &context,
+        int32_t &value);
+
+    template <class AsyncContext>
+    static napi_status ParseArgsOnlyCallBack(napi_env env, napi_callback_info info, AsyncContext &context);
+
+    static AssetType GetAssetType(MediaType type);
+
+    static void AppendFetchOptionSelection(std::string &selection, const std::string &newCondition);
+
+    template <class AsyncContext>
+    static bool GetLocationPredicate(AsyncContext &context,
+        std::shared_ptr<DataShare::DataShareAbsPredicates> &predicate);
+
+    static int TransErrorCode(const std::string &Name, std::shared_ptr<DataShare::DataShareResultSet> resultSet);
+
+    static int TransErrorCode(const std::string &Name, int error);
+
+    static void HandleError(napi_env env, int error, napi_value &errorObj, const std::string &Name);
+
+    static void CreateNapiErrorObject(napi_env env, napi_value &errorObj, const int32_t errCode,
+        const std::string errMsg);
+
+    static void InvokeJSAsyncMethod(napi_env env, napi_deferred deferred, napi_ref callbackRef, napi_async_work work,
+        const SendableJSAsyncContextOutput &asyncContext);
+
+    template <class AsyncContext>
+    static napi_value NapiCreateAsyncWork(napi_env env, std::unique_ptr<AsyncContext> &asyncContext,
+        const std::string &resourceName,  void (*execute)(napi_env, void *),
+        void (*complete)(napi_env, napi_status, void *));
+
+    static std::tuple<bool, std::unique_ptr<char[]>, size_t> ToUTF8String(napi_env env, napi_value value);
+
+    static bool IsExistsByPropertyName(napi_env env, napi_value jsObject, const char *propertyName);
+
+    static napi_value GetPropertyValueByName(napi_env env, napi_value jsObject, const char *propertyName);
+
+    static bool CheckJSArgsTypeAsFunc(napi_env env, napi_value arg);
+
+    static bool IsArrayForNapiValue(napi_env env, napi_value param, uint32_t &arraySize);
+
+    static napi_status HasCallback(napi_env env, const size_t argc, const napi_value argv[],
+        bool &isCallback);
+
+    static napi_value GetInt32Arg(napi_env env, napi_value arg, int32_t &value);
+
+    static void UriAppendKeyValue(std::string &uri, const std::string &key, const std::string &value);
+
+    static napi_value AddDefaultAssetColumns(napi_env env, std::vector<std::string> &fetchColumn,
+        std::function<bool(const std::string &columnName)> isValidColumn, NapiAssetType assetType,
+        const PhotoAlbumSubType subType = PhotoAlbumSubType::USER_GENERIC);
+
+    static int32_t GetSystemAlbumPredicates(const PhotoAlbumSubType subType,
+        DataShare::DataSharePredicates &predicates, const bool hiddenOnly);
+    static int32_t GetUserAlbumPredicates(const int32_t albumId,
+        DataShare::DataSharePredicates &predicates, const bool hiddenOnly);
+    static int32_t GetAnalysisAlbumPredicates(const int32_t albumId, DataShare::DataSharePredicates &predicates);
+    static int32_t GetFeaturedSinglePortraitAlbumPredicates(
+        const int32_t albumId, DataShare::DataSharePredicates &predicates);
+    static int32_t GetPortraitAlbumPredicates(const int32_t albumId, DataShare::DataSharePredicates &predicates);
+    static int32_t GetAllLocationPredicates(DataShare::DataSharePredicates &predicates);
+    static int32_t GetSourceAlbumPredicates(const int32_t albumId, DataShare::DataSharePredicates &predicates,
+        const bool hiddenOnly);
+    static bool IsFeaturedSinglePortraitAlbum(std::string albumName, DataShare::DataSharePredicates &predicates);
+    static bool IsSystemApp();
+
+    static napi_value GetNapiValueArray(napi_env env, napi_value arg, std::vector<napi_value> &values);
+    static napi_value GetStringArray(
+        napi_env env, std::vector<napi_value> &napiValues, std::vector<std::string> &values);
+    static napi_value GetNextRowObject(napi_env env, std::shared_ptr<NativeRdb::AbsSharedResultSet> &resultSet);
+    static napi_value CreateValueByIndex(napi_env env, int32_t index, std::string name,
+        std::shared_ptr<NativeRdb::AbsSharedResultSet> &resultSet, const std::shared_ptr<FileAsset> &asset);
+
+    template <class AsyncContext>
+    static napi_status ParsePredicates(napi_env env,
+        const napi_value arg, AsyncContext &context, const FetchOptionType &fetchOptType);
+
+private:
+    static napi_status hasFetchOpt(napi_env env, const napi_value arg, bool &hasFetchOpt);
+};
+
+class SendableNapiScopeHandler {
+public:
+    SendableNapiScopeHandler(napi_env env);
+    ~SendableNapiScopeHandler();
+    bool IsValid();
+
+private:
+    napi_env env_;
+    napi_handle_scope scope_;
+    bool isValid_ = false;
+};
+} // namespace Media
+} // namespace OHOS
+
+#endif  // INTERFACES_KITS_JS_MEDIALIBRARY_INCLUDE_MEDIALIBRARY_NAPI_SENDABLE_UTILS_H_
