58ada4694477af19203a442f234a6cd6ae5c4b93
nlzhi <niliangzhi@huawei.com>
2024-08-21 17:04:26 +0800

    scan with out update album info when create a photo
    Signed-off-by: nlzhi <niliangzhi@huawei.com>
    Change-Id: Ib0dc0c0240dc1564620fe1a2540f44b3a3fdffeb

--- frameworks/innerkitsimpl/media_library_helper/include/medialibrary_operation.h
+++ frameworks/innerkitsimpl/media_library_helper/include/medialibrary_operation.h
@@ -147,6 +147,7 @@ enum class EXPORT OperationType : uint32_t {
     SAVE_CAMERA_PHOTO,
     REMOVE_MSC_TASK,
     TOOL_QUERY_BY_DISPLAY_NAME,
+    SCAN_WITHOUT_ALBUM_UPDATE,
 };
 
 #define OPRN_OBJ_MAP MediaOperation::GetOprnObjMap()
