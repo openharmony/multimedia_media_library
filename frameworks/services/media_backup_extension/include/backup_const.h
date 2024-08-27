bbb1ed5e2d0a71349f8377e1d5837ae0d0156693
z30051992 <zhangtianmin2@huawei.com>
2024-08-27 14:24:41 +0800

    add backup & use fixed upgrade path
    Signed-off-by: z30051992 <zhangtianmin2@huawei.com>
    Change-Id: Ice7dceeac5e076c84581379b0b02cb17eec72644

--- frameworks/services/media_backup_extension/include/backup_const.h
+++ frameworks/services/media_backup_extension/include/backup_const.h
@@ -54,6 +54,7 @@ const std::string GALLERT_HIDDEN_ALBUM = "/Pictures/hiddenAlbum";
 const std::string GALLERT_ROOT_PATH = "/storage/emulated/";
 const std::string RESTORE_FAILED_FILES_PATH = "/storage/media/local/files/Docs/Documents/restore_failed_files";
 const std::string PHOTO_FILTER_SELECTED_SIZE = "filter_selected_size";
+const std::string RESTORE_SANDBOX_DIR = "/data/storage/el2/base/.backup/restore";
 
 // DB field for update scene
 const std::string GALLERY_ID = "_id";
