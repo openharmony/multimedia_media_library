9f3d1937c0dc95f1ab5f88aa7e65e9b4c57b8800
高YJ <gaoyongjie1@huawei.com>
2024-09-05 14:50:30 +0800

    短时授权
    Signed-off-by: 高YJ <gaoyongjie1@huawei.com>
    Change-Id: I679ecb569e094c8df7b1e46f4c7e6280a12ece6f

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/media_datashare_ext_ability.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/media_datashare_ext_ability.cpp
@@ -405,6 +405,18 @@ static int32_t HandleSecurityComponentPermission(MediaLibraryCommand &cmd)
     return E_NEED_FURTHER_CHECK;
 }
 
+static int32_t HandleShortPermission(bool &need)
+{
+    int32_t err = PermissionUtils::CheckPhotoCallerPermission(PERM_SHORT_TERM_WRITE_IMAGEVIDEO) ? E_SUCCESS :
+        E_PERMISSION_DENIED;
+    if (err == E_SUCCESS) {
+        need = true;
+    } else {
+        need = false;
+    }
+    return err;
+}
+
 static int32_t UserFileMgrPermissionCheck(MediaLibraryCommand &cmd, const bool isWrite)
 {
     static const set<OperationObject> USER_FILE_MGR_OBJECTS = {
@@ -731,12 +743,15 @@ int MediaDataShareExtAbility::InsertExt(const Uri &uri, const DataShareValuesBuc
     PermParam permParam = {
         .isWrite = true,
     };
+    bool needToResetTime = false;
     CHECK_AND_RETURN_RET_LOG(permissionHandler_ != nullptr, E_PERMISSION_DENIED, "permissionHandler_ is nullptr");
     int err = permissionHandler_->CheckPermission(cmd, permParam);
     MEDIA_DEBUG_LOG("permissionHandler_ err=%{public}d", err);
     if (err != E_SUCCESS) {
         err = CheckPermFromUri(cmd, true);
     }
+    if ((err != E_SUCCESS) && cmd.GetUriStringWithoutSegment() == PAH_CREATE_PHOTO)
+        err = HandleShortPermission(needToResetTime);
     int32_t type = static_cast<int32_t>(cmd.GetOprnType());
     int32_t object = static_cast<int32_t>(cmd.GetOprnObject());
     if (err < 0) {
@@ -745,7 +760,17 @@ int MediaDataShareExtAbility::InsertExt(const Uri &uri, const DataShareValuesBuc
     }
 
     DfxTimer dfxTimer(type, object, COMMON_TIME_OUT, true);
-    return MediaLibraryDataManager::GetInstance()->InsertExt(cmd, value, result);
+    int32_t ret = MediaLibraryDataManager::GetInstance()->InsertExt(cmd, value, result);
+    if (needToResetTime) {
+        AccessTokenID tokenCaller = IPCSkeleton::GetCallingTokenID();
+        err = Security::AccessToken::AccessTokenKit::GrantPermissionForSpecifiedTime(tokenCaller,
+            PERM_SHORT_TERM_WRITE_IMAGEVIDEO, SHORT_TERM_PERMISSION_DURATION_300S);
+        if (err < 0) {
+            MEDIA_ERR_LOG("queryResultSet is nullptr! errCode: %{public}d", err);
+            return err;
+        }
+    }
+    return ret;
 }
 
 int MediaDataShareExtAbility::Update(const Uri &uri, const DataSharePredicates &predicates,
