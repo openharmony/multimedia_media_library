9904eefbfc8b82d6fb0fbbb7f6efca0d18d983b8
yanghaiyang7 <yanghaiyang7@huawei.com>
2024-09-08 12:20:44 +0000

    【相册融合专项】双升单、双克隆单、单克隆单，支持相册融合、相册互通、相册一对一特性
    Signed-off-by: yanghaiyang7 <yanghaiyang7@huawei.com>

---
+++ frameworks/services/media_backup_extension/src/restore/photos_dao.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <string>
+#include <vector>
+
+#include "photos_dao.h"
+#include "rdb_store.h"
+#include "result_set_utils.h"
+#include "userfile_manager_types.h"
+#include "backup_const.h"
+
+namespace OHOS::Media {
+/**
+ * @brief Find FileInfo related PhotoAlbu, by lPath, displayName, fileSize and orientation.
+ */
+PhotosDao::PhotosRowData PhotosDao::FindSameFileInAlbum(const FileInfo &fileInfo, int32_t maxFileId)
+{
+    PhotosDao::PhotosRowData rowData;
+    if (maxFileId <= 0) {
+        return rowData;
+    }
+    int videoFlag = fileInfo.fileType != MEDIA_TYPE_VIDEO ? 0 : 1;
+    const std::vector<NativeRdb::ValueObject> params = {
+        fileInfo.lPath, maxFileId, fileInfo.displayName, fileInfo.fileSize, videoFlag, fileInfo.orientation};
+    std::string querySql = this->SQL_PHOTOS_FIND_SAME_FILE_IN_ALBUM;
+    auto resultSet = this->mediaLibraryRdb_->QuerySql(querySql, params);
+    if (resultSet == nullptr || resultSet->GoToFirstRow() != NativeRdb::E_OK) {
+        return rowData;
+    }
+    rowData.fileId = GetInt32Val("file_id", resultSet);
+    rowData.data = GetStringVal("data", resultSet);
+    return rowData;
+}
+
+/**
+ * @brief Find FileInfo, which is not related PhotoAlbum, by displayName, fileSize and orientation.
+ */
+PhotosDao::PhotosRowData PhotosDao::FindSameFileWithoutAlbum(const FileInfo &fileInfo, int32_t maxFileId)
+{
+    PhotosDao::PhotosRowData rowData;
+    if (maxFileId <= 0) {
+        return rowData;
+    }
+    int videoFlag = fileInfo.fileType != MEDIA_TYPE_VIDEO ? 0 : 1;
+
+    const std::vector<NativeRdb::ValueObject> params = {
+        maxFileId, fileInfo.displayName, fileInfo.fileSize, videoFlag, fileInfo.orientation};
+    std::string querySql = this->SQL_PHOTOS_FIND_SAME_FILE_WITHOUT_ALBUM;
+    auto resultSet = this->mediaLibraryRdb_->QuerySql(querySql, params);
+    if (resultSet == nullptr || resultSet->GoToFirstRow() != NativeRdb::E_OK) {
+        return rowData;
+    }
+    rowData.fileId = GetInt32Val("file_id", resultSet);
+    rowData.data = GetStringVal("data", resultSet);
+    return rowData;
+}
+
+/**
+ * @brief Get basic information of the Photos.
+ */
+PhotosDao::PhotosBasicInfo PhotosDao::GetBasicInfo()
+{
+    std::string querySql = this->SQL_PHOTOS_BASIC_INFO;
+    auto resultSet = this->mediaLibraryRdb_->QuerySql(querySql);
+    PhotosDao::PhotosBasicInfo basicInfo = {0, 0};
+    if (resultSet == nullptr || resultSet->GoToFirstRow() != NativeRdb::E_OK) {
+        return basicInfo;
+    }
+    basicInfo.maxFileId = GetInt32Val("max_file_id", resultSet);
+    basicInfo.count = GetInt32Val("count", resultSet);
+    return basicInfo;
+}
+}  // namespace OHOS::Media
