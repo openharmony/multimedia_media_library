c8f846fc831b08a35e6df468f8e8ada2c04f0715
yanghaiyang7 <yanghaiyang7@huawei.com>
2024-09-08 12:21:25 +0000

    【相册融合专项】双升单、双克隆单、单克隆单，支持相册融合、相册互通、相册一对一特性
    Signed-off-by: yanghaiyang7 <yanghaiyang7@huawei.com>

---
+++ frameworks/services/media_backup_extension/src/restore/photo_album_restore.cpp
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <vector>
+#include <string>
+#include <unordered_set>
+
+#include "photo_album_restore.h"
+#include "media_log.h"
+#include "rdb_store.h"
+#include "rdb_errno.h"
+#include "result_set_utils.h"
+#include "userfile_manager_types.h"
+#include "backup_const.h"
+#include "photo_album_dao.h"
+
+namespace OHOS::Media {
+/**
+ * @brief get the data of gallery_album table, to restore the PhotoAlbum table
+ */
+std::vector<PhotoAlbumRestore::GalleryAlbumRowData> PhotoAlbumRestore::GetGalleryAlbums()
+{
+    std::vector<PhotoAlbumRestore::GalleryAlbumRowData> result;
+    std::string querySql = this->SQL_GALLERY_ALBUM_SELECT;
+    int rowCount = 0;
+    int offset = 0;
+    int pageSize = 200;
+    do {
+        std::vector<NativeRdb::ValueObject> params = {offset, pageSize};
+        auto resultSet = this->galleryRdb_->QuerySql(querySql, params);
+        if (resultSet == nullptr) {
+            MEDIA_ERR_LOG("Query resultSql is null.");
+            break;
+        }
+        while (resultSet->GoToNextRow() == NativeRdb::E_OK) {
+            PhotoAlbumRestore::GalleryAlbumRowData albumInfo;
+            albumInfo.albumName = GetStringVal(this->GALLERY_ALBUM_NAME, resultSet);
+            albumInfo.relativeBucketId = GetStringVal(this->GALLERY_ALBUM_BUCKET_ID, resultSet);
+            albumInfo.lPath = GetStringVal(this->GALLERY_ALBUM_lPATH, resultSet);
+            albumInfo.bundleName = GetStringVal(this->GALLERY_BUNDLE_NAME, resultSet);
+            albumInfo.priority = GetInt32Val(this->GALLERY_PRIORITY, resultSet);
+            result.emplace_back(albumInfo);
+        }
+        // Check if there are more rows to fetch.
+        resultSet->GetRowCount(rowCount);
+        offset += pageSize;
+    } while (rowCount > 0);
+    return result;
+}
+
+/**
+ * @brief Find the albums that need to be restored.
+ */
+std::vector<PhotoAlbumDao::PhotoAlbumRowData> PhotoAlbumRestore::GetAlbumsToRestore(
+    const std::vector<PhotoAlbumDao::PhotoAlbumRowData> &photoAlbums,
+    const std::vector<PhotoAlbumRestore::GalleryAlbumRowData> &galleryAlbums)
+{
+    // cache the lPath of the albums in the gallery_media table
+    std::unordered_set<std::string> uniquePaths;
+    for (const auto &album : photoAlbums) {
+        uniquePaths.insert(album.lPath);
+    }
+    // filter gallery_media by lPath
+    std::vector<PhotoAlbumRestore::GalleryAlbumRowData> filteredAlbums;
+    std::copy_if(galleryAlbums.begin(),
+        galleryAlbums.end(),
+        std::back_inserter(filteredAlbums),
+        [&uniquePaths](const PhotoAlbumRestore::GalleryAlbumRowData &album) {
+            return uniquePaths.find(album.lPath) == uniquePaths.end();
+        });
+    // build the result
+    std::vector<PhotoAlbumDao::PhotoAlbumRowData> result;
+    for (const PhotoAlbumRestore::GalleryAlbumRowData &galleryAlbum : filteredAlbums) {
+        PhotoAlbumDao::PhotoAlbumRowData albumRowData;
+        albumRowData.albumType = static_cast<int32_t>(PhotoAlbumType::SOURCE);
+        albumRowData.albumSubType = static_cast<int32_t>(PhotoAlbumSubType::SOURCE_GENERIC);
+        albumRowData.albumName = galleryAlbum.albumName;
+        albumRowData.bundleName = galleryAlbum.bundleName;
+        albumRowData.lPath = galleryAlbum.lPath;
+        albumRowData.priority = galleryAlbum.priority;
+        result.emplace_back(albumRowData);
+    }
+    return result;
+}
+}  // namespace OHOS::Media
