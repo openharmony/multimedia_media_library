57fcf2878e617827ee6f6f6058acfa16a9c79f63
Clari <huchao53@huawei.com>
2024-09-14 04:11:08 +0000

    update frameworks/services/media_cloud_sync_notify_handle/include/cloud_sync_notify_handler.h.
    Signed-off-by: Clari <huchao53@huawei.com>

--- frameworks/services/media_cloud_sync_notify_handle/include/cloud_sync_notify_handler.h
+++ frameworks/services/media_cloud_sync_notify_handle/include/cloud_sync_notify_handler.h
@@ -24,16 +24,20 @@ namespace Media {
 
 #define EXPORT __attribute__ ((visibility ("default")))
 
+/**
+ * @brief 云同步状态枚举
+ */
 enum CloudSyncStatus : int32_t {
-    BEGIN = 0,
-    FIRST_FIVE_HUNDRED,
-    INCREMENT_DOWNLOAD,
-    TOTAL_DOWNLOAD,
-    TOTAL_DOWNLOAD_FINISH,
-    SYNC_SWITCHED_OFF,
-}
+    BEGIN = 0,                                   // 退出华为账号
+    FIRST_FIVE_HUNDRED,                          // 端云下行前500张
+    INCREMENT_DOWNLOAD,                          // 增量任务
+    TOTAL_DOWNLOAD,                              // 全量任务
+    TOTAL_DOWNLOAD_FINISH,                       // 全量下行云缩略图任务结束
+    SYNC_SWITCHED_OFF,                           // 关闭同步开关
+};
 
-const std::string CLOUDSYNC_STATUS_KEY = "";
+const std::string CLOUDSYNC_STATUS_KEY = "persist.kernel.cloudsync.status";
+
 class CloudSyncNotifyHandler {
 public:
     CloudSyncNotifyHandler(const CloudSyncNotifyInfo &info):notifyInfo_(info) {};
