90bb8cc24f9cf27e838b64c7fca67f45cffcb9b2
yanghaiyang7 <yanghaiyang7@huawei.com>
2024-09-22 10:06:37 +0800

    after initiate album_plugin table, add 2 default album into PhotoAlbum.
    Signed-off-by: yanghaiyang7 <yanghaiyang7@huawei.com>
    Change-Id: If1ed90960b4fa1b817f5304d023902aed57bc66f

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
@@ -783,8 +783,6 @@ inline void BuildInsertSystemAlbumSql(const ValuesBucket &values, const AbsRdbPr
 
 int32_t PrepareAlbumPlugin(RdbStore &store)
 {
-    store.ExecuteSql(CREATE_DEFALUT_ALBUM_FOR_NO_RELATIONSHIP_ASSET);
-    store.ExecuteSql(CREATE_HIDDEN_ALBUM_FOR_DUAL_ASSET);
     AlbumPluginTableEventHandler albumPluginTableEventHander;
     int32_t ret = albumPluginTableEventHander.OnCreate(store);
     // after initiate album_plugin table, add 2 default album into PhotoAlbum.
