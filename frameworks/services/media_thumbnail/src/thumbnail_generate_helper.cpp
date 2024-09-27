9d7b36d3036dfdc3d8be5f4bc9066915e2b05e29
huchao <huchao53@huawei.com>
2024-09-27 11:25:23 +0800

    modify thumb thread pool
    Signed-off-by: huchao <huchao53@huawei.com>

--- frameworks/services/media_thumbnail/src/thumbnail_generate_helper.cpp
+++ frameworks/services/media_thumbnail/src/thumbnail_generate_helper.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+222
 #define MLOG_TAG "Thumbnail"
 
 #include "thumbnail_generate_helper.h"
--- frameworks/services/media_thumbnail/src/thumbnail_generate_worker_manager.cpp
+++ frameworks/services/media_thumbnail/src/thumbnail_generate_worker_manager.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+555
 #include "thumbnail_generate_worker_manager.h"
 
 #include "medialibrary_errno.h"
