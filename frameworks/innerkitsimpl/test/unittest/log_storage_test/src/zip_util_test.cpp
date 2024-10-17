6e43a972fd4eb5db75a9c43a456a49acd7ff7aa5
韩晓东 <hanxiaodong17@h-partners.com>
2024-10-17 16:13:37 +0800

    add tdd
    Signed-off-by: 韩晓东 <hanxiaodong17@h-partners.com>

---
+++ frameworks/innerkitsimpl/test/unittest/log_storage_test/src/zip_util_test.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "zip_util_test.h"
+
+#include <iostream>
+#include "media_log.h"
+#include <fstream>
+#define private public
+#include "zip_util.h"
+#undef private
+
+using namespace std;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Media {
+void ZipUtilTest::SetUpTestCase(void) {}
+
+void ZipUtilTest::TearDownTestCase(void) {}
+
+void ZipUtilTest::SetUp(void) {}
+
+void ZipUtilTest::TearDown(void) {}
+
+HWTEST_F(ZipUtilTest, Create_Zip_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Create_Zip_Test_001 begin");
+    std::string filePath = "/data/app/el2/100/log/com.ohos.medialibrary.medialibrarydata/audit/media_library.zip";
+    zipFile compressZip = ZipUtil::CreateZipFile(filePath, 0);
+    ASSERT_NE(compressZip, nullptr);
+    ZipUtil::CloseZipFile(compressZip);
+    MEDIA_INFO_LOG("Create_Zip_Test_001 end");
+}
+ 
+HWTEST_F(ZipUtilTest, Create_Zip_Test_002, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Create_Zip_Test_002 begin");
+    std::string filePath = "/data/app/el2/100/log/com.ohos.medialibrary.medialibrarydata/audit/media_library.csv";
+    std::ofstream file(filePath);
+    ASSERT_EQ(file.is_open(), true);
+    file.close();
+    std::string zipFilePath = "/data/app/el2/100/log/com.ohos.medialibrary.medialibrarydata/audit/media_library.zip";
+    zipFile compressZip = ZipUtil::CreateZipFile(zipFilePath);
+    ASSERT_NE(compressZip, nullptr);
+    int ret = ZipUtil::AddFileInZip(compressZip, filePath, KEEP_NONE_PARENT_PATH);
+    EXPECT_EQ(ret, 0);
+    ZipUtil::CloseZipFile(compressZip);
+    MEDIA_INFO_LOG("Create_Zip_Test_002 end");
+}
+} // namespace OHOS
+} // namespace OHOS
