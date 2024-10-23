581b62fea33de893de41db645ecb9e37d96ad65b
韩晓东 <hanxiaodong17@h-partners.com>
2024-10-23 14:35:22 +0800

    add tdd 1023
    Signed-off-by: 韩晓东 <hanxiaodong17@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/log_storage_test/src/hi_audit_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/log_storage_test/src/hi_audit_test.cpp
@@ -15,51 +15,77 @@
 
 #include "hi_audit_test.h"
 
-#include <iostream>
 #include "media_log.h"
-#include <fstream>
+#include <dirent.h>
+#include <fcntl.h>
+#define private public
 #include "hi_audit.h"
-#include "zip_util.h"
 
 using namespace std;
 using namespace testing::ext;
 
 namespace OHOS {
 namespace Media {
+struct HiAuditConfig {
+    std::string logPath;
+    std::string logName;
+    uint32_t logSize;
+    uint32_t fileSize;
+    uint32_t fileCount;
+};
+
+HiAuditConfig HIAUDIT_CONFIG = { "/data/storage/el2/log/audit/", "media_library", 2 * 1024, 3 * 1204 * 1024, 10 };
+const std::string HIAUDIT_LOG_NAME = HIAUDIT_CONFIG.logPath + HIAUDIT_CONFIG.logName + "_audit.csv";
+
 void HiAuditTest::SetUpTestCase(void) {}
 
 void HiAuditTest::TearDownTestCase(void) {}
 
-void HiAuditTest::SetUp(void) {}
+void HiAuditTest::SetUp(void)
+{
+    // Clear the test environment
+    if (access(HIAUDIT_CONFIG.logPath.c_str(), F_OK) == 0) {
+        std::string command = "rm -rf " + HIAUDIT_CONFIG.logPath;
+        system(command.c_str());
+    }
+}
 
 void HiAuditTest::TearDown(void) {}
 
-HWTEST_F(HiAuditTest, HiAuditTest_Write_test_001, TestSize.Level0)
+HWTEST_F(HiAuditTest, HiAuditTest_DirectoryExists_test_001, TestSize.Level0)
 {
-    MEDIA_INFO_LOG("HiAuditTest_Write_test_001 begin");
-    std::string filePath = "/data/app/el2/100/log/com.ohos.medialibrary.medialibrarydata/audit/media_library.csv";
-    int oriLineCount = 0;
-    if (std::filesystem::exists(filePath)) {
-        std::string line;
-        std::ifstream file(filePath);
-        ASSERT_EQ(file.is_open(), true);
-        while (std::getline(file, line)) {
-            ++oriLineCount;
-        }
-    }
-    AuditLog auditLog = { true, "USER BEHAVIOR", "ADD", "io", 1, "running", "ok" };
-    OHOS::Media::HiAudit::GetInstance().Write(auditLog);
-    int finalLineCount = 0;
-    if (std::filesystem::exists(filePath)) {
-        std::string line;
-        std::ifstream file(filePath);
-        ASSERT_EQ(file.is_open(), true);
-        while (std::getline(file, line)) {
-            ++finalLineCount;
-        }
-    }
-    ASSERT_EQ(finalLineCount, oriLineCount);
-    MEDIA_INFO_LOG("HiAuditTest_Write_test_001 end");
+    MEDIA_INFO_LOG("HiAuditTest_DirectoryExists_test_001 begin");
+    // Create a Log Directory
+    ASSERT_EQ(mkdir(HIAUDIT_CONFIG.logPath.c_str(), S_IRWXU | S_IRWXG | S_IROTH), 0);
+
+    // Create a Log Directory
+    int fd =
+        open(HIAUDIT_LOG_NAME.c_str(), O_CREAT | O_APPEND | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    ASSERT_GT(fd, 0);
+    close(fd);
+
+    // Obtain the HiAudit Instance
+    HiAudit &audit = HiAudit::GetInstance();
+
+    // Check Result
+    EXPECT_GT(audit.writeFd_, 0);
+    struct stat st;
+    ASSERT_EQ(stat(HIAUDIT_LOG_NAME.c_str(), &st), 0);
+    EXPECT_EQ(audit.writeLogSize_.load(), static_cast<uint64_t>(st.st_size));
+    MEDIA_INFO_LOG("HiAuditTest_DirectoryExists_test_001 end");
 }
-} // namespace OHOS
+
+HWTEST_F(HiAuditTest, HiAuditTest_DirectoryNotExists_test_002, TestSize.Level0)
+{
+    MEDIA_INFO_LOG("HiAuditTest_DirectoryNotExists_test_002 begin");
+    // Obtain the HiAudit Instance
+    HiAudit &audit = HiAudit::GetInstance();
+
+    // Check Result
+    EXPECT_GT(audit.writeFd_, 0);
+    EXPECT_EQ(audit.writeLogSize_.load(), 0);
+    MEDIA_INFO_LOG("HiAuditTest_DirectoryNotExists_test_002 end");
+
+}
+} // namespace Media
 } // namespace OHOS
