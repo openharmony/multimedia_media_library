1fb71551bc219273220b2be8a3594994852f0695
宫赫 <gonghe1@huawei.com>
2024-11-18 02:29:03 +0000

    update frameworks/services/media_scanner/src/scanner/media_scanner.cpp. 修改为英文注释
    Signed-off-by: 宫赫 <gonghe1@huawei.com>

--- frameworks/services/media_scanner/src/scanner/media_scanner.cpp
+++ frameworks/services/media_scanner/src/scanner/media_scanner.cpp
@@ -519,7 +519,7 @@ int32_t MediaScannerObj::BuildData(const struct stat &statInfo)
 
     // statinfo
     data_->SetFileSize(statInfo.st_size);
-    // 临时文件不会发送通知，不修改FileDataModified，保持IsForAdd为true
+    // Temp file will not be notified. GetForAdd remain true
     if (!data_->GetIsTemp()) {
         data_->SetFileDateModified(static_cast<int64_t>(MediaFileUtils::Timespec2Millisecond(statInfo.st_mtim)));
     }
