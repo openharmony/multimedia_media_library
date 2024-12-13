b2e2dc5774f1169cddaa31e6fa37c732c709b889
yangyang <yangyang581@huawei.com>
2024-12-13 15:54:07 +0800

    fix rename_operation states
    Signed-off-by: yangyang <yangyang581@huawei.com>
    Change-Id: I21f36093f89ee558c82eb4721975aee406bd010e

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_album_operations.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_album_operations.cpp
@@ -724,7 +724,7 @@ void GetDisplayLevelAlbumPredicates(const int32_t value, DataShare::DataSharePre
             USER_DISPLAY_LEVEL + " !=2) OR " + USER_DISPLAY_LEVEL + " IS NULL) AND ((" +
             whereClauseDisplay + ") OR (" + relatedMeFirstPage + ") OR (" + whereClauseSatifyCount + ") OR (" +
             whereClauseAlbumName + "))) GROUP BY " +
-            GROUP_TAG + " ORDER BY CASE WHEN " + RENAME_OPERATION + " = 1 THEN 0 ELSE 1 END, " + COUNT + " DESC";
+            GROUP_TAG + " ORDER BY CASE WHEN " + RENAME_OPERATION + " != 0 THEN 0 ELSE 1 END, " + COUNT + " DESC";
     } else if (value == SECOND_PAGE) {
         whereClause = ALBUM_SUBTYPE + " = " + to_string(PORTRAIT) + " AND (" + USER_DISPLAY_LEVEL + " = 2 OR (" +
             COUNT + " < " + to_string(PORTRAIT_FIRST_PAGE_MIN_COUNT) + " AND " + COUNT + " >= " +
@@ -733,7 +733,7 @@ void GetDisplayLevelAlbumPredicates(const int32_t value, DataShare::DataSharePre
             " IS NULL) " + " AND NOT (" + whereClauseAlbumName + ")))" +
             " AND " + ALBUM_ID + " NOT IN " + whereClauseRelatedMe +
             " GROUP BY " + GROUP_TAG +
-            " ORDER BY CASE WHEN " + RENAME_OPERATION + " = 1 THEN 0 ELSE 1 END, " + COUNT + " DESC";
+            " ORDER BY CASE WHEN " + RENAME_OPERATION + " != 0 THEN 0 ELSE 1 END, " + COUNT + " DESC";
     } else if (value == FAVORITE_PAGE) {
         whereClause = ALBUM_SUBTYPE + " = " + to_string(PORTRAIT) + " AND (" + USER_DISPLAY_LEVEL + " = 3 )GROUP BY " +
             GROUP_TAG + " ORDER BY " + RANK;
