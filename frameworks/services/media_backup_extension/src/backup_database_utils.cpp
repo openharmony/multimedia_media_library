fc820375c90475ab9116c880e5f306711bc132be
fengshiyi <fengyang44@huawei.com>
2025-02-20 16:53:26 +0800

    push
    Signed-off-by: fengshiyi <fengyang44@huawei.com>

--- frameworks/services/media_backup_extension/src/backup_database_utils.cpp
+++ frameworks/services/media_backup_extension/src/backup_database_utils.cpp
@@ -1013,8 +1013,7 @@ void BackupDatabaseUtils::UpdateBurstPhotos(const std::shared_ptr<NativeRdb::Rdb
 {
     const string updateSql =
         "UPDATE " + PhotoColumn::PHOTOS_TABLE + " SET " + PhotoColumn::PHOTO_BURST_COVER_LEVEL + " = 1," + 
-        PhotoColumn::PHOTO_BURST_KEY + " = NULL " + 
-        "WHERE file_id IN (" + SQL_SELECT_ERROR_BURST_PHOTOS  + ")" +
+        PhotoColumn::PHOTO_BURST_KEY + " = NULL WHERE " + SQL_SELECT_ERROR_BURST_PHOTOS +
         "AND file_id IN (" + SQL_SELECT_CLONE_FILE_IDS + ")";
     int32_t erroCode = BackupDatabaseUtils::ExecuteSQL(rdbStore, updateSql);
     CHECK_AND_PRINT_LOG(erroCode >= 0, "execute update continuous shooting photos, ret=%{public}d", erroCode);
