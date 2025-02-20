020fcfcb4ca66351f3a3f61a366cbace3c203f6a
fengshiyi <fengyang44@huawei.com>
2025-02-20 16:58:17 +0800

    push
    Signed-off-by: fengshiyi <fengyang44@huawei.com>

--- frameworks/services/media_backup_extension/src/backup_database_utils.cpp
+++ frameworks/services/media_backup_extension/src/backup_database_utils.cpp
@@ -1012,7 +1012,7 @@ std::shared_ptr<NativeRdb::ResultSet> BackupDatabaseUtils::QuerySql(
 void BackupDatabaseUtils::UpdateBurstPhotos(const std::shared_ptr<NativeRdb::RdbStore> &rdbStore)
 {
     const string updateSql =
-        "UPDATE " + PhotoColumn::PHOTOS_TABLE + " SET " + PhotoColumn::PHOTO_BURST_COVER_LEVEL + " = 1," + 
+        "UPDATE " + PhotoColumn::PHOTOS_TABLE + " SET " + PhotoColumn::PHOTO_BURST_COVER_LEVEL + " = 1," +
         PhotoColumn::PHOTO_BURST_KEY + " = NULL WHERE " + SQL_SELECT_ERROR_BURST_PHOTOS +
         "AND file_id IN (" + SQL_SELECT_CLONE_FILE_IDS + ")";
     int32_t erroCode = BackupDatabaseUtils::ExecuteSQL(rdbStore, updateSql);
