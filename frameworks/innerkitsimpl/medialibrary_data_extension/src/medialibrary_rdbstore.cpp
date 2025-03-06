963927bb3e17302392ce347db33df62ddac5604f
jiamingyun123 <jiamingyun1@huawei.com>
2025-03-06 10:12:33 +0000

    update frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp.
    Signed-off-by: jiamingyun123 <jiamingyun1@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdbstore.cpp
@@ -471,8 +471,8 @@ void MediaLibraryRdbStore::UpdateLocationKnowledgeIdx(const shared_ptr<MediaLibr
 {
     MEDIA_INFO_LOG("start update location knowledg index");
     const vector<string> sqls = {
-        PhotoColumn::DROP_LOCATION_KNOWLEDGE_INDEX,
-        PhotoColumn::ADD_LOCATION_KNOWLEDGE_INDEX,
+        DROP_KNOWLEDGE_INDEX,
+        CREATE_NEW_KNOWLEDGE_INDEX,
     };
     ExecSqls(sqls, *store->GetRaw().get());
     MEDIA_INFO_LOG("end update location knowledg index");
