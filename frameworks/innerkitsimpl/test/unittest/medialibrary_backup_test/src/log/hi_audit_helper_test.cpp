8768e8784eca1fe5dced5e91bfa4173b5d3c450b
yangyang <yangyang581@huawei.com>
2025-03-19 10:09:34 +0800

    backup/log tdd
    Signed-off-by: yangyang <yangyang581@huawei.com>
    Change-Id: I61116f4a7e696353a6f1fe29c8b57839e5c3c66d

---
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_backup_test/src/log/hi_audit_helper_test.cpp
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "hi_audit_helper_test.h"
+#include "media_backup_report_data_type.h"
+#include "media_log.h"
+
+#define private public
+#include "backup_hi_audit_helper.h"
+
+using namespace std;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Media {
+void HiAuditHelperTest::SetUpTestCase(void) {}
+
+void HiAuditHelperTest::TearDownTestCase(void) {}
+
+void HiAuditHelperTest::SetUp(void) {}
+
+void HiAuditHelperTest::TearDown(void) {}
+
+HWTEST_F(HiAuditHelperTest, Write_Error_Audit_Log_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Write_Error_Audit_Log_Test_001 begin");
+    BackupHiAuditHelper helper;
+    ErrorInfo info(0, 1, "failed", "test error");
+
+    helper.WriteErrorAuditLog(info);
+    EXPECT_TRUE(true);
+    MEDIA_INFO_LOG("Write_Error_Audit_Log_Test_001 end");
+}
+
+HWTEST_F(HiAuditHelperTest, Write_Progress_Audit_Log_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Write_Progress_Audit_Log_Test_001 begin");
+    BackupHiAuditHelper helper;
+    std::string status = "50%";
+    std::string extend = "progress info";
+
+    helper.WriteProgressAuditLog(status, extend);
+    EXPECT_TRUE(true);
+    MEDIA_INFO_LOG("Write_Progress_Audit_Log_Test_001 end");
+}
+
+HWTEST_F(HiAuditHelperTest, Write_Report_Audit_Log_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Write_Report_Audit_Log_Test_001 begin");
+    BackupHiAuditHelper helper;
+    std::string extend = "report info";
+
+    helper.WriteReportAuditLog(extend);
+    EXPECT_TRUE(true);
+    MEDIA_INFO_LOG("Write_Report_Audit_Log_Test_001 end");
+}
+
+HWTEST_F(HiAuditHelperTest, Set_Basic_Audit_Log_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Set_Basic_Audit_Log_Test_001 begin");
+    BackupHiAuditHelper helper;
+    BackupAuditLog auditLog;
+    std::string extend = "basic info";
+
+    helper.SetBasicAuditLog(auditLog, extend);
+    EXPECT_FALSE(auditLog.extend.empty());
+    MEDIA_INFO_LOG("Set_Basic_Audit_Log_Test_001 end");
+}
+
+HWTEST_F(HiAuditHelperTest, Set_Error_Audit_Log_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Set_Error_Audit_Log_Test_001 begin");
+    BackupHiAuditHelper helper;
+    BackupAuditLog auditLog;
+    ErrorInfo info(-1, 0, "failed", "test error");
+
+    helper.SetErrorAuditLog(auditLog, info);
+    EXPECT_EQ(auditLog.operationType, "RESTORE");
+
+    MEDIA_INFO_LOG("Set_Error_Audit_Log_Test_001 end");
+}
+} // namespace Media
+} // namespace OHOS
---
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_backup_test/src/log/zip_util_test.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "zip_util_test.h"
+
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <filesystem>
+
+#include <iostream>
+#include "media_log.h"
+#include <fstream>
+#define private public
+#include "backup_zip_util.h"
+
+using namespace std;
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Media {
+void ZipUtilTest::SetUpTestCase(void) {}
+
+void ZipUtilTest::TearDownTestCase(void) {}
+
+void ZipUtilTest::SetUp(void) {}
+
+void ZipUtilTest::TearDown(void) {}
+
+HWTEST_F(ZipUtilTest, Create_Zip_Test_001, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Create_Zip_Test_001 begin");
+    std::string filePath = "/data/storage/el2/log/test.zip";
+    zipFile compressZip = BackupZipUtil::CreateZipFile(filePath, 0);
+    ASSERT_NE(compressZip, nullptr);
+    BackupZipUtil::CloseZipFile(compressZip);
+    MEDIA_INFO_LOG("Create_Zip_Test_001 end");
+}
+
+HWTEST_F(ZipUtilTest, Create_Zip_Test_002, testing::ext::TestSize.Level0)
+{
+    MEDIA_INFO_LOG("Create_Zip_Test_002 begin");
+    std::string filePath = "/data/storage/el2/log/test.csv";
+    std::ofstream file(filePath);
+    ASSERT_EQ(file.is_open(), true);
+    file.close();
+    std::string zipFilePath = "/data/storage/el2/log/test.zip";
+    zipFile compressZip = BackupZipUtil::CreateZipFile(zipFilePath);
+    ASSERT_NE(compressZip, nullptr);
+    int ret = BackupZipUtil::AddFileInZip(compressZip, filePath, KEEP_NONE_PARENT_PATH);
+    EXPECT_EQ(ret, 0);
+    BackupZipUtil::CloseZipFile(compressZip);
+    MEDIA_INFO_LOG("Create_Zip_Test_002 end");
+}
+} // namespace Media
+} // namespace OHOS
