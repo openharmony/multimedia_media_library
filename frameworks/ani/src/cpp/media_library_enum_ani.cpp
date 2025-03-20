438996ebfe779c9e962e34e22c4f975ca2416a1d
liujia178 <liujia178@huawei.com>
2025-03-20 11:49:17 +0800

    Modify for get and set enum item
    Signed-off-by: liujia178 <liujia178@huawei.com>

--- frameworks/ani/src/cpp/media_library_enum_ani.cpp
+++ frameworks/ani/src/cpp/media_library_enum_ani.cpp
@@ -20,45 +20,6 @@
 
 namespace OHOS {
 namespace Media {
-
-static const std::map<EnumTypeInt32, std::string> ENUM_TYPE_MAP_INT32 = {
-    {EnumTypeInt32::PhotoTypeAni, ANI_CLASS_ENUM_PHOTO_TYPE},
-    {EnumTypeInt32::PhotoSubtypeAni, ANI_CLASS_ENUM_PHOTO_SUBTYPE},
-    {EnumTypeInt32::DynamicRangeTypeAni, ANI_CLASS_ENUM_DYNAMIC_RANGE_TYPE},
-    {EnumTypeInt32::PositionTypeAni, ANI_CLASS_ENUM_POSITION_TYPE},
-    {EnumTypeInt32::AnalysisTypeAni, ANI_CLASS_ENUM_ANALYSIS_TYPE},
-    {EnumTypeInt32::RecommendationTypeAni, ANI_CLASS_ENUM_RECOMMENDATION_TYPE},
-    {EnumTypeInt32::DeliveryModeAni, ANI_CLASS_ENUM_DELIVERY_MODE},
-    {EnumTypeInt32::CompatibleModeAni, ANI_CLASS_ENUM_COMPATIBLE_MODE},
-    {EnumTypeInt32::SourceModeAni, ANI_CLASS_ENUM_SOURCE_MODE},
-    {EnumTypeInt32::PhotoPermissionTypeAni, ANI_CLASS_ENUM_PHOTO_PERMISSION_TYPE},
-    {EnumTypeInt32::HideSensitiveTypeAni, ANI_CLASS_ENUM_HIDE_SENSITIVE_TYPE},
-    {EnumTypeInt32::AuthorizationModeAni, ANI_CLASS_ENUM_AUTHORIZATION_MODE},
-    {EnumTypeInt32::CompleteButtonTextAni, ANI_CLASS_ENUM_COMPLETE_BUTTON_TEXT},
-    {EnumTypeInt32::WatermarkTypeAni, ANI_CLASS_ENUM_WATERMARK_TYPE},
-    {EnumTypeInt32::HiddenPhotosDisplayModeAni, ANI_CLASS_ENUM_HIDDEN_PHOTOS_DISPLAY_MODE},
-    {EnumTypeInt32::AlbumTypeAni, ANI_CLASS_ENUM_ALBUM_TYPE},
-    {EnumTypeInt32::AlbumSubtypeAni, ANI_CLASS_ENUM_ALBUM_SUBTYPE},
-    {EnumTypeInt32::RequestPhotoTypeAni, ANI_CLASS_ENUM_REQUEST_PHOTO_TYPE},
-    {EnumTypeInt32::NotifyTypeAni, ANI_CLASS_ENUM_NOTIFY_TYPE},
-    {EnumTypeInt32::ResourceTypeAni, ANI_CLASS_ENUM_RESOURCE_TYPE},
-    {EnumTypeInt32::ImageFileTypeAni, ANI_CLASS_ENUM_IMAGE_FILE_TYPE},
-    {EnumTypeInt32::MovingPhotoEffectModeAni, ANI_CLASS_ENUM_MOVING_PHOTO_EFFECT_MODE},
-    {EnumTypeInt32::VideoEnhancementTypeAni, ANI_CLASS_ENUM_VIDEO_ENHANCEMENT_TYPE},
-    {EnumTypeInt32::HighlightAlbumInfoTypeAni, ANI_CLASS_ENUM_HIGHLIGHT_ALBUM_INFO_TYPE},
-    {EnumTypeInt32::HighlightUserActionTypeAni, ANI_CLASS_ENUM_HIGHLIGHT_USER_ACTION_TYPE},
-    {EnumTypeInt32::ThumbnailTypeAni, ANI_CLASS_ENUM_THUMBNAIL_TYPE},
-    {EnumTypeInt32::CloudEnhancementTaskStageAni, ANI_CLASS_ENUM_CLOUD_ENHANCEMENT_TASK_STAGE},
-    {EnumTypeInt32::CloudEnhancementStateAni, ANI_CLASS_ENUM_CLOUD_ENHANCEMENT_STATE},
-};
-
-static const std::map<EnumTypeString, std::string> ENUM_TYPE_MAP_STRING = {
-    {EnumTypeString::PhotoKeysAni, ANI_CLASS_ENUM_PHOTO_KEYS},
-    {EnumTypeString::AlbumKeysAni, ANI_CLASS_ENUM_ALBUM_KEYS},
-    {EnumTypeString::DefaultChangeUriAni, ANI_CLASS_ENUM_DEFAULT_CHANGE_URI},
-    {EnumTypeString::PhotoViewMIMETypesAni, ANI_CLASS_ENUM_PHOTO_VIEW_MIME_TYPES},
-};
-
 static const std::map<MediaType, int32_t> ANI_MEDIATYPE_INDEX_MAP = {
     {MediaType::MEDIA_TYPE_IMAGE, 0},
     {MediaType::MEDIA_TYPE_VIDEO, 1},
@@ -137,16 +98,9 @@ static const std::map<CloudEnhancementTaskStage, int32_t> ANI_CLOUDENHANCEMENTTA
     {CloudEnhancementTaskStage::TASK_STAGE_COMPLETED, 6},
 };
 
-ani_status MediaLibraryEnumAni::EnumGetValueInt32(ani_env *env, EnumTypeInt32 enumType, ani_enum_item enumItem,
-    int32_t &value)
+ani_status MediaLibraryEnumAni::EnumGetValueInt32(ani_env *env, ani_enum_item enumItem, int32_t &value)
 {
     CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
-
-    auto it = ENUM_TYPE_MAP_INT32.find(enumType);
-    CHECK_COND_RET(it != ENUM_TYPE_MAP_INT32.end(), ANI_INVALID_ARGS, "Invalid enumType: %{public}d", enumType);
-
-    ani_enum aniEnum {};
-    CHECK_STATUS_RET(env->FindEnum(it->second.c_str(), &aniEnum), "FindEnum failed");
 
     ani_int aniInt {};
     CHECK_STATUS_RET(env->EnumItem_GetValue_Int(enumItem, &aniInt), "EnumItem_GetValue_Int failed");
@@ -154,16 +108,9 @@ ani_status MediaLibraryEnumAni::EnumGetValueInt32(ani_env *env, EnumTypeInt32 en
     return ANI_OK;
 }
 
-ani_status MediaLibraryEnumAni::EnumGetValueString(ani_env *env, EnumTypeString enumType, ani_enum_item enumItem,
-    std::string &value)
+ani_status MediaLibraryEnumAni::EnumGetValueString(ani_env *env, ani_enum_item enumItem, std::string &value)
 {
     CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
-
-    auto it = ENUM_TYPE_MAP_STRING.find(enumType);
-    CHECK_COND_RET(it != ENUM_TYPE_MAP_STRING.end(), ANI_INVALID_ARGS, "Invalid enumType: %{public}d", enumType);
-
-    ani_enum aniEnum {};
-    CHECK_STATUS_RET(env->FindEnum(it->second.c_str(), &aniEnum), "FindEnum failed");
 
     ani_string aniString {};
     CHECK_STATUS_RET(env->EnumItem_GetValue_String(enumItem, &aniString), "EnumItem_GetValue_String failed");
@@ -171,76 +118,132 @@ ani_status MediaLibraryEnumAni::EnumGetValueString(ani_env *env, EnumTypeString 
     return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(MediaType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, MediaType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_MEDIATYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_MEDIATYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_MEDIATYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_PHOTO_TYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(PhotoSubType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, PhotoSubType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_PHOTOSUBTYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_PHOTOSUBTYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_PHOTOSUBTYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_PHOTO_SUBTYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(DynamicRangeType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, DynamicRangeType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_DYNAMICRANGETYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_DYNAMICRANGETYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_DYNAMICRANGETYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_DYNAMIC_RANGE_TYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(PhotoPositionType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, PhotoPositionType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_PHOTOPOSITIONTYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_PHOTOPOSITIONTYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_PHOTOPOSITIONTYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_POSITION_TYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(PhotoAlbumType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, PhotoAlbumType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_PHOTOALBUMTYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_PHOTOALBUMTYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_PHOTOALBUMTYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_ALBUM_TYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(PhotoAlbumSubType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, PhotoAlbumSubType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_PHOTOALBUMSUBTYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_PHOTOALBUMSUBTYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_PHOTOALBUMSUBTYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_ALBUM_SUBTYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(NotifyType value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, NotifyType value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_NOTIFYTYPE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_NOTIFYTYPE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_NOTIFYTYPE_INDEX_MAP.end(), ANI_INVALID_ARGS, "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_NOTIFY_TYPE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(MovingPhotoEffectMode value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, MovingPhotoEffectMode value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_MOVINGPHOTOEFFECTMODE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_MOVINGPHOTOEFFECTMODE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_MOVINGPHOTOEFFECTMODE_INDEX_MAP.end(), ANI_INVALID_ARGS,
+        "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_MOVING_PHOTO_EFFECT_MODE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
-bool MediaLibraryEnumAni::EnumGetIndex(CloudEnhancementTaskStage value, ani_int &enumIndex)
+ani_status MediaLibraryEnumAni::ToAniEnum(ani_env *env, CloudEnhancementTaskStage value, ani_enum_item &aniEnumItem)
 {
+    CHECK_COND_RET(env != nullptr, ANI_INVALID_ARGS, "Invalid env");
+
     auto it = ANI_CLOUDENHANCEMENTTASKSTAGE_INDEX_MAP.find(value);
-    CHECK_COND_RET(it != ANI_CLOUDENHANCEMENTTASKSTAGE_INDEX_MAP.end(), false, "Unsupport enum: %{public}d", value);
-    enumIndex = static_cast<ani_int>(it->second);
-    return true;
+    CHECK_COND_RET(it != ANI_CLOUDENHANCEMENTTASKSTAGE_INDEX_MAP.end(), ANI_INVALID_ARGS,
+        "Unsupport enum: %{public}d", value);
+    ani_int enumIndex = static_cast<ani_int>(it->second);
+
+    ani_enum aniEnum {};
+    CHECK_STATUS_RET(env->FindEnum(ANI_CLASS_ENUM_MOVING_PHOTO_EFFECT_MODE.c_str(), &aniEnum), "Find Enum Fail");
+    CHECK_STATUS_RET(env->Enum_GetEnumItemByIndex(aniEnum, enumIndex, &aniEnumItem), "Find Enum item Fail");
+    return ANI_OK;
 }
 
 } // namespace Media
