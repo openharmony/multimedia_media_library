383843e7010064d9b0874ce04e1c2a802d85e28f
zhaochang14 <zhaochang14@huawei.com>
2025-03-22 19:24:31 +0800

    zhaochang14@huawei.com
    Signed-off-by: zhaochang14 <zhaochang14@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/medialibrary_album_operation_test/src/albums_refresh_manager_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_album_operation_test/src/albums_refresh_manager_test.cpp
@@ -245,6 +245,7 @@ HWTEST_F(AlbumsRefreshManagerTest, GetSyncNotifyInfo_Notify_INVALID, TestSize.Le
 {
     MEDIA_INFO_LOG("Start GetSyncNotifyInfo_Notify_INVALID");
     CloudSyncNotifyInfo notifyInfo;
+    notifyInfo.type = ChangeType::INVAILD;
     NotifyType notifyType = AlbumsRefreshManager::GetInstance()
                                                 .GetSyncNotifyInfo(notifyInfo, OTHER_URI_TYPE)
                                                 .notifyType;
