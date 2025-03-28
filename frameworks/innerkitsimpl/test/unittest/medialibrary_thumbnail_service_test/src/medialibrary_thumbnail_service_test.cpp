530fb9a93e2c6da82fbb241dbe6cfe60e2f7e911
zengguangquan <zengguangquan@h-partners.com>
2025-03-28 15:47:31 +0800

    超rom相关用例修改
    Signed-off-by: zengguangquan <zengguangquan@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/src/medialibrary_thumbnail_service_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/src/medialibrary_thumbnail_service_test.cpp
@@ -228,7 +228,7 @@ HWTEST_F(MediaLibraryThumbnailServiceTest, medialib_GenerateThumbnailBackground_
     }
     shared_ptr<ThumbnailService> serverTest = ThumbnailService::GetInstance();
     int32_t ret = serverTest->GenerateThumbnailBackground();
-    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(ret <= 0, true);
     shared_ptr<DistributedKv::SingleKvStore> kvStorePtr = make_shared<MockSingleKvStore>();
     shared_ptr<OHOS::AbilityRuntime::Context> context;
 #ifdef DISTRIBUTED
@@ -237,14 +237,14 @@ HWTEST_F(MediaLibraryThumbnailServiceTest, medialib_GenerateThumbnailBackground_
     serverTest->Init(nullptr, context);
 #endif
     ret = serverTest->GenerateThumbnailBackground();
-    EXPECT_EQ(ret, -1);
+    EXPECT_NE(ret, 0);
 #ifdef DISTRIBUTED
     serverTest->Init(storePtr, kvStorePtr, context);
 #else
     serverTest->Init(storePtr, context);
 #endif
     ret = serverTest->GenerateThumbnailBackground();
-    EXPECT_EQ(ret, 0);
+    EXPECT_EQ(ret <= 0, true);
     serverTest->ReleaseService();
 }
 
@@ -947,7 +947,7 @@ HWTEST_F(MediaLibraryThumbnailServiceTest, thumbnail_generate_helper_test_002, T
 {
     ThumbRdbOpt opts;
     auto res = ThumbnailGenerateHelper::UpgradeThumbnailBackground(opts, false);
-    EXPECT_EQ(res, -1);
+    EXPECT_NE(res, 0);
 }
 
 HWTEST_F(MediaLibraryThumbnailServiceTest, thumbnail_generate_helper_test_003, TestSize.Level0)
