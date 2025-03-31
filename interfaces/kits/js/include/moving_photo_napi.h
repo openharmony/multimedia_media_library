484659e341cfd4278700ff295b7b719743fddf78
caojiajun <caojiajun6@huawei.com>
2025-04-01 01:13:07 +0800

    support reading cloud moving photo
    Signed-off-by: caojiajun <caojiajun6@huawei.com>
    Change-Id: Ieead11a48740a16973635fb6ad670de965f54b59

--- interfaces/kits/js/include/moving_photo_napi.h
+++ interfaces/kits/js/include/moving_photo_napi.h
@@ -80,6 +80,7 @@ public:
     static void OnProgress(napi_env env, napi_value cb, void *context, void *data);
     static int32_t GetFdFromUri(const std::string &sandBoxUri);
     static void SubRequestContent(int32_t fd, MovingPhotoAsyncContext* context);
+    static void RequestCloudContentArrayBuffer(int32_t fd, MovingPhotoAsyncContext* context);
     static void CallRequestContentCallBack(napi_env env, MovingPhotoAsyncContext* context);
 private:
     EXPORT static napi_value Constructor(napi_env env, napi_callback_info info);
