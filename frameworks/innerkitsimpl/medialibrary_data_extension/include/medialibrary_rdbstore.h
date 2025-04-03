0fedd5ab965b1cc15f726f94a79f7a6fedf615cc
jiamingyun123 <jiamingyun1@huawei.com>
2025-04-03 02:15:12 +0000

    update frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_rdbstore.h.
    Signed-off-by: jiamingyun123 <jiamingyun1@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_rdbstore.h
+++ frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_rdbstore.h
@@ -102,6 +102,7 @@ public:
     EXPORT static void RevertFixDateAddedIndex(const std::shared_ptr<MediaLibraryRdbStore> store);
     EXPORT static void AddAlbumIndex(const std::shared_ptr<MediaLibraryRdbStore> store);
     EXPORT static void UpdateLocationKnowledgeIdx(const std::shared_ptr<MediaLibraryRdbStore> store);
+    EXPORT static void AddAlbumSubtypeAndNameldx(const std::shared_ptr<MediaLibraryRdbStore> store);
     EXPORT static void AddCloudEnhancementAlbumIndex(const std::shared_ptr<MediaLibraryRdbStore> store);
     EXPORT static void AddPhotoDateAddedIndex(const std::shared_ptr<MediaLibraryRdbStore> store);
     EXPORT static void UpdateLatitudeAndLongitudeDefaultNull(const std::shared_ptr<MediaLibraryRdbStore> store);
