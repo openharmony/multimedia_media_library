c18b3efeab852e73d391abd180ef721501e46f0b
Meisy <liusijie20@huawei.com>
2025-04-07 13:46:17 +0000

    update frameworks/innerkitsimpl/medialibrary_data_extension/src/picture_data_operations.cpp
    Signed-off-by: Meisy <liusijie20@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/picture_data_operations.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/picture_data_operations.cpp
@@ -45,23 +45,23 @@ PictureDataOperations::~PictureDataOperations()
     highQualityPictureImageId.clear();
 }
 
-static std::pair<bool, bool> IsPictureTempAndEdited(const string &photoId)
+static int32_t IsPictureTempAndEdited(const string &photoId, bool &isTemp, bool &isEdited)
 {
     MediaLibraryTracer tracer;
     tracer.Start("IsPictureTempAndEdited " + photoId);
-    CHECK_AND_RETURN_RET_LOG(MediaLibraryDataManagerUtils::IsNumber(photoId), std::make_pair(false, false),
+    CHECK_AND_RETURN_RET_LOG(MediaLibraryDataManagerUtils::IsNumber(photoId), E_ERR,
         "photoId is invalid");
     auto rdbStore = MediaLibraryUnistoreManager::GetInstance().GetRdbStore();
-    CHECK_AND_RETURN_RET_LOG(rdbStore != nullptr, std::make_pair(false, false), "Failed to get rdbStore");
+    CHECK_AND_RETURN_RET_LOG(rdbStore != nullptr, E_ERR, "Failed to get rdbStore");
     NativeRdb::AbsRdbPredicates predicates(PhotoColumn::PHOTOS_TABLE);
     predicates.EqualTo(PhotoColumn::PHOTO_ID, photoId);
     vector<string> columns { PhotoColumn::PHOTO_IS_TEMP, PhotoColumn::PHOTO_EDIT_TIME };
     auto resultSet = rdbStore->Query(predicates, columns);
     CHECK_AND_RETURN_RET_LOG(resultSet != nullptr && resultSet->GoToFirstRow() == NativeRdb::E_OK,
-        std::make_pair(false, false), "resultSet is empty");
-    bool isTemp = (GetInt32Val(PhotoColumn::PHOTO_IS_TEMP, resultSet) == 1);
-    bool isEdited = (GetInt64Val(PhotoColumn::PHOTO_EDIT_TIME, resultSet) > 0);
-    return std::make_pair(isTemp, isEdited);
+        E_ERR, "resultSet is empty");
+    isTemp = (GetInt32Val(PhotoColumn::PHOTO_IS_TEMP, resultSet) == 1);
+    isEdited = (GetInt64Val(PhotoColumn::PHOTO_EDIT_TIME, resultSet) > 0);
+    return E_OK;
 }
 
 void PictureDataOperations::CleanPictureMapData(std::map<std::string, sptr<PicturePair>>& pictureMap,
@@ -74,13 +74,14 @@ void PictureDataOperations::CleanPictureMapData(std::map<std::string, sptr<Pictu
         time_t now = time(nullptr);
         bool isNeedDeletePicture = ((iter->second)->expireTime_ < now) && ((iter->second)->isCleanImmediately_);
         if (isNeedDeletePicture || ((iter->second)->expireTime_ + SAVE_PICTURE_TIMEOUT_SEC) < now) {
-            std::pair<bool, bool> result = IsPictureTempAndEdited(iter->first);
+            bool isTemp = false;
+            bool isEdited = false;
+            IsPictureTempAndEdited(iter->first, isTemp, isEdited);
             bool isLowQualityPicture = (pictureType != HIGH_QUALITY_PICTURE);
-            if (result.first) {
-                FileUtils::SavePicture(iter->first, (iter->second)->picture_,
-                    result.second, isLowQualityPicture);
+            if (isTemp) {
+                FileUtils::SavePicture(iter->first, (iter->second)->picture_, isEdited, isLowQualityPicture);
                 MEDIA_INFO_LOG("end SavePicture, photoId: %{public}s, isEdited: %{public}d",
-                    (iter->first).c_str(), static_cast<int32_t>(result.second));
+                    (iter->first).c_str(), static_cast<int32_t>(isEdited));
             }
             MEDIA_INFO_LOG("enter CleanDateByPictureMap %{public}s enter", (iter->first).c_str());
             iter->second = nullptr;
@@ -171,12 +172,13 @@ void PictureDataOperations::CleanHighQualityPictureDataInternal(const std::strin
         std::string imageId = *iter;
         std::map<std::string, sptr<PicturePair>>::iterator iterPicture = highQualityPictureMap_.find(imageId);
         if (iterPicture != highQualityPictureMap_.end() && (iterPicture->second)->isCleanImmediately_) {
-            std::pair<bool, bool> result = IsPictureTempAndEdited(iterPicture->first);
-            if (result.first) {
-                FileUtils::SavePicture(iterPicture->first, (iterPicture->second)->picture_,
-                    result.second, false);
+            bool isTemp = false;
+            bool isEdited = false;
+            IsPictureTempAndEdited(iterPicture->first, isTemp, isEdited);
+            if (isTemp) {
+                FileUtils::SavePicture(iterPicture->first, (iterPicture->second)->picture_, isEdited, false);
                 MEDIA_INFO_LOG("end SavePicture, photoId: %{public}s, isEdited: %{public}d",
-                    (iterPicture->first).c_str(), static_cast<int32_t>(result.second));
+                    (iterPicture->first).c_str(), static_cast<int32_t>(isEdited));
             }
             highQualityPictureMap_.erase(iterPicture);
             iter = pictureImageIdList.erase(iter);
