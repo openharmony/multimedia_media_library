032e55c26179ed98766910fc0c48bdd0785d633e
jiamingyun123 <jiamingyun1@huawei.com>
2025-04-07 01:47:27 +0000

    update frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdb_utils.cpp.
    Signed-off-by: jiamingyun123 <jiamingyun1@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdb_utils.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_rdb_utils.cpp
@@ -983,8 +983,8 @@ static shared_ptr<ResultSet> QueryPortraitAlbumCover(const shared_ptr<MediaLibra
         "AND Photos.time_pending = 0 "
         "AND Photos.is_temp = 0 "
         "AND Photos.burst_cover_level = 1 "
-        "AND AnalysisAlbum.album_id IN (SELECT album_id FROM AnalysisAlbum where AnalysisAlbum.group_tag \
-        IN (SELECT group_tag FROM AnalysisAlbum WHERE album_id = " +
+        "AND AnalysisAlbum.album_id IN (SELECT album_id FROM AnalysisAlbum where AnalysisAlbum.group_tag " +
+        "IN (SELECT group_tag FROM AnalysisAlbum WHERE album_id = " +
         albumId +
         " LIMIT 1) )";
     predicates.SetWhereClause(clause);
