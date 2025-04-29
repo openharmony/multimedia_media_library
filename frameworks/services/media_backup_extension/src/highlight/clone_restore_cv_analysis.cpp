3a4ab7e43de39b641e684036b35a6100c8ccb0d4
tianminz <zhangtianmin2@huawei.com>
2025-04-29 10:46:10 +0800

    limit album_asset
    Signed-off-by: tianminz <zhangtianmin2@huawei.com>
    Change-Id: I5ee31d0df9026f7739c63176a2a18760d716b4ee

--- frameworks/services/media_backup_extension/src/highlight/clone_restore_cv_analysis.cpp
+++ frameworks/services/media_backup_extension/src/highlight/clone_restore_cv_analysis.cpp
@@ -183,7 +183,9 @@ int32_t CloneRestoreCVAnalysis::GetNewAssetId(int32_t assetId)
 
 void CloneRestoreCVAnalysis::GetAssetAlbumInfos(CloneRestoreHighlight &cloneHighlight)
 {
-    const std::string QUERY_SQL = "SELECT * FROM tab_analysis_album_asset_map LIMIT ?, ?";
+    const std::string QUERY_SQL = "SELECT tab_analysis_album_asset_map.* FROM tab_analysis_album_asset_map "
+        " INNER JOIN tab_highlight_album AS h ON tab_analysis_album_asset_map.map_album = h.id "
+        " WHERE h.highlight_status > 0 LIMIT ?, ?";
     int32_t rowCount = 0;
     int32_t offset = 0;
     do {
