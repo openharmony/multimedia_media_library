b1e4266db720c6b441a455180e3f5452bffcd6d3
zengguangquan <zengguangquan@h-partners.com>
2025-04-30 11:51:43 +0800

    thumbnail部分蓝黄差异同步
    Signed-off-by: zengguangquan <zengguangquan@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/include/medialibrary_ithumbnail_helper_test.h
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/include/medialibrary_ithumbnail_helper_test.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-#ifndef MEDIALIBRARY_EXT_UNIT_TEST_H
-#define MEDIALIBRARY_EXT_UNIT_TEST_H
+#ifndef MEDIALIBRARY_ITHUMBNAIL_HELPER_TEST_H
+#define MEDIALIBRARY_ITHUMBNAIL_HELPER_TEST_H
 
 #include <gtest/gtest.h>
  
@@ -29,4 +29,4 @@ public:
 };
 } // namespace Media
 } // namespace OHOS
-#endif // MEDIALIBRARY_EXT_UNIT_TEST_H
+#endif // MEDIALIBRARY_ITHUMBNAIL_HELPER_TEST_H
