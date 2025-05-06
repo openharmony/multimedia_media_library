6dca99a84969045d862f732d222d9a129e1414ac
wangyifei <wangyifei54@huawei.com>
2025-05-06 10:53:07 +0800

    fix
    Signed-off-by: wangyifei <wangyifei54@huawei.com>

--- frameworks/js/src/sendable/sendable_file_asset_napi.cpp
+++ frameworks/js/src/sendable/sendable_file_asset_napi.cpp
@@ -917,6 +917,7 @@ static int32_t CheckSystemApiKeys(napi_env env, const string &key)
         PhotoColumn::PHOTO_ORIGINAL_SUBTYPE,
         PENDING_STATUS,
         MEDIA_DATA_DB_DATE_TRASHED_MS,
+        MEDIA_SUM_SIZE,
     };
 
     if (SYSTEM_API_KEYS.find(key) != SYSTEM_API_KEYS.end() && !SendableMediaLibraryNapiUtils::IsSystemApp()) {
