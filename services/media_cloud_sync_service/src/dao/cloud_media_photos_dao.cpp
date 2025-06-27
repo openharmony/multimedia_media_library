166c6c8d8f4edd6b910107af38a241dc6a9e1b0a
yaozihao <yaozihao2@h-partners.com>
2025-06-27 11:19:22 +0800

    新增上行成功后，无法更新已有cloud_id的资产状态
    Signed-off-by: yaozihao <yaozihao2@h-partners.com>

--- services/media_cloud_sync_service/src/dao/cloud_media_photos_dao.cpp
+++ services/media_cloud_sync_service/src/dao/cloud_media_photos_dao.cpp
@@ -1284,9 +1284,8 @@ int32_t CloudMediaPhotosDao::UpdatePhotoCreatedRecord(
     }
 
     int32_t changedRows;
-    std::string whereClause = "file_id = ? AND dirty = ? AND COALESCE(cloud_id, '') <> ?";
-    std::vector<std::string> whereArgs = {
-        fileId, std::to_string(static_cast<int32_t>(DirtyType::TYPE_NEW)), record.cloudId};
+    std::string whereClause = PhotoColumn::MEDIA_ID + " = ? AND " + PhotoColumn::PHOTO_DIRTY + " = ?";
+    std::vector<std::string> whereArgs = {fileId, std::to_string(static_cast<int32_t>(DirtyType::TYPE_NEW))};
     int32_t ret = photoRefresh->Update(changedRows, PhotoColumn::PHOTOS_TABLE, valuesBucket, whereClause, whereArgs);
     MEDIA_INFO_LOG("UpdatePhotoCreatedRecord ret:%{public}d, changedRows:%{public}d, cloudId:%{public}s",
         ret,
