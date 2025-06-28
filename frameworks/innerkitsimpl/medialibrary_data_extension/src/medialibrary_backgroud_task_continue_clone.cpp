f59b9d2bf62ce80d36453d9e96ae4ba1397ccc21
tianminz <zhangtianmin2@huawei.com>
2025-06-28 19:13:10 +0800

    support json in clone folder
    Signed-off-by: tianminz <zhangtianmin2@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_backgroud_task_continue_clone.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_backgroud_task_continue_clone.cpp
@@ -32,8 +32,7 @@ namespace OHOS {
 namespace Media {
 const int32_t SECONDS_TO_MS = 1000;
 const int32_t MS_OF_ONE_DAY = 24 * 60 * 60 * 1000;
-static const std::string DATA_CLONE_DESCRIPTION_JSON =
-    PhotoColumn::FILES_LOCAL_DIR + ".backup/restore/dataclone_description.json";
+const static std::string DATA_CLONE_DESCRIPTION_JSON_FILE_NAME = "/dataclone_description.json";
 const static std::string CLONE_TIME_STAMP = "cloneTimestamp";
 const static std::string CLONE_FOLDER_PATH = PhotoColumn::FILES_LOCAL_DIR + ".backup/clone";
 const static std::string RESTORE_FOLDER_PATH = PhotoColumn::FILES_LOCAL_DIR + ".backup/restore";
@@ -51,33 +50,34 @@ int32_t MedialibrarySubscriber::DoClearContinueCloneData()
     auto asyncWorker = MediaLibraryAsyncWorker::GetInstance();
     CHECK_AND_RETURN_RET_LOG(asyncWorker != nullptr, E_FAIL,
         "Failed to get async worker instance!");
-    shared_ptr<MediaLibraryAsyncTask> ClearContinueCloneTask =
+    shared_ptr<MediaLibraryAsyncTask> clearContinueCloneTask =
         make_shared<MediaLibraryAsyncTask>(ClearContinueCloneData, nullptr);
-    CHECK_AND_RETURN_RET_LOG(ClearContinueCloneTask != nullptr, E_FAIL,
-        "Failed to create async task for ClearContinueCloneTask !");
-    asyncWorker->AddTask(ClearContinueCloneTask, false);
+    CHECK_AND_RETURN_RET_LOG(clearContinueCloneTask != nullptr, E_FAIL,
+        "Failed to create async task for clearContinueCloneTask !");
+    asyncWorker->AddTask(clearContinueCloneTask, false);
     return E_SUCCESS;
 }
 
 bool MedialibrarySubscriber::IsClearContinueCloneData(const std::string &path)
 {
-    int64_t cloneTimestamp = MediaFileUtils::GetFileModificationTime(DATA_CLONE_DESCRIPTION_JSON) * SECONDS_TO_MS;
+    int64_t cloneTimestamp = MediaFileUtils::GetFileModificationTime(path) * SECONDS_TO_MS;
     MEDIA_INFO_LOG("IsClearContinueCloneData fileModificationTime:%{public}" PRId64, cloneTimestamp);
     int64_t curTime = MediaFileUtils::UTCTimeMilliSeconds();
     bool contentNotExistRet = curTime - cloneTimestamp > MS_OF_ONE_DAY;
     std::string content = "";
     bool isContentExist = LoadStringFromFile(path, content) && !content.empty();
-    CHECK_AND_RETURN_RET_LOG(isContentExist, contentNotExistRet, "file is empty %{private}s", path.c_str());
+    CHECK_AND_RETURN_RET_LOG(isContentExist, contentNotExistRet, "file is empty %{public}s",
+        MediaFileUtils::DesensitizePath(path).c_str());
     nlohmann::json jsonArray = nlohmann::json::parse(content, nullptr, false);
     CHECK_AND_RETURN_RET_LOG(!jsonArray.is_discarded(), contentNotExistRet, "json array is empty ");
-    for (auto& [key, value] : jsonArray.items()) {
+    for (auto &[key, value] : jsonArray.items()) {
         if (key == CLONE_TIME_STAMP && value.is_number_integer()) {
             cloneTimestamp = value.get<int64_t>();
         }
     }
     if (cloneTimestamp == 0) {
         MEDIA_INFO_LOG("not have clone time stamp key");
-        cloneTimestamp = MediaFileUtils::GetFileModificationTime(DATA_CLONE_DESCRIPTION_JSON) * SECONDS_TO_MS;
+        cloneTimestamp = MediaFileUtils::GetFileModificationTime(path) * SECONDS_TO_MS;
     }
     MEDIA_INFO_LOG("IsClearContinueCloneData cloneTimestamp:%{public}" PRId64, cloneTimestamp);
     return curTime - cloneTimestamp > MS_OF_ONE_DAY;
@@ -88,18 +88,24 @@ bool MedialibrarySubscriber::TryClearContinueCloneData()
     bool isDirExists =
         MediaFileUtils::IsDirExists(CLONE_FOLDER_PATH) || MediaFileUtils::IsDirExists(RESTORE_FOLDER_PATH);
     CHECK_AND_RETURN_RET_LOG(isDirExists, false, "clone foler not exist, return");
-    if (!MediaFileUtils::IsFileExists(DATA_CLONE_DESCRIPTION_JSON)) {
+    std::string path = GetDataCloneDescriptionJsonPath();
+    MEDIA_INFO_LOG("GetDataCloneDescriptionJsonPath: %{public}s", MediaFileUtils::DesensitizePath(path).c_str());
+    if (!MediaFileUtils::IsFileExists(path)) {
         int32_t ret = DoClearContinueCloneData();
         CHECK_AND_RETURN_RET_LOG(ret == E_SUCCESS, false, "DoUpdateDirtyForCloudClone failed");
         return true;
     }
-    CHECK_AND_RETURN_RET_LOG(IsClearContinueCloneData(DATA_CLONE_DESCRIPTION_JSON), false,
-        "not clean continue clone data");
+    CHECK_AND_RETURN_RET_LOG(IsClearContinueCloneData(path), false, "not clean continue clone data");
     int32_t taskRet = DoClearContinueCloneData();
     CHECK_AND_RETURN_RET_LOG(taskRet == E_SUCCESS, false, "DoUpdateDirtyForCloudClone failed");
     return true;
 }
 
-
+std::string MedialibrarySubscriber::GetDataCloneDescriptionJsonPath()
+{
+    std::string restorePath = RESTORE_FOLDER_PATH + DATA_CLONE_DESCRIPTION_JSON_FILE_NAME;
+    std::string clonePath = CLONE_FOLDER_PATH + DATA_CLONE_DESCRIPTION_JSON_FILE_NAME;
+    return MediaFileUtils::IsFileExists(restorePath) ? restorePath : clonePath;
+}
 }  // namespace Media
 }  // namespace OHOS
