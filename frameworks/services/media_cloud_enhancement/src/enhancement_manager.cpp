25ecced295e0d87044ef44cc05140107ecfb0bdf
l30003877 <liwei541@h-partners.com>
2025-07-25 16:11:19 +0800

    修复压测云增强cppcrash codecheck
    Signed-off-by: l30003877 <liwei541@h-partners.com>

--- frameworks/services/media_cloud_enhancement/src/enhancement_manager.cpp
+++ frameworks/services/media_cloud_enhancement/src/enhancement_manager.cpp
@@ -218,11 +218,8 @@ bool EnhancementManager::InitAsync()
 
 bool EnhancementManager::Init()
 {
-    static std::mutex m;
-    static int flag = 0;
-    std::lock_guard<std::mutex> lg(m);
-    if (flag) return true;
-    flag = 1;
+    static std::atomic<int> flag{0};
+    if (flag.exchange(1)) return true;
 #ifdef ABILITY_CLOUD_ENHANCEMENT_SUPPORT
     // restart
     CHECK_AND_RETURN_RET_LOG(LoadService(), false, "load enhancement service error");
