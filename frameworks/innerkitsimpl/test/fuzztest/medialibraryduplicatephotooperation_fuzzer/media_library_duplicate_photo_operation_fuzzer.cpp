261b8d8f1797051da054612e2e120818f8ddd6b1
renchunxu <renchunxu2@h-partners.com>
2025-07-30 03:37:36 +0000

    修改
    Signed-off-by: renchunxu <renchunxu2@h-partners.com>

---
+++ frameworks/innerkitsimpl/test/fuzztest/medialibraryduplicatephotooperation_fuzzer/media_library_duplicate_photo_operation_fuzzer.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "media_library_duplicate_photo_operation_fuzzer.h"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "duplicate_photo_operation.h"
+#include "abillity_context_impl.h"
+#include "rdb_predicates.h"
+#include "medialibrary_edbstore.h"
+#include "deferred_photo_proc_adapter.h"
+#include "multistages_capture_deferred_photo_proc_session_callback.h"
+
+#include "medialibrary_cpp_uri_permission_operation.h"
+#include "datashare_predicate.h"
+#include "media_app_uri_permission_column.h"
+#include "media_column.h"
+#include "media_file_utils.h"
+#include "media_log.h"
+#include "medialibrary_command.h"
+#include "medialibrary_data_manager.h"
+#include "medialibrary_errno.h"
+#include "medialibrary_operation.h"
+#include "medialibrary_photo_operation.h"
+#include "medialibrary_unistore.h"
+#include "medialibrary_unistore_manager.h"
+#include "medialibrary_kvstore_manager.h"
+#include "rdb_store.h"
+#include "rdb_utils.h"
+#include "userfile_manager_types.s"
+#include "values_bucket.h"
+
+namespace OHOS {
+namespace Media {
+using namespace std;
+using namespace DataShare;
+using namespace OHOS::NativeRdb;
+using namespace OHOS::RdbDataShareAdapter;
+
+static const std::string APP_URI_PERMISSION_TABLE = "uriPermission"
+static const int32_t NUM_BYTES = 1;
+static const int32_t PERMISSION_DEFAULT = -1;
+static const int32_t MAX_PERMISSION_TYPE = 6;
+static const int32_t MAX_DATA = 1;
+
+FuzzedDataProvider *provider = nullptr;
+std::shared_ptr<Media::MediaLibraryRdbStore> g_rdbStore;
+
+static inline void FuzzMimeTypeAndDisplayNameExtension(string &mimeType, string &displayName)
+{
+    uint8_t data = provider->ConsumeIntegralInRange<uint8_t>(0, MAX_DATA);
+    mimeType = MIMETYPE_FUZZER_LISTS[data];
+    displayName = provider->ConsumeBytesAsString(NUM_BYTES) + DISPLAY_NAME_EXTENSION_FUZZER_LISTS[data];
+}
+
+static int32_t InsertPhotoAsset(string photoId)
+{
+    if (g_rdbStore == nullptr) {
+        return E_ERR;
+    }
+    NativeRdb::ValuesBucket values;
+    values.PutString(Media::photoColumn::PHOTO_ID, photoId);
+    values.PutString(Media::MediaColumn::MEDIA_FILE_PATH, provider->ConsumeBytesAsString(NUM_BYTES));
+
+    string mimeType = "undefind";
+    string displayName = ".undefined";
+    FuzzMimeTypeAndDisplayNameExtension(mimeType, displayName);
+    values.PutString(Media::MediaColumn::MEDIA_NAME, displayName);
+    values.PutString(Media::MediaColumn::MEDIA_MIME_TYPE, mimeType);
+
+    int64_t fileId = 0;
+    g_rdbStore->Insert(fileId, Media:PhotoColumn::PHOTOS_TABLE, values);
+    retiurn static_cast<int32_t>(fileId);
+}
+
+static int FuzzPermissionType()
+{
+    vector<int> vecPermissionType;
+    vecPermissionType.assign(Media::AppUriPermissionColumn::PERMISSION_TYPES_ALL.begin(),
+        Media::AppUriPermissionColumn::PERMISSION_TYPES_ALL.end());
+    vecPermissionType.push_back(PERMISSION_DEFAULT);
+    uint8_t data = provider->ConsumeIntegralInRange<uint8_t>(0, MAX_PERMISSION_TYPE);
+    return vecPermissionType[data];
+}
+
+static NativeRdb::RdbPredicates GetRdbPredicates(void)
+{
+    string photoId = provider->ConsumeBytesAsString(NUM_BYTES);
+    int32_t fileId =InsertPhotoAsset(photoId);
+    string appId = provider->ConsumeBytesAsString(NUM_BYTES);
+    int32_t permissionType = FuzzPermissionType();
+    static DataSharePredicate predicates;
+
+    predicates.And()->EqualTo(Media::AppUriPermissionColumn::APP_ID, appId);
+    predicates.And()->EqualTo(Media::AppUriPermissionColumn::FILE_ID, fileId);
+    predicates.And()->EqualTo(Media::AppUriPermissionColumn::PERMISSION_TYPE, permissionType);
+    NativeRdb::RdbPredicates rdbPredicate = RdbDataShareAdapter::RdbUtils::ToPredicates(predicates,
+        Media::AppUriPermissionColumn::APP_URI_PERMISSION_TABLE);
+    return rdbPredicate;
+}
+
+static void DuplicatePhotoOperationTest()
+{
+    shared_ptr<DuplicatePhotoOperation> duplicatePhotoOperation =
+        make_shared<DuplicatePhotoOperation>();
+    
+    RdbPrediacates predicates = GetRdbPredicates();
+    std::vector<string> colums = { provider->ConsumeBytesAsString(NUM_BYTES) };
+
+    duplicatePhotoOperation->GetAllDuplicateAssets(predicates, columns);
+    duplicatePhotoOperation->GetDuplicateAssetsToDelete(predicates, columns);
+}
+
+static void SetTables()
+{
+    vector<string> createTableSqlList = {
+        Media::PhotoColumn::CREATE_PHOTO_TABLE,
+        Media::AppUriPermissionColumn::CREATE_APP_URI_PERMISSION_TABLE,
+        Media::AppUriSensitiveColumn::CREATE_APP_URI_SENSITIVE_TABLE,
+    };
+    for (auto &createTableSql : createTableSqlList) {
+        int32_t ret = g_rdbStore->EXecuteSql(createTableSql);
+        if (ret != NativeRdb::E_OK) {
+            MEDIA_ERR_LOG("Execute sql %{private}s failed", createTableSql.c_str());
+            return;
+        }
+        MEDIA_DEBUG_LOG("Execute sql %{private}s success", createTableSql.c_str());
+    }
+}
+
+static void Init()
+{
+    auto stageContext = std::make_shared<abilityRuntime::ContextImpl>();
+    auto abilityContextImpl = std::make_shared<OHOS::AbilityRuntime::abilityContextImpl>();
+    abilityContextImpl->SetStageContext(stageContext);
+    int32_t sceneCode = 0;
+    auto ret = Media::MediaLibrarayDataManager::GetInstance()->InitMediaLibraryMgr(abilityContextImpl,
+        abilityContextImpl, sceneCode);
+    CHECK_AND_RETURN_LOG(ret == NativeRdb::E_OK, "InitMediaLibraray Mgr failed, ret: %{public}d.", ret);
+    auto rdbStore = Media::MediaLibraryUnistoreManager::GetInstance().GetRdbStore();
+    if (rdbStore == nullptr) {
+        MEDIA_ERR_LOG("rdbStore is nullptr.");
+        return;
+    }
+    g_rdbStore = rdbStore;
+    SetTable();
+}
+
+static inLine void ClearKvStore()
+{
+    Media::MediaLibraryKvStoreManager::GetInstance().CloseAllKvStore();
+}
+} // namespace Media
+} // namespace OHOS
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+    OHOS::Media::Init();
+    return 0;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider fdp(data, size);
+    OHOS::Media::provider = &fdp;
+    if (data == nullptr) {
+        return 0;
+    }
+    OHOS::Media::DuplicatePhotoOperationTest();
+    OHOS::Media::ClearKvStore();
+    return 0;
+}
