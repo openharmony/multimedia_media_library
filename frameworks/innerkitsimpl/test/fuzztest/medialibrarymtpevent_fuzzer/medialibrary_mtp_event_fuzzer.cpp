0adf4564d422ae06a08ca0c079d572ce550aaca5
caochuan <caochuan@huawei.com>
2025-08-14 10:47:38 +0800

    mtp fuzz fix
    Signed-off-by: caochuan <caochuan@huawei.com>

---
+++ frameworks/innerkitsimpl/test/fuzztest/medialibrarymtpevent_fuzzer/medialibrary_mtp_event_fuzzer.cpp
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "medialibrary_mtp_event_fuzzer.h"
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <fuzzer/FuzzedDataProvider.h>
+
+#include "system_ability_definition.h"
+#include "iservice_registry.h"
+#include "userfilemgr_uri.h"
+#include "payload_data.h"
+#include "close_session_data.h"
+#include "media_log.h"
+
+#define private public
+#include "media_log.h"
+#include "media_mtp_utils.h"
+#include "mtp_driver.h"
+#include "mtp_error_utils.h"
+#include "mtp_event.h"
+#include "mtp_file_observer.h"
+#include "mtp_manager.h"
+#include "mtp_monitor.h"
+#include "mtp_operation.h"
+#include "mtp_packet.h"
+#include "mtp_service.h"
+#include "mtp_storage_manager.h"
+#include "mtp_store_observer.h"
+#include "packet_payload_factory.h"
+#include "ptp_album_handles.h"
+#undef private
+
+namespace OHOS {
+using namespace std;
+using namespace Media;
+
+static const int32_t NUM_BYTES = 1;
+static const int32_t MAX_BYTE_VALUE = 256;
+static const int32_t SEED_SIZE = 1024;
+FuzzedDataProvider *provider = nullptr;
+
+static inline vector<uint32_t> FuzzVectorUInt32()
+{
+    return {provider->ConsumeIntegral<uint32_t>()};
+}
+
+static MtpOperationContext FuzzMtpOperationContext()
+{
+    MtpOperationContext context;
+    context.operationCode = provider->ConsumeIntegral<uint16_t>();
+    context.transactionID = provider->ConsumeIntegral<uint32_t>();
+    context.devicePropertyCode = provider->ConsumeIntegral<uint32_t>();
+    context.storageID = provider->ConsumeIntegral<uint32_t>();
+    context.format = provider->ConsumeIntegral<uint16_t>();
+    context.parent = provider->ConsumeIntegral<uint32_t>();
+    context.handle = provider->ConsumeIntegral<uint32_t>();
+    context.property = provider->ConsumeIntegral<uint32_t>();
+    context.groupCode = provider->ConsumeIntegral<uint32_t>();
+    context.depth = provider->ConsumeIntegral<uint32_t>();
+    context.properStrValue = provider->ConsumeBytesAsString(NUM_BYTES);
+    context.properIntValue = provider->ConsumeIntegral<int64_t>();
+    context.handles = make_shared<UInt32List>(FuzzVectorUInt32());
+    context.name = provider->ConsumeBytesAsString(NUM_BYTES);
+    context.created = provider->ConsumeBytesAsString(NUM_BYTES);
+    context.modified = provider->ConsumeBytesAsString(NUM_BYTES);
+    context.indata = provider->ConsumeBool();
+    context.storageInfoID = provider->ConsumeIntegral<uint32_t>();
+    context.sessionOpen = provider->ConsumeBool();
+    context.sessionID = provider->ConsumeIntegral<uint32_t>();
+    context.mtpDriver = make_shared<MtpDriver>();
+    context.tempSessionID = provider->ConsumeIntegral<uint32_t>();
+    context.eventHandle = provider->ConsumeIntegral<uint32_t>();
+    context.eventProperty = provider->ConsumeIntegral<uint32_t>();
+    return context;
+}
+
+static void MtpEventTest()
+{
+    shared_ptr<MtpOperationContext> context = make_shared<MtpOperationContext>(
+        FuzzMtpOperationContext());
+    if (context == nullptr) {
+        MEDIA_ERR_LOG("context is nullptr");
+        return;
+    }
+    shared_ptr<MtpEvent> mtpEvent = make_shared<MtpEvent>(context);
+    string path = provider->ConsumeBytesAsString(NUM_BYTES);
+    uint32_t handle = provider->ConsumeIntegral<uint32_t>();
+    string fsUuid = provider->ConsumeBytesAsString(NUM_BYTES);
+    mtpEvent->SendObjectAdded(path);
+    mtpEvent->SendObjectRemoved(path);
+    mtpEvent->SendObjectRemovedByHandle(handle);
+    mtpEvent->SendObjectInfoChanged(path);
+    mtpEvent->SendDevicePropertyChanged();
+    mtpEvent->SendStoreAdded(fsUuid);
+    mtpEvent->SendStoreRemoved(fsUuid);
+}
+
+static int32_t AddSeed()
+{
+    char *seedData = new char[OHOS::SEED_SIZE];
+    for (int i = 0; i < OHOS::SEED_SIZE; i++) {
+        seedData[i] = static_cast<char>(i % MAX_BYTE_VALUE);
+    }
+
+    const char* filename = "corpus/seed.txt";
+    std::ofstream file(filename, std::ios::binary | std::ios::trunc);
+    if (!file) {
+        MEDIA_ERR_LOG("Cannot open file filename:%{public}s", filename);
+        delete[] seedData;
+        return Media::E_ERR;
+    }
+    file.write(seedData, OHOS::SEED_SIZE);
+    file.close();
+    delete[] seedData;
+    MEDIA_INFO_LOG("seedData has been successfully written to file filename:%{public}s", filename);
+    return Media::E_OK;
+}
+} // namespace OHOS
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
+{
+    OHOS::AddSeed();
+    return 0;
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    /* Run your code on data */
+    FuzzedDataProvider fdp(data, size);
+    OHOS::provider = &fdp;
+    if (data == nullptr) {
+        return 0;
+    }
+    OHOS::MtpEventTest();
+    return 0;
+}
