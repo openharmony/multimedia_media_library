e75c7b10f23c72c371ae401dcedc204aaed80848
pengpeng7872 <2285070166@qq.com>
2025-09-11 13:04:25 +0800

    modify badgetypes -> badgetype
    Signed-off-by: pengpeng7872 <2285070166@qq.com>

--- frameworks/js/src/photopickercomponent.js
+++ frameworks/js/src/photopickercomponent.js
@@ -961,10 +961,10 @@ export var ItemDisplayRatio;
     e[e.ORIGINAL_SIZE_RATIO = 1] = 'ORIGINAL_SIZE_RATIO';
 }(ItemDisplayRatio || (ItemDisplayRatio = {}));
 
-export var BadgeTypes;
+export var BadgeType;
 !function(e) {
     e[e.BADGE_UPLOADED = 1] = 'BADGE_UPLOADED';
-}(BadgeTypes || (BadgeTypes = {}));
+}(BadgeType || (BadgeType = {}));
 
 export var BadgeOptionType;
 !function(e) {
@@ -975,4 +975,4 @@ export var BadgeOptionType;
 
 export default { PhotoPickerComponent, PickerController, PickerOptions, DataType, BaseItemInfo, ItemInfo, PhotoBrowserInfo, AnimatorParams,
     MaxSelected, ItemType, ClickType, PickerOrientation, SelectMode, PickerColorMode, ReminderMode, MaxCountType, PhotoBrowserRange, PhotoBrowserUIElement,
-    VideoPlayerState, SaveMode, SingleLineConfig, ItemDisplayRatio, BadgeOptionType, BadgeTypes, BadgeConfig };
+    VideoPlayerState, SaveMode, SingleLineConfig, ItemDisplayRatio, BadgeOptionType, BadgeType, BadgeConfig };
