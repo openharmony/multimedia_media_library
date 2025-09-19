7eaadb881fb4fafc43a793eb52047e87b9dd8f05
LingYinzhou1 <lingyinzhou1@huawei.com>
2025-09-19 10:00:12 +0800

    feat:索引加字段支持火龙果和云增强，删除索引并且解除indexedby依赖替换为orderby，合并索引
    Signed-off-by: LingYinzhou1 <lingyinzhou1@huawei.com>

--- frameworks/js/src/sendable/sendable_photo_album_napi.cpp
+++ frameworks/js/src/sendable/sendable_photo_album_napi.cpp
@@ -505,7 +505,7 @@ static napi_value ParseArgsGetPhotoAssets(napi_env env, napi_callback_info info,
             return nullptr;
         }
         // sort by hidden time desc if is hidden asset
-        context->OrderByDesc(PhotoColumn::PHOTO_HIDDEN_TIME);
+        context->predicates.OrderByDesc(PhotoColumn::PHOTO_HIDDEN_TIME);
     }
 
     napi_value result = nullptr;
