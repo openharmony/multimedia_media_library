3d0ea4589f9f06f4fbfd8c05f2a6b5ce3d24376d
wuhongtao3 <wuhongtao3@huawei.com>
2025-10-05 18:14:57 +0800

    cloud_album_order_development
    Signed-off-by: wuhongtao3 <wuhongtao3@huawei.com>

--- interfaces/inner_api/media_library_helper/include/media_album_order_back.h
+++ interfaces/inner_api/media_library_helper/include/media_album_order_back.h
@@ -1,10 +1,25 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef FRAMEWORKS_SERVICES_MEDIA_MULTI_STAGES_CAPTURE_INCLUDE_MEDIA_ALBUM_ORDER_BACK_H
 #define FRAMEWORKS_SERVICES_MEDIA_MULTI_STAGES_CAPTURE_INCLUDE_MEDIA_ALBUM_ORDER_BACK_H
 
 #include <string>
 
-namespace OHOS{
-namespace Media{
+namespace OHOS {
+namespace Media {
 const std::string ALBUM_ORDER_BACK_TABLE = "OrderBackAlbum";
 const std::string LPATH = "lpath";
 const std::string ALBUMS_ORDER = "albums_order";
