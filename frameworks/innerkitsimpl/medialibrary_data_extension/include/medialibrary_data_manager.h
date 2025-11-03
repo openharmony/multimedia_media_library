82ff920afd33ced571e9c713c1984bbbe8a724da
caochuan <caochuan@huawei.com>
2025-11-03 19:35:22 +0800

    Medialibrary transcode data Aging 移植
    Signed-off-by: caochuan <caochuan@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager.h
+++ frameworks/innerkitsimpl/medialibrary_data_extension/include/medialibrary_data_manager.h
@@ -123,9 +123,6 @@ public:
     EXPORT int32_t UpdateDirtyForCloudClone(int32_t version);
     EXPORT int32_t ClearDirtyHdcData();
     EXPORT int32_t UpdateMediaSizeFromStorage();
-    EXPORT void AgingTmpCompatibleDuplicates();
-    EXPORT void InterruptAgingTmpCompatibleDuplicates();
-    EXPORT int32_t AgingTmpCompatibleDuplicate(int32_t fileId, const std::string &filePath);
     EXPORT int HandleAnalysisFaceUpdate(MediaLibraryCommand& cmd, NativeRdb::ValuesBucket &value,
                 const DataShare::DataSharePredicates &predicates);
     EXPORT int32_t RestoreInvalidPosData();
@@ -164,7 +161,6 @@ private:
     int32_t SolveInsertCmdSub(MediaLibraryCommand &cmd);
     void HandleOtherInitOperations();
     void InitRefreshAlbum();
-    void AgingTmpCompatibleDuplicatesThread();
     int32_t ProcessThumbnailBatchCmd(const MediaLibraryCommand &cmd,
         const NativeRdb::ValuesBucket &value, const DataShare::DataSharePredicates &predicates);
     void SubscriberPowerConsumptionDetection();
