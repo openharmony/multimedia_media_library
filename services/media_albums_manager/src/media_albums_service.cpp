986b23feabf9a12645210a3b2068d2276a988b12
ran880 <zhuangchangsheng@h-partners.com>
2025-11-03 18:56:06 +0800

    add
    Signed-off-by: ran880 <zhuangchangsheng@h-partners.com>

--- services/media_albums_manager/src/media_albums_service.cpp
+++ services/media_albums_manager/src/media_albums_service.cpp
@@ -437,8 +437,10 @@ std::shared_ptr<DataShare::DataShareResultSet> MediaAlbumsService::AlbumGetSelec
         maxScore = this->rdbOperation_.GetAssetScore(dto, curFileId);
     }
     minScore = this->rdbOperation_.GetLimitScore(dto);
-    CHECK_AND_RETURN_RET_LOG(minScore < maxScore,
-        make_shared<DataShare::DataShareResultSet>(), "AlbumGetSelectedAssets minScore >= maxScore");
+    if (minScore >= maxScore) {
+        MEDIA_ERR_LOG("AlbumGetSelectedAssets minScore >= maxScore");
+        return std::make_shared<DataShare::DataShareResultSet>();
+    }
     dto.minScore = minScore;
     dto.maxScore = maxScore;
     return this->rdbOperation_.GetSelectedAssets(dto);
