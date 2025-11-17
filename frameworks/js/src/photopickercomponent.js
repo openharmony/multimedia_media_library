1a29704e8f1ba3ec98fd4424de8a9671543cb86d
lingyinzhou <lingyinzhou1@huawei.com>
2025-11-17 18:12:08 +0800

    通过displayName修改是否跟随宿主
    Signed-off-by: lingyinzhou <lingyinzhou1@huawei.com>

--- frameworks/js/src/photopickercomponent.js
+++ frameworks/js/src/photopickercomponent.js
@@ -30,13 +30,14 @@ const bundleManager = requireNapi('bundle.bundleManager');
 const LengthMetrics = requireNapi('arkui.node').LengthMetrics;
 const LengthUnit = requireNapi('arkui.node').LengthUnit;
 const photoAccessHelper = requireNapi('file.photoAccessHelper');
+const display = requireNapi('display');
 const PHOTO_VIEW_MIME_TYPE_MAP = new Map([
     ['*/*', 'FILTER_MEDIA_TYPE_ALL'],
     ['image/*', 'FILTER_MEDIA_TYPE_IMAGE'],
     ['video/*', 'FILTER_MEDIA_TYPE_VIDEO'],
     ['image/movingPhoto', 'FILTER_MEDIA_TYPE_IMAGE_MOVING_PHOTO']
 ]);
-
+const cooperation_multi_name = 'Cooperation-multi'
 export class PhotoPickerComponent extends ViewPU {
     constructor(e, o, t, i = -1, n = void 0) {
         super(e, t, i);
@@ -62,6 +63,7 @@ export class PhotoPickerComponent extends ViewPU {
         this.preselectedInfos = void 0;
         this.__pickerController = new SynchedPropertyNesedObjectPU(o.pickerController, this, 'pickerController');
         this.proxy = void 0;
+        this.dpiFollowStrategy = SecurityDpiFollowStrategy.FOLLOW_UI_EXTENSION_ABILITY_DPI;
         this.__revokeIndex = new ObservedPropertySimplePU(0, this, 'revokeIndex');
         this.setInitiallyProvidedValue(o);
         this.declareWatch('pickerController', this.onChanged);
@@ -92,6 +94,11 @@ export class PhotoPickerComponent extends ViewPU {
         void 0 !== e.proxy && (this.proxy = e.proxy);
         if (e.revokeIndex !== undefined) {
             this.revokeIndex = e.revokeIndex;
+        }
+        const displayName = display.getDefaultDisplaySync().name;
+        console.log(`displayName = ${displayName}`);
+        if (cooperation_multi_name === displayName){
+            this.dpiFollowStrategy = SecurityDpiFollowStrategy.FOLLOW_HOST_DPI;
         }
     }
 
@@ -316,10 +323,11 @@ export class PhotoPickerComponent extends ViewPU {
                     combinedMediaTypeFilter: null === (f = this.pickerOptions) || void 0 === f ? void 0 : f.combinedMediaTypeFilter,
                     pickerIndex: null === (y = this.pickerOptions) || void 0 === y ? void 0 : y.pickerIndex,
                     isMovingPhotoBadgeShown:  null === (s = this.pickerOptions) || void 0 === s ? void 0 : s.isMovingPhotoBadgeShown,
+                    isSlidingSupported:  null === (s = this.pickerOptions) || void 0 === s ? void 0 : s.isSlidingSupported,
                 }
             }
             ,{
-                dpiFollowStrategy: SecurityDpiFollowStrategy.FOLLOW_HOST_DPI
+                dpiFollowStrategy: this.dpiFollowStrategy
             });
             SecurityUIExtensionComponent.height('100%');
             SecurityUIExtensionComponent.width('100%');
