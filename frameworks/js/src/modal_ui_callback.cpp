f3959ad9dde1701535003841db32a0f92796069c
lingyinzhou <lingyinzhou1@huawei.com>
2025-11-20 17:32:51 +0800

    picker跟随navigation销毁
    Signed-off-by: lingyinzhou <lingyinzhou1@huawei.com>

--- frameworks/js/src/modal_ui_callback.cpp
+++ frameworks/js/src/modal_ui_callback.cpp
@@ -69,6 +69,15 @@ void ModalUICallback::OnResultForModal(int32_t resultCode, const OHOS::AAFwk::Wa
 void ModalUICallback::OnReceive(const OHOS::AAFwk::WantParams &request)
 {
     NAPI_INFO_LOG("OnReceive enter.");
+    int32_t code = request.GetIntParam("code", INVALID_MSG_CODE);
+    if (code !== PICKER_MSG_CODE_PAGE_APPEAR) {
+        return;
+    }
+    if (this->uiContent != nullptr) {
+        this->uiContent->UpdateModalUIExtensionConfig(this->sessionId_, {
+            .prohibitedRemoveByNavigation = false,
+        })
+    }
 }
 
 void ModalUICallback::OnDestroy()
