cb9e5f3688dfe33f7162ef5e1e345f5ba6eb56f7
caochuan <caochuan@huawei.com>
2025-11-24 11:35:57 +0800

    groupAlbum克隆
    Signed-off-by: caochuan <caochuan@huawei.com>

--- frameworks/services/media_backup_extension/src/group_photo_album/group_photo_album_restore.cpp
+++ frameworks/services/media_backup_extension/src/group_photo_album/group_photo_album_restore.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2025 Huawei Device Co., Ltd.
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,16 +14,11 @@
  */
 #define MLOG_TAG "CloneGroupPhotoAlbum"
 
-#include "clone_group_photo_album.h"
+#include "group_photo_album_restore.h"
+
 #include "backup_const_column.h"
-#include "backup_const_map.h"
 #include "backup_database_utils.h"
-#include "backup_file_utils.h"
 #include "backup_log_utils.h"
-#include "database_report.h"
-#include "cloud_sync_helper.h"
-#include "exif_rotate_utils.h"
-#include "gallery_db_upgrade.h"
 #include "media_column.h"
 #include "media_file_utils.h"
 #include "media_log.h"
@@ -31,43 +26,26 @@
 #include "medialibrary_errno.h"
 #include "medialibrary_rdb_transaction.h"
 #include "photo_album_restore.h"
-#include "photos_dao.h"
-#include "photos_restore.h"
 #include "result_set_utils.h"
-#include "upgrade_restore_task_report.h"
 #include "userfile_manager_types.h"
+#include "upgrade_restore_task_report.h"
 #include "vision_album_column.h"
 #include "vision_column.h"
 #include "vision_face_tag_column.h"
 #include "vision_image_face_column.h"
-#include "vision_photo_map_column.h"
 
 namespace OHOS {
 namespace Media {
-CloneGroupPhotoAlbum::CloneGroupPhotoAlbum(std::shared_ptr<NativeRdb::RdbStore> galleryRdb,
-    std::shared_ptr<NativeRdb::RdbStore> mediaLibraryRdb)
+CloneGroupPhotoAlbum::CloneGroupPhotoAlbum(int32_t sceneCode, std::string taskId,
+    std::shared_ptr<NativeRdb::RdbStore> mediaLibraryRdb, std::shared_ptr<NativeRdb::RdbStore> galleryRdb)
 {
+    sceneCode_ = sceneCode;
+    taskId_ = taskId;
     mediaLibraryRdb_ = mediaLibraryRdb;
     galleryRdb_ = galleryRdb;
 }
 
-static std::vector<std::string> GroupTagSplit(const std::string& str, const std::string& delimiter)
-{
-    std::vector<std::string> result;
-    size_t start = 0;
-    size_t end = str.find(delimiter);
-
-    while (end != std::string::npos) {
-        result.emplace_back(str.substr(start, end - start));
-        start = end + delimiter.length();
-        end = str.find(delimiter, start);
-    }
-    result.emplace_back(str.substr(start));
-
-    return result;
-}
-
-static std::string JoinGroupTag(const std::map<std::string, std::vector<std::string>>& groupTagMap)
+static std::string JoinGroupTag(const std::unordered_map<std::string, std::vector<std::string>>& groupTagMap)
 {
     string groupTagId = "";
     CHECK_AND_RETURN_RET(groupTagMap.size() != 0, groupTagId);
@@ -84,9 +62,9 @@ static std::string JoinGroupTag(const std::map<std::string, std::vector<std::str
     return groupTagId;
 }
 
-bool CloneGroupPhotoAlbum::GetFileIdsByGroupTag(GroupAlbumInfo& info)
+bool CloneGroupPhotoAlbum::GetFileIdsByGroupTag(CloneGroupPhotoAlbum::GroupAlbumInfo &info)
 {
-    std::map<std::string, std::string> tagMap = info.tagIdMap;
+    std::unordered_map<std::string, std::vector<std::string>> tagMap = info.groupTagMap;
     CHECK_AND_RETURN_RET_LOG(!tagMap.empty(), false, "tagMap is empty.");
     std::string querySql =  R"(SELECT DISTINCT file_id FROM tab_analysis_image_face WHERE 1=1 )";
     std::string firstSql = R"(AND file_id IN (SELECT file_id FROM tab_analysis_image_face
@@ -94,7 +72,11 @@ bool CloneGroupPhotoAlbum::GetFileIdsByGroupTag(GroupAlbumInfo& info)
     std::string lastSql = R"() AND (face.total_faces = 0 OR face.total_faces = )";
     std::string groupSql = R"( ORDER BY file_id DESC)";
     for (const auto& tagIt : tagMap) {
-        querySql += firstSql + tagIt.second + lastSql + std::to_string(tagMap.size()) + "))";
+        std::string tagIdSelection;
+        for (auto& tagIdItem : tagIt.second) {
+            BackupDatabaseUtils::UpdateSelection(tagIdSelection, tagIdItem, true);
+        }
+        querySql += firstSql + tagIdSelection + lastSql + std::to_string(tagMap.size()) + "))";
     }
     querySql += groupSql;
     MEDIA_INFO_LOG("querySql = %{public}s", querySql.c_str());
@@ -109,63 +91,67 @@ bool CloneGroupPhotoAlbum::GetFileIdsByGroupTag(GroupAlbumInfo& info)
     return true;
 }
 
-bool CloneGroupPhotoAlbum::QueryTagIdFromMergeTag(GroupAlbumInfo& info)
+void CloneGroupPhotoAlbum::QueryTagIdFromMergeTag(CloneGroupPhotoAlbum::GroupAlbumInfo &info)
 {
     std::string querySql = "SELECT " + GALLERY_MERGE_TAG_TAG_ID + " FROM merge_tag WHERE " +
         GALLERY_GROUP_TAG + " = ?";
     std::vector<std::string> tagVector = info.groupTagVec;
-    CHECK_AND_RETURN_RET_LOG(!tagVector.empty(), false, "tagVector is empty.");
+    CHECK_AND_RETURN_LOG(!tagVector.empty(), "tagVector is empty.");
     for (const auto& tag : tagVector) {
         auto resultSet = BackupDatabaseUtils::GetQueryResultSet(galleryRdb_, querySql, { tag });
-        CHECK_AND_RETURN_RET_LOG(resultSet != nullptr, false, "Query resultSql is null.");
+        CHECK_AND_RETURN_LOG(resultSet != nullptr, "Query resultSql is null.");
         std::vector<std::string> tagIdVec;
-        std::vector<std::string> tagIdVector;
         while (resultSet->GoToNextRow() == NativeRdb::E_OK) {
             std::string tagId = GetStringVal(GALLERY_MERGE_TAG_TAG_ID, resultSet);
-            tagIdVector.emplace_back("'" + tagId + "'");
             tagIdVec.emplace_back(std::move(tagId));
         }
-        CHECK_AND_RETURN_RET_LOG(tagIdVec.size() != 0, false, "mergeTag do not find any tag_id.");
-        info.groupTagMap.insert(std::make_pair(tag, tagIdVec));
-        info.tagIdMap.insert(std::make_pair(tag, BackupDatabaseUtils::JoinValues(tagIdVector, ",")));
         resultSet->Close();
+        CHECK_AND_RETURN_LOG(!tagIdVec.empty(), "tagIdVec is empty.");
+        info.groupTagMap.insert(std::make_pair(tag, tagIdVec));
     }
-    return true;
+    return;
 }
 
-std::vector<GroupAlbumInfo> CloneGroupPhotoAlbum::GetGroupPhotoAlbumInfo()
+std::vector<CloneGroupPhotoAlbum::GroupAlbumInfo> CloneGroupPhotoAlbum::GetGroupPhotoAlbumInfo()
 {
     MEDIA_INFO_LOG("Start GetGroupPhotoAlbumInfo.");
     std::string querySql = "SELECT " + GALLERY_MERGE_TAG_TAG_ID + ", " + GALLERY_GROUP_TAG + ", " +
         GALLERY_TAG_NAME + ", " + GALLERY_USER_OPERATION + ", " + GALLERY_RENAME_OPERATION +
         " FROM merge_tag WHERE group_Tag LIKE '%|%'";
-    std::vector<GroupAlbumInfo> result;
+    std::vector<CloneGroupPhotoAlbum::GroupAlbumInfo> result;
     auto resultSet = BackupDatabaseUtils::GetQueryResultSet(galleryRdb_, querySql);
     CHECK_AND_RETURN_RET_LOG(resultSet != nullptr, result, "Query resultSql is null.");
     while (resultSet->GoToNextRow() == NativeRdb::E_OK) {
-        GroupAlbumInfo info;
+        CloneGroupPhotoAlbum::GroupAlbumInfo info;
         std::vector<std::string> tagVector;
         string groupTag = GetStringVal(GALLERY_GROUP_TAG, resultSet);
         CHECK_AND_CONTINUE(!groupTag.empty());
-        tagVector = GroupTagSplit(groupTag, "|");
+        tagVector = BackupDatabaseUtils::SplitString(groupTag, '|');
         info.groupTagVec = tagVector;
-        CHECK_AND_CONTINUE(QueryTagIdFromMergeTag(info));
-        CHECK_AND_CONTINUE(GetFileIdsByGroupTag(info));
-        info.groupTag = JoinGroupTag(info.groupTagMap);
-        MEDIA_INFO_LOG("info.groupTag = %{public}s", info.groupTag.c_str());
-        info.tagId = info.groupTag;
         info.tagName = GetStringVal(GALLERY_TAG_NAME, resultSet);
         info.userOperation = GetInt32Val(GALLERY_USER_OPERATION, resultSet);
         info.renameOperation = (!info.tagName.empty() ? RENAME_OPERATION_RENAMED : 0);
         result.emplace_back(info);
     }
     resultSet->Close();
+    for (auto it = result.begin(); it != result.end();) {
+        auto& info = *it;
+        QueryTagIdFromMergeTag(info);
+        if (!GetFileIdsByGroupTag(info)) {
+            it = result.erase(it);
+        } else {
+            info.groupTag = JoinGroupTag(info.groupTagMap);
+            MEDIA_INFO_LOG("info.groupTag = %{public}s", info.groupTag.c_str());
+            info.tagId = info.groupTag;
+            ++it;
+        }
+    }
     MEDIA_INFO_LOG("GetGroupPhotoAlbumInfo end.");
     return result;
 }
 
-void CloneGroupPhotoAlbum::QueryGroupPhotoAlbum(std::vector<GroupAlbumInfo> &groupPhotoAlbumInfos,
-    std::map<int32_t, std::vector<int32_t>> &groupPhotoMap)
+void CloneGroupPhotoAlbum::QueryGroupPhotoAlbum(std::vector<CloneGroupPhotoAlbum::GroupAlbumInfo>&
+    groupPhotoAlbumInfos, std::map<int32_t, std::vector<int32_t>> &groupPhotoMap)
 {
     std::string querySql = "SELECT " + ALBUM_ID + " FROM " + ANALYSIS_ALBUM_TABLE + " WHERE " +
         GROUP_TAG + " IS NOT NULL" + " AND " + GROUP_TAG + " = ?";
@@ -203,7 +189,8 @@ void CloneGroupPhotoAlbum::InsertAnalysisPhotoMap(const std::map<int32_t, std::v
     string updateSql = baseUpdateSql;
     bool isFirst = true;
     bool isFirstUpdate = true;
-
+    int64_t start = MediaFileUtils::UTCTimeMilliSeconds();
+    int32_t count = 0;
     for (const auto& [albumId, fileIds] : groupPhotoMap) {
         for (int fileId : fileIds) {
             if (!isFirst) {
@@ -221,6 +208,7 @@ void CloneGroupPhotoAlbum::InsertAnalysisPhotoMap(const std::map<int32_t, std::v
                 CHECK_AND_RETURN(ExecuteBatchSql(sql, updateSql));
                 sql = baseSql;
                 updateSql = baseUpdateSql;
+                count += currentBatchSize;
                 currentBatchSize = 0;
                 isFirst = true;
                 isFirstUpdate = true;
@@ -229,7 +217,15 @@ void CloneGroupPhotoAlbum::InsertAnalysisPhotoMap(const std::map<int32_t, std::v
     }
     if (currentBatchSize > 0) {
         ExecuteBatchSql(sql, updateSql);
+        count += currentBatchSize;
     }
+    int64_t end = MediaFileUtils::UTCTimeMilliSeconds();
+    int64_t time = end - start;
+    std::stringstream albumReport;
+    albumReport << "InsertAnalysisPhotoMap rowCount: " << count << ", InsertAnalysisPhotoMap time: " << time;
+    UpgradeRestoreTaskReport().SetSceneCode(sceneCode_).SetTaskId(taskId_)
+        .Report("CLONE_GROUP_PHOTO_ALBUM_STATS", "", albumReport.str());
+    MEDIA_INFO_LOG("InsertAnalysisPhotoMap end, count = %{public}d, time = %{public}" PRId64, count, time);
 }
 
 int32_t CloneGroupPhotoAlbum::BatchInsertWithRetry(const std::string &tableName,
@@ -256,7 +252,7 @@ int32_t CloneGroupPhotoAlbum::BatchInsertWithRetry(const std::string &tableName,
 void CloneGroupPhotoAlbum::UpdateGroupPhoto()
 {
     MEDIA_INFO_LOG("Start to update group photo album.");
-    std::vector<GroupAlbumInfo> result;
+    std::vector<CloneGroupPhotoAlbum::GroupAlbumInfo> result;
     int64_t start = MediaFileUtils::UTCTimeMilliSeconds();
     result = GetGroupPhotoAlbumInfo();
     CHECK_AND_RETURN_LOG(result.size() != 0, "Query resultSql is null.");
@@ -276,6 +272,10 @@ void CloneGroupPhotoAlbum::UpdateGroupPhoto()
     }
     int64_t rowNum = 0;
     int32_t errCode = BatchInsertWithRetry(ANALYSIS_ALBUM_TABLE, values, rowNum);
+    int64_t analysisAlbumEnd = MediaFileUtils::UTCTimeMilliSeconds();
+    int64_t analysisAlbumTime = analysisAlbumEnd - start;
+    MEDIA_INFO_LOG("End InsertAnalysisAlbum. AnalysisAlbumRowNum = %{public}" PRId64
+        "AnalysisAlbumTime = %{public}" PRId64, rowNum, analysisAlbumTime);
     CHECK_AND_RETURN(errCode == E_OK);
     std::map<int32_t, std::vector<int32_t>> groupPhotoMap;
     QueryGroupPhotoAlbum(result, groupPhotoMap);
@@ -283,7 +283,12 @@ void CloneGroupPhotoAlbum::UpdateGroupPhoto()
     InsertAnalysisPhotoMap(groupPhotoMap);
     int64_t end = MediaFileUtils::UTCTimeMilliSeconds();
     int64_t sumTime = end - start;
-    MEDIA_INFO_LOG("End UpdateGroupPhoto. sumTime = %{public}" PRId64, sumTime);
+    std::stringstream updateGroupReport;
+    updateGroupReport << "InsertAnalysisAlbum rowNum: " << rowNum << ", InsertAnalysisAlbum time: " <<
+        analysisAlbumTime << ", GroupPhotoRestoreSumTime: " << sumTime;
+    UpgradeRestoreTaskReport().SetSceneCode(sceneCode_).SetTaskId(taskId_)
+        .Report("CLONE_GROUP_PHOTO_ALBUM_STATS", "", updateGroupReport.str());
+    MEDIA_INFO_LOG("End UpdateGroupPhoto. GroupPhotoRestoreSumTime = %{public}" PRId64, sumTime);
 }
 }
 }  // namespace OHOS
