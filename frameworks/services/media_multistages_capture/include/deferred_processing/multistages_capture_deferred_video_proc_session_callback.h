70439195519bda6e7e18c57f776be9ecfb933d1f
W0J6R22 <wangjunren4@huawei.com>
2025-11-26 15:04:32 +0800

    update: 更新文件 multistages_capture_deferred_video_proc_session_callback.h
    Signed-off-by: W0J6R22 <wangjunren4@huawei.com>

--- frameworks/services/media_multistages_capture/include/deferred_processing/multistages_capture_deferred_video_proc_session_callback.h
+++ frameworks/services/media_multistages_capture/include/deferred_processing/multistages_capture_deferred_video_proc_session_callback.h
@@ -25,7 +25,6 @@
 
 namespace OHOS {
 namespace Media {
-using namespace Notification;
 #define EXPORT __attribute__ ((visibility ("default")))
 class MultiStagesCaptureDeferredVideoProcSessionCallback : public CameraStandard::IDeferredVideoProcSessionCallback {
 public:
