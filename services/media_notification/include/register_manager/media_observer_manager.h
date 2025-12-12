691e5e676ce99b870406c2840a31c2fb621805a5
caochuan <caochuan@huawei.com>
2025-12-12 18:55:54 +0800

    单资产单相册监听bug修复
    Signed-off-by: caochuan <caochuan@huawei.com>

--- services/media_notification/include/register_manager/media_observer_manager.h
+++ services/media_notification/include/register_manager/media_observer_manager.h
@@ -50,8 +50,7 @@ public:
         const sptr<AAFwk::IDataAbilityObserver> &dataObserver, const std::string &uri);
     EXPORT int32_t RemoveSingleObserverUris(const NotifyUriType &registerUri,
         const sptr<AAFwk::IDataAbilityObserver> &dataObserver, const std::string& uri);
-    EXPORT bool FindSingleObserverWithUri(const NotifyUriType &uri,
-        const sptr<AAFwk::IDataAbilityObserver> &dataObserver);
+    EXPORT bool FindSingleObserverWithUri(const NotifyUriType &uri, const uint32_t callingTokenId);
     EXPORT bool FindSingleObserver(const NotifyUriType &uri, std::vector<ObserverInfo>& obsInfos);
     EXPORT int32_t RemoveSingleObserverUris(ObserverInfo& singleObserverInfo, const std::string &uri);
     EXPORT bool isUriDataPresentInSingleObserver(const std::unordered_set<std::string> &observedUris,
