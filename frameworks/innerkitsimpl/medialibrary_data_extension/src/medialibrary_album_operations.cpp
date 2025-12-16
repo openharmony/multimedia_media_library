53601a70336f4180c2d37babd4f06e583093a4a1
caochuan <caochuan@huawei.com>
2025-12-16 10:06:53 +0800

    photokeys enum fix
    Signed-off-by: caochuan <caochuan@huawei.com>

--- frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_album_operations.cpp
+++ frameworks/innerkitsimpl/medialibrary_data_extension/src/medialibrary_album_operations.cpp
@@ -331,6 +331,7 @@ static void PrepareUserAlbum(const string &albumName, ValuesBucket &values)
     values.PutInt(PhotoAlbumColumns::ALBUM_TYPE, PhotoAlbumType::USER);
     values.PutInt(PhotoAlbumColumns::ALBUM_SUBTYPE, PhotoAlbumSubType::USER_GENERIC);
     values.PutLong(PhotoAlbumColumns::ALBUM_DATE_MODIFIED, MediaFileUtils::UTCTimeMilliSeconds());
+    values.PutLong(PhotoAlbumColumns::CHANGE_TIME, MediaFileUtils::UTCTimeMilliSeconds());
     values.PutInt(PhotoAlbumColumns::ALBUM_IS_LOCAL, 1); // local album is 1.
     values.PutInt(PhotoAlbumColumns::ALBUM_PRIORITY, ALBUM_PRIORITY_DEFAULT);
     values.PutString(PhotoAlbumColumns::ALBUM_LPATH, USER_ALBUM_LPATH_PREFIX + albumName);
