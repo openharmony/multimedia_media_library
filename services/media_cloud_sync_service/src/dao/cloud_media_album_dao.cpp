f51ac8708dc90fec69de9df553f58c7d63d4e674
wuhongtao3 <wuhongtao3@huawei.com>
2025-12-16 14:17:47 +0800

    cloud_album_order_development_modify
    Signed-off-by: wuhongtao3 <wuhongtao3@huawei.com>

--- services/media_cloud_sync_service/src/dao/cloud_media_album_dao.cpp
+++ services/media_cloud_sync_service/src/dao/cloud_media_album_dao.cpp
@@ -642,18 +642,18 @@ int32_t CloudMediaAlbumDao::UpdateAlbumOrderInfo(PhotoAlbumDto &record, NativeRd
 
     bool found = false;
     if (resultSet->GoToFirstRow() == NativeRdb::E_OK) {
-        int32_t albumOrder = get<int32_t>(ResultSetUtils::GetValFromColumn("albums_order", resultSet, 
+        int32_t albumOrder = get<int32_t>(ResultSetUtils::GetValFromColumn("albums_order", resultSet,
                                                                            TYPE_INT32));
         int32_t orderType = get<int32_t>(ResultSetUtils::GetValFromColumn("order_type", resultSet, 
+                                                                          TYPE_INT32));
+        int32_t orderSection = get<int32_t>(ResultSetUtils::GetValFromColumn("order_section", resultSet,
+                                                                             TYPE_INT32));
+        int32_t albumOrder2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_albums_order", resultSet,
+                                                                            TYPE_INT32));
+        int32_t orderType2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_order_type", resultSet,
                                                                            TYPE_INT32));
-        int32_t orderSection = get<int32_t>(ResultSetUtils::GetValFromColumn("order_section", resultSet, 
-                                                                           TYPE_INT32));
-        int32_t albumOrder2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_albums_order", resultSet, 
-                                                                           TYPE_INT32));
-        int32_t orderType2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_order_type", resultSet, 
-                                                                           TYPE_INT32));
-        int32_t orderSection2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_order_section", resultSet, 
-                                                                           TYPE_INT32));
+        int32_t orderSection2 = get<int32_t>(ResultSetUtils::GetValFromColumn("style2_order_section", resultSet,
+                                                                              TYPE_INT32));
 
         values.PutInt(PhotoAlbumColumns::ALBUMS_ORDER, albumOrder);
         values.PutInt(PhotoAlbumColumns::ORDER_TYPE, orderType);
