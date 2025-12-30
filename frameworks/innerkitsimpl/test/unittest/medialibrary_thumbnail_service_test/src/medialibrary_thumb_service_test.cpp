ef044cf4da87b96b83c5d7fda161b34dcbd7e4d2
zengguangquan <zengguangquan@h-partners.com>
2025-12-30 16:31:06 +0800

    update: 更新文件 medialibrary_thumb_service_test.cpp
    Signed-off-by: zengguangquan <zengguangquan@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/src/medialibrary_thumb_service_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/medialibrary_thumbnail_service_test/src/medialibrary_thumb_service_test.cpp
@@ -68,7 +68,7 @@ HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_WhenThumbT
     EXPECT_GE(fd, 0);
 }
 
-HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_ThumbTypeIsThumbAstc, TestSize.Level0)
+HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_WhenThumbTypeIsThumbAstc, TestSize.Level0)
 {
     ThumbnailService thumbnailService;
     std::string path = "/path/to/thumbnail";
@@ -81,7 +81,7 @@ HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_ThumbTypeI
     EXPECT_GE(fd, 0);
 }
 
-HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_WhenThumbTypeIsNotThumbOrThumbAstc, TestSize.Level0)
+HWTEST_F(MediaLibraryThumbServiceTest, GetThumbFd_ShouldReturnValidFd_ThumbTypeIsNotThumbOrThumbAstc, TestSize.Level0)
 {
     ThumbnailService thumbnailService;
     std::string path = "/path/to/thumbnail";
