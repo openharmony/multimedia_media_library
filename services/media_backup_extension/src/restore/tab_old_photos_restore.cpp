95d1ef304ad80ae4fdc17f40873ad3c6e1343b47
tolgayakar <tolga.yakar@huawei.com>
2026-01-14 15:48:49 +0300

    tab_old_photos_restore BatchInsertWithRetry
    Signed-off-by: tolgayakar <tolga.yakar@huawei.com>

--- services/media_backup_extension/src/restore/tab_old_photos_restore.cpp
+++ services/media_backup_extension/src/restore/tab_old_photos_restore.cpp
@@ -21,6 +21,7 @@
 #include "backup_database_utils.h"
 #include "media_file_utils.h"
 #include "media_log.h"
+#include "media_column.h"
 
 namespace OHOS::Media {
 int32_t TabOldPhotosRestore::Restore(
@@ -29,89 +30,56 @@ int32_t TabOldPhotosRestore::Restore(
     CHECK_AND_RETURN_RET_LOG(rdbStorePtr != nullptr, NativeRdb::E_DB_NOT_EXIST,
         "rdbStorePtr is nullptr, Maybe init failed");
 
-    int64_t startSet = MediaFileUtils::UTCTimeMilliSeconds();
     TabOldPhotosRestoreHelper restoreHelper;
-    restoreHelper.SetPlaceHoldersAndBindArgs(fileInfos);
-    CHECK_AND_RETURN_RET(!restoreHelper.IsEmpty(), NativeRdb::E_OK);
-
+    
     int64_t startInsert = MediaFileUtils::UTCTimeMilliSeconds();
-    int32_t ret = restoreHelper.InsertIntoTable(rdbStorePtr);
-    CHECK_AND_EXECUTE(ret == NativeRdb::E_OK,
-        MEDIA_ERR_LOG("Restore failed, ret=%{public}d, executeSql=%{public}s, bindArgs: %{public}s",
-            ret, restoreHelper.GetInsertSql().c_str(), ToString(restoreHelper.GetBindArgs()).c_str()));
+    int32_t ret = restoreHelper.BatchInsertWithRetry(rdbStorePtr, fileInfos);
+    CHECK_AND_EXECUTE(ret == NativeRdb::E_OK, MEDIA_ERR_LOG("Restore failed, ret=%{public}d", ret));
 
     int64_t end = MediaFileUtils::UTCTimeMilliSeconds();
-    MEDIA_INFO_LOG("Set %{public}zu cost %{public}" PRId64 ", insert cost %{public}" PRId64,
-        restoreHelper.GetInsertSize(), startInsert - startSet, end - startInsert);
+    MEDIA_INFO_LOG("End Restore insert cost %{public}" PRId64, end - startInsert);
     return ret;
 }
 
-std::string TabOldPhotosRestore::ToString(const std::vector<NativeRdb::ValueObject> &values)
+std::vector<NativeRdb::ValuesBucket> TabOldPhotosRestoreHelper::GetInsertValues(const std::vector<FileInfo> &fileInfos)
 {
-    std::vector<std::string> result;
-    for (auto &value : values) {
-        std::string str;
-        value.GetString(str);
-        result.emplace_back(str + ", ");
+    std::vector<NativeRdb::ValuesBucket> values;
+
+    for (auto &fileInfo : fileInfos) {
+        NativeRdb::ValuesBucket value;
+
+        value.PutInt(PhotoColumn::TAB_OLD_PHOTOS_COLUMN_FILE_ID, fileInfo.fileIdNew);
+        value.PutInt(PhotoColumn::TAB_OLD_PHOTOS_COLUMN_OLD_FILE_ID, fileInfo.localMediaId);
+        value.PutString(PhotoColumn::TAB_OLD_PHOTOS_COLUMN_OLD_DATA, fileInfo.oldPath);
+        value.PutString(PhotoColumn::TAB_OLD_PHOTOS_COLUMN_DATA, fileInfo.cloudPath);
+
+        values.emplace_back(value);
     }
-    return std::accumulate(result.begin(), result.end(), std::string());
+
+    return values;
 }
 
-void TabOldPhotosRestoreHelper::SetPlaceHoldersAndBindArgs(const std::vector<FileInfo> &fileInfos)
+int32_t TabOldPhotosRestoreHelper::BatchInsertWithRetry(std::shared_ptr<NativeRdb::RdbStore> rdbStorePtr,
+    const std::vector<FileInfo> &fileInfos)
 {
-    for (const auto &fileInfo : fileInfos) {
-        if (!fileInfo.needMove) {
-            continue;
-        }
-        AddPlaceHolders();
-        AddBindArgs(fileInfo);
+    int32_t errCode = E_ERR;
+    TransactionOperations trans{ __func__ };
+    trans.SetBackupRdbStore(rdbStorePtr);
+
+    int64_t rowNum = 0;
+    auto values = GetInsertValues(fileInfos);
+    if (values.empty()) {
+        MEDIA_ERR_LOG("TabOldPhotosRestoreHelper::BatchInsertWithRetry values are empty!");
+        return E_ERR;
     }
-}
-
-bool TabOldPhotosRestoreHelper::IsEmpty()
-{
-    return placeHolders_.empty() || bindArgs_.empty();
-}
-
-int32_t TabOldPhotosRestoreHelper::InsertIntoTable(std::shared_ptr<NativeRdb::RdbStore> rdbStorePtr)
-{
-    CHECK_AND_RETURN_RET_LOG(rdbStorePtr != nullptr, NativeRdb::E_DB_NOT_EXIST, "rdbStorePtr is nullptr");
-
-    std::string insertSql = GetInsertSql();
-    std::vector<NativeRdb::ValueObject> bindArgs = GetBindArgs();
-    return rdbStorePtr->ExecuteSql(insertSql, bindArgs);
-}
-
-std::string TabOldPhotosRestoreHelper::GetInsertSql()
-{
-    return GetInputTableClause() + SQL_TAB_OLD_PHOTOS_INSERT;
-}
-
-std::vector<NativeRdb::ValueObject> TabOldPhotosRestoreHelper::GetBindArgs()
-{
-    return bindArgs_;
-}
-
-size_t TabOldPhotosRestoreHelper::GetInsertSize()
-{
-    return placeHolders_.size();
-}
-
-void TabOldPhotosRestoreHelper::AddPlaceHolders()
-{
-    placeHolders_.emplace_back(SQL_PLACEHOLDERS);
-}
-
-void TabOldPhotosRestoreHelper::AddBindArgs(const FileInfo &fileInfo)
-{
-    bindArgs_.emplace_back(fileInfo.localMediaId);
-    bindArgs_.emplace_back(fileInfo.oldPath);
-    bindArgs_.emplace_back(fileInfo.cloudPath);
-}
-
-std::string TabOldPhotosRestoreHelper::GetInputTableClause()
-{
-    return "WITH INPUT (old_file_id, old_data, data) AS (VALUES " +
-        BackupDatabaseUtils::JoinValues(placeHolders_, ",") + " ) ";
+    std::function<int(void)> func = [&]()->int {
+        errCode = trans.BatchInsert(rowNum, PhotoColumn::TAB_OLD_PHOTOS_TABLE, values);
+        CHECK_AND_PRINT_LOG(errCode == E_OK,
+            "InsertSql failed, errCode: %{public}d, rowNum: %{public}ld.", errCode, (long)rowNum);
+        return errCode;
+    };
+    errCode = trans.RetryTrans(func, true);
+    CHECK_AND_PRINT_LOG(errCode == E_OK, "BatchInsertWithRetry: trans finish fail!, ret:%{public}d", errCode);
+    return errCode;
 }
 } // namespace OHOS::Media
