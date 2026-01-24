3d9e83ff361deda1031d6bc00f49081c0f772dfa
cilinf <fumenglei@huawei.com>
2026-01-24 17:32:18 +0800

    缓存兜底罗盘耗时长
    Signed-off-by: cilinf <fumenglei@huawei.com>

--- services/media_camera_character_service/src/deferred_processing/multistages_capture_deferred_photo_proc_session_callback.cpp
+++ services/media_camera_character_service/src/deferred_processing/multistages_capture_deferred_photo_proc_session_callback.cpp
@@ -454,7 +454,7 @@ void MultiStagesCaptureDeferredPhotoProcSessionCallback::OnDeliveryLowQualityIma
     int32_t fileId = GetInt32Val(MediaColumn::MEDIA_ID, resultSet);
     bool isEdited = (GetInt64Val(PhotoColumn::PHOTO_EDIT_TIME, resultSet) > 0);
     resultSet->Close();
-    MultiStagesPhotoCaptureManager::GetInstance().DealLowQualityPicture(photoId, fileId std::move(picture), isEdited);
+    MultiStagesPhotoCaptureManager::GetInstance().DealLowQualityPicture(photoId, fileId, std::move(picture), isEdited);
     HILOG_COMM_INFO("%{public}s:{%{public}s:%{public}d} MultistagesCapture save low quality image end",
         MLOG_TAG, __FUNCTION__, __LINE__);
 }
