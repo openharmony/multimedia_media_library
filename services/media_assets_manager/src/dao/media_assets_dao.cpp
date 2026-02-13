af26ebb4d622a4959d4fea28a8900a8bb67a6254
yanghaiyang7 <yanghaiyang7@huawei.com>
2026-02-13 17:09:02 +0800

    【问题修复】仅删除本地和合并相同资产，适配south_device_type
    Signed-off-by: yanghaiyang7 <yanghaiyang7@huawei.com>
    Change-Id: I57e437aaefaa8e273c4fde1996068704585ae427

--- services/media_assets_manager/src/dao/media_assets_dao.cpp
+++ services/media_assets_manager/src/dao/media_assets_dao.cpp
@@ -269,6 +269,7 @@ int32_t MediaAssetsDao::MergeCloudInfoIntoTargetPhoto(const PhotosPo &sourcePhot
     uint32_t position = static_cast<uint32_t>(sourcePhotoInfo.position.value_or(0)) |
                         static_cast<uint32_t>(targetPhotoInfo.position.value_or(0));
     values.PutInt(PhotoColumn::PHOTO_POSITION, static_cast<int32_t>(position));
+    this->HandleSouthDeviceType(sourcePhotoInfo, targetPhotoInfo, values);
     int32_t changedRows = -1;
     int32_t ret = photoRefresh->Update(changedRows, values, predicates);
     MEDIA_INFO_LOG("MergeCloudInfoIntoTargetPhoto Completed, "
@@ -355,16 +356,16 @@ bool MediaAssetsDao::IsSameAssetIgnoreAlbum(const PhotosPo &photoInfo, const Pho
     return isSame && isSameOrientation;
 }
 
-int32_t MediaAssetsDao::UpdatePositionToBoth(
-    const PhotosPo &photoInfo, std::shared_ptr<AccurateRefresh::AssetAccurateRefresh> &photoRefresh)
+int32_t MediaAssetsDao::UpdatePositionToBoth(const PhotosPo &sourcePhotoInfo, const PhotosPo &targetPhotoInfo,
+                                             std::shared_ptr<AccurateRefresh::AssetAccurateRefresh> &photoRefresh)
 {
-    int32_t position = photoInfo.position.value_or(1);
+    int32_t position = targetPhotoInfo.position.value_or(1);
     bool isValid = position != static_cast<int32_t>(PhotoPositionType::LOCAL_AND_CLOUD);
     CHECK_AND_RETURN_RET_INFO_LOG(isValid, E_OK, "UpdatePositionToBoth, position is correct, no need to update.");
     CHECK_AND_RETURN_RET_LOG(photoRefresh != nullptr, E_RDB_STORE_NULL, "UpdatePosition Failed to get photoRefresh.");
 
     NativeRdb::AbsRdbPredicates predicates = NativeRdb::AbsRdbPredicates(PhotoColumn::PHOTOS_TABLE);
-    predicates.EqualTo(PhotoColumn::MEDIA_ID, photoInfo.fileId.value_or(0));
+    predicates.EqualTo(PhotoColumn::MEDIA_ID, targetPhotoInfo.fileId.value_or(0));
     NativeRdb::ValuesBucket values;
     values.PutInt(PhotoColumn::PHOTO_POSITION, static_cast<int32_t>(PhotoPositionType::LOCAL_AND_CLOUD));
     int32_t changedRows = -1;
@@ -373,20 +374,21 @@ int32_t MediaAssetsDao::UpdatePositionToBoth(
                    "ret: %{public}d, ChangedRows: %{public}d, cloudId: %{public}s",
         ret,
         changedRows,
-        photoInfo.cloudId.value_or("").c_str());
+        targetPhotoInfo.cloudId.value_or("").c_str());
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "Failed to UpdatePositionToBoth.");
     CHECK_AND_RETURN_RET_WARN_LOG(
         changedRows > 0, ret, "UpdatePositionToBoth Check updateRows: %{public}d.", changedRows);
     auto watch = MediaLibraryNotify::GetInstance();
     CHECK_AND_RETURN_RET_LOG(watch != nullptr, ret, "watch is nullptr");
-    watch->Notify(PhotoColumn::PHOTO_URI_PREFIX + to_string(photoInfo.fileId.value_or(0)), NotifyType::NOTIFY_UPDATE);
+    watch->Notify(
+        PhotoColumn::PHOTO_URI_PREFIX + to_string(targetPhotoInfo.fileId.value_or(0)), NotifyType::NOTIFY_UPDATE);
     return E_OK;
 }
 
-int32_t MediaAssetsDao::UpdatePositionToBothAndFileSourceTypeToLake(
-    const PhotosPo &photoInfo, std::shared_ptr<AccurateRefresh::AssetAccurateRefresh> &photoRefresh)
+int32_t MediaAssetsDao::UpdatePositionToBothAndFileSourceTypeToLake(const PhotosPo &sourcePhotoInfo,
+    const PhotosPo &targetPhotoInfo, std::shared_ptr<AccurateRefresh::AssetAccurateRefresh> &photoRefresh)
 {
-    int32_t position = photoInfo.position.value_or(1);
+    int32_t position = targetPhotoInfo.position.value_or(1);
     bool isValid = position != static_cast<int32_t>(PhotoPositionType::LOCAL_AND_CLOUD);
     CHECK_AND_RETURN_RET_INFO_LOG(
         isValid, E_OK, "UpdatePositionToBothAndFileSourceTypeToLake, position is correct, no need to update.");
@@ -395,7 +397,7 @@ int32_t MediaAssetsDao::UpdatePositionToBothAndFileSourceTypeToLake(
         "UpdatePositionToBothAndFileSourceTypeToLake Failed to get photoRefresh.");
 
     NativeRdb::AbsRdbPredicates predicates = NativeRdb::AbsRdbPredicates(PhotoColumn::PHOTOS_TABLE);
-    predicates.EqualTo(PhotoColumn::MEDIA_ID, photoInfo.fileId.value_or(0));
+    predicates.EqualTo(PhotoColumn::MEDIA_ID, targetPhotoInfo.fileId.value_or(0));
     NativeRdb::ValuesBucket values;
     values.PutInt(PhotoColumn::PHOTO_POSITION, static_cast<int32_t>(PhotoPositionType::LOCAL_AND_CLOUD));
     values.PutInt(PhotoColumn::PHOTO_FILE_SOURCE_TYPE, static_cast<int32_t>(FileSourceType::MEDIA_HO_LAKE));
@@ -405,13 +407,14 @@ int32_t MediaAssetsDao::UpdatePositionToBothAndFileSourceTypeToLake(
                    "ret: %{public}d, ChangedRows: %{public}d, cloudId: %{public}s",
         ret,
         changedRows,
-        photoInfo.cloudId.value_or("").c_str());
+        targetPhotoInfo.cloudId.value_or("").c_str());
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "Failed to UpdatePositionToBothAndFileSourceTypeToLake.");
     CHECK_AND_RETURN_RET_WARN_LOG(
-        changedRows > 0, ret, "UpdatePositionToBothAndFileSourceTypeToLake Check updateRows: %{public}d.", changedRows);
+        changedRows > 0, ret, "UpdatePositionToBothAndFileSourceTypeToLake updateRows: %{public}d.", changedRows);
     auto watch = MediaLibraryNotify::GetInstance();
     CHECK_AND_RETURN_RET_LOG(watch != nullptr, ret, "watch is nullptr");
-    watch->Notify(PhotoColumn::PHOTO_URI_PREFIX + to_string(photoInfo.fileId.value_or(0)), NotifyType::NOTIFY_UPDATE);
+    watch->Notify(
+        PhotoColumn::PHOTO_URI_PREFIX + to_string(targetPhotoInfo.fileId.value_or(0)), NotifyType::NOTIFY_UPDATE);
     return E_OK;
 }
 
@@ -440,5 +443,18 @@ int32_t MediaAssetsDao::DeletePhotoExtTable(const std::string &fileId)
     MEDIA_INFO_LOG("DeletePhotoExtTable completed, ret: %{public}d, photoId: %{public}s, deletedRows: %{public}d",
         ret, fileId.c_str(), deletedRows);
     return ret;
+}
+
+int32_t MediaAssetsDao::HandleSouthDeviceType(const PhotosPo &sourcePhotoInfo, const PhotosPo &targetPhotoInfo,
+                                              NativeRdb::ValuesBucket &values)
+{
+    int32_t sourceSouthDeviceType = sourcePhotoInfo.southDeviceType.value_or(0);
+    int32_t targetSouthDeviceType = targetPhotoInfo.southDeviceType.value_or(0);
+    bool isSourceValid = sourceSouthDeviceType != static_cast<int32_t>(SouthDeviceType::SOUTH_DEVICE_NULL);
+    bool isTargetValid = targetSouthDeviceType == static_cast<int32_t>(SouthDeviceType::SOUTH_DEVICE_NULL);
+    values.PutInt(PhotoColumn::PHOTO_SOUTH_DEVICE_TYPE, sourceSouthDeviceType);
+    MEDIA_DEBUG_LOG("HandleSouthDeviceType, sourceSouthDeviceType: %{public}d, targetSouthDeviceType: %{public}d",
+                    sourceSouthDeviceType, targetSouthDeviceType);
+    return E_OK;
 }
 }  // namespace OHOS::Media::Common
--- services/media_assets_manager/src/service/media_assets_recover_service.cpp
+++ services/media_assets_manager/src/service/media_assets_recover_service.cpp
@@ -311,7 +311,7 @@ int32_t MediaAssetsRecoverService::MediaAndMediaMergeLocalToCloudAsset(const Pho
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "MergeAssetFile fail, ret: %{public}d", ret);
     ret = this->RemoveAssetAndFile(sourcePhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "RemoveAssetAndFile fail, ret: %{public}d", ret);
-    ret = this->mediaAssetsDao_.UpdatePositionToBoth(targetPhotoInfo, photoRefresh);
+    ret = this->mediaAssetsDao_.UpdatePositionToBoth(sourcePhotoInfo, targetPhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "UpdatePositionToBoth fail, ret: %{public}d", ret);
     MEDIA_INFO_LOG(
         "MediaAndMediaMergeLocalToCloudAsset completed, "
@@ -355,7 +355,8 @@ int32_t MediaAssetsRecoverService::MediaAndLakeMergeLocalToCloudAsset(const Phot
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "MergeAssetFile fail, ret: %{public}d", ret);
     ret = this->RemoveAssetAndFile(sourcePhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "RemoveAssetAndFile fail, ret: %{public}d", ret);
-    ret = this->mediaAssetsDao_.UpdatePositionToBothAndFileSourceTypeToLake(targetPhotoInfo, photoRefresh);
+    ret = this->mediaAssetsDao_.UpdatePositionToBothAndFileSourceTypeToLake(
+                                                        sourcePhotoInfo, targetPhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(
         ret == E_OK, ret, "UpdatePositionToBothAndFileSourceTypeToLake fail, ret: %{public}d", ret);
     MEDIA_INFO_LOG(
@@ -381,7 +382,7 @@ int32_t MediaAssetsRecoverService::MediaAndLakeMergeLocalToHiddenCloudAsset(cons
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "MergeAssetFile fail, ret: %{public}d", ret);
     ret = this->RemoveAssetAndFile(sourcePhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "RemoveAssetAndFile fail, ret: %{public}d", ret);
-    ret = this->mediaAssetsDao_.UpdatePositionToBoth(targetPhotoInfo, photoRefresh);
+    ret = this->mediaAssetsDao_.UpdatePositionToBoth(sourcePhotoInfo, targetPhotoInfo, photoRefresh);
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, ret, "UpdatePositionToBoth fail, ret: %{public}d", ret);
     MEDIA_INFO_LOG(
         "MediaAndLakeMergeLocalToHiddenCloudAsset completed, "
