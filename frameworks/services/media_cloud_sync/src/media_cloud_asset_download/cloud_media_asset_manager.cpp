fa4200768271d4b25b51a2092d475e1f093da0a7
janneyli <liyang782@huawei.com>
2026-02-27 20:28:33 +0800

    update: 更新文件 cloud_media_asset_manager.cpp
    Signed-off-by: janneyli <liyang782@huawei.com>

--- frameworks/services/media_cloud_sync/src/media_cloud_asset_download/cloud_media_asset_manager.cpp
+++ frameworks/services/media_cloud_sync/src/media_cloud_asset_download/cloud_media_asset_manager.cpp
@@ -1118,7 +1118,9 @@ int32_t CloudMediaAssetManager::SetNetworkPolicyForBatchDownload(SetNetworkPolic
         this->batchDownloadResourcesTaskDao_.UpdateStatusAllFailAndAutoPauseToWaiting();
         MEDIA_INFO_LOG("BatchSelectFileDownload Set All LaunchBatchDownloadProcessor");
         BackgroundCloudBatchSelectedFileProcessor::SetBatchDownloadAddedFlag(true);
-        BackgroundCloudBatchSelectedFileProcessor::LaunchNetWorkBatchDownloadProcessor(); // 触发启动检查
+        if (reqBody.networkPolicy == static_cast<int32_t>(BatchDownloadNetWorkPolicyType::TYPE_CELLNET)) {
+            BackgroundCloudBatchSelectedFileProcessor::LaunchNetWorkBatchDownloadProcessor(); // 触发启动检查
+        }
         return ret;
     }
     std::vector<std::string> allFileIds;
@@ -1134,7 +1136,9 @@ int32_t CloudMediaAssetManager::SetNetworkPolicyForBatchDownload(SetNetworkPolic
     CHECK_AND_RETURN_RET_LOG(ret == E_OK, E_ERR, "UpdateNetworkPolicyDownloadTasks failed");
     MEDIA_INFO_LOG("BatchSelectFileDownload Set LaunchBatchDownloadProcessor");
     BackgroundCloudBatchSelectedFileProcessor::SetBatchDownloadAddedFlag(true);
-    BackgroundCloudBatchSelectedFileProcessor::LaunchNetWorkBatchDownloadProcessor(); // 触发启动检查
+    if (reqBody.networkPolicy == static_cast<int32_t>(BatchDownloadNetWorkPolicyType::TYPE_CELLNET)) {
+        BackgroundCloudBatchSelectedFileProcessor::LaunchNetWorkBatchDownloadProcessor(); // 触发启动检查
+    }
 #endif
     return E_OK;
 }
