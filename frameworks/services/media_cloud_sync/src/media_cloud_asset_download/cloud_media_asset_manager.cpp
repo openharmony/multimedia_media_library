9ef65b4730d955482360cf395f71b4a086484037
janneyli <liyang782@huawei.com>
2026-03-05 09:53:09 +0800

    update: 更新文件 cloud_media_asset_manager.cpp
    Signed-off-by: janneyli <liyang782@huawei.com>

--- frameworks/services/media_cloud_sync/src/media_cloud_asset_download/cloud_media_asset_manager.cpp
+++ frameworks/services/media_cloud_sync/src/media_cloud_asset_download/cloud_media_asset_manager.cpp
@@ -378,14 +378,14 @@ int32_t CloudMediaAssetManager::ReadyDataForDelete(std::vector<std::string> &fil
 }
 
 std::string CloudMediaAssetManager::GetEditDataDirPath(const std::string &path)
-{ 
+{
     CHECK_AND_RETURN_RET(path.length() >= ROOT_MEDIA_DIR.length(), "");
     return MEDIA_EDIT_DATA_DIR + path.substr(ROOT_MEDIA_DIR.length());
 }
 
 int32_t CloudMediaAssetManager::DeleteEditdata(const std::string &path)
 {
-    string editDataDirPath = GetEditDataDir(path);
+    string editDataDirPath = GetEditDataDirPath(path);
     CHECK_AND_RETURN_RET_LOG(!editDataDirPath.empty(), E_ERR, "Cannot get editPath, path: %{private}s", path.c_str());
     if (MediaFileUtils::IsFileExists(editDataDirPath)) {
         CHECK_AND_RETURN_RET_LOG(MediaFileUtils::DeleteDir(editDataDirPath), E_ERR,
