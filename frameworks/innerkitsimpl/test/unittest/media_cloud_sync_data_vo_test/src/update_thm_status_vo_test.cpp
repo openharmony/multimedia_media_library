82587cd354330b005bb2b2e531c7b8da2d9a9456
yanghaiyang7 <yanghaiyang7@huawei.com>
2026-03-10 17:24:54 +0800

    【可靠性加固】在 media_cloud_sync_data 模块，对 ReadXX 和 WriteXX 函数，添加 CHECK_AND_RETURN_RET_LOG 失败检查 V5
    Signed-off-by: yanghaiyang7 <yanghaiyang7@huawei.com>
    Change-Id: I417146da3033bf3d3848e2f24b9d39c9ff88821c

---
+++ frameworks/innerkitsimpl/test/unittest/media_cloud_sync_data_vo_test/src/update_thm_status_vo_test.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "update_thm_status_vo.h"
+
+#include <gtest/gtest.h>
+#include <message_parcel.h>
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS::Media::CloudSync;
+
+namespace OHOS::Media::CloudSync {
+
+class UpdateThmStatusVoTest : public testing::Test {};
+
+HWTEST_F(UpdateThmStatusVoTest, TC001_Unmarshalling_Success, TestSize.Level1)
+{
+    // 用例说明：测试序列化与反序列化；覆盖正常路径（触发条件：正常数据）；验证业务状态断言：反序列化后的数据与原始数据一致
+    UpdateThmStatusReqBody reqBody;
+    reqBody.cloudId = "cloud_id_123";
+    reqBody.thmStatus = 1;
+
+    OHOS::MessageParcel parcel;
+    bool ret = reqBody.Marshalling(parcel);
+    ASSERT_TRUE(ret);
+
+    parcel.RewindRead(0);
+    UpdateThmStatusReqBody restored;
+    ret = restored.Unmarshalling(parcel);
+    ASSERT_TRUE(ret);
+
+    EXPECT_EQ(restored.cloudId, reqBody.cloudId);
+    EXPECT_EQ(restored.thmStatus, reqBody.thmStatus);
+}
+
+}  // namespace OHOS::Media::CloudSync
