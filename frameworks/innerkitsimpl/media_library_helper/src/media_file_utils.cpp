d0b6eaf21cfa7a476a4563715a9f76c93bbf23c2
yaozh <yaozihao2@h-partners.com>
2026-03-16 15:34:58 +0800

    开放子弹时刻相关字段
    Signed-off-by: yaozh <yaozihao2@h-partners.com>

--- frameworks/innerkitsimpl/media_library_helper/src/media_file_utils.cpp
+++ frameworks/innerkitsimpl/media_library_helper/src/media_file_utils.cpp
@@ -2672,5 +2672,20 @@ bool MediaFileUtils::CheckLivePhoto4dStatus(int32_t livePhoto4dStatus)
     return livePhoto4dStatus >= static_cast<int32_t>(LivePhoto4dStatusType::TYPE_UNIDENTIFIED) &&
         livePhoto4dStatus <= static_cast<int32_t>(LivePhoto4dStatusType::TYPE_LIVEPHOTO_4D);
 }
+
+bool MediaFileUtils::IsValidUuid(const std::string& uuidStr)
+{
+    // UUID格式: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    // 8-4-4-4-12 格式的十六进制字符串
+    if (uuidStr.empty()) {
+        return true;
+    }
+    std::regex uuid_regex(
+        "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$",
+        std::regex_constants::icase  // 忽略大小写
+    );
+
+    return std::regex_match(uuidStr, uuid_regex);
+}
 } // namespace OHOS::Media
 // LCOV_EXCL_STOP
