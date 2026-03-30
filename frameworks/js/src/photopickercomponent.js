e114f845731e660a9dd8c26d155c0ef17d9c73e8
_huawei <sunhuawei5@huawei.com>
2026-03-30 17:53:46 +0800

    嵌入式picker删除默认mimetype值
    Signed-off-by: _huawei <sunhuawei5@huawei.com>

--- frameworks/js/src/photopickercomponent.js
+++ frameworks/js/src/photopickercomponent.js
@@ -865,11 +865,9 @@ export class PhotoPickerComponent extends ViewPU {
     }
 
     convertMIMETypeToFilterType(e) {
-        let o;
+        let o = undefined;
         if (PHOTO_VIEW_MIME_TYPE_MAP.has(e)) {
             o = PHOTO_VIEW_MIME_TYPE_MAP.get(e);
-        } else {
-            o = PHOTO_VIEW_MIME_TYPE_MAP.get('*/*');
         }
         console.info('PhotoPickerComponent convertMIMETypeToFilterType: ' + JSON.stringify(o));
         return o;
