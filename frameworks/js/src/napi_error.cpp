d45e0e7c4af8715bf9abe6e6da95e5a22b3f9336
qinquan5 <qinquan5@huawei.com>
2026-04-10 20:18:20 +0800

    媒体库API错误码整改
    Signed-off-by: qinquan5 <qinquan5@huawei.com>

--- frameworks/js/src/napi_error.cpp
+++ frameworks/js/src/napi_error.cpp
@@ -21,6 +21,54 @@ using namespace std;
 
 namespace OHOS {
 namespace Media {
+
+static std::string GetSpecificErrorMessage(int32_t err, const char *funcName)
+{
+    if (err != JS_INNER_FAIL) {
+        return "";
+    }
+
+    if (funcName == nullptr) {
+        return "";
+    }
+
+    std::string func(funcName);
+
+    if (func.find("Constructor") != std::string::npos ||
+        func.find("_constructor") != std::string::npos) {
+        return "Failed to create object.";
+    }
+    if (func.find("Get") != std::string::npos) {
+        if (func.find("Count") != std::string::npos) {
+            return "Failed to get count.";
+        }
+        if (func.find("Type") != std::string::npos) {
+            return "Failed to get type.";
+        }
+        return "Failed to get value.";
+    }
+    if (func.find("Set") != std::string::npos) {
+        return "Failed to set value.";
+    }
+    if (func.find("Create") != std::string::npos) {
+        return "Failed to create Object.";
+    }
+    if (func.find("UnWrap") != std::string::npos) {
+        return "Failed to unwrap Object.";
+    }
+    if (func.find("Array") != std::string::npos) {
+        return "Failed to process array.";
+    }
+    if (func.find("CallBack") != std::string::npos) {
+        return "Failed to process CallBack.";
+    }
+    if (func.find("Init") != std::string::npos) {
+        return "Failed to process CallBack.";
+    }
+
+    return "";
+}
+
 void NapiError::SetApiName(const std::string &Name)
 {
     apiName = Name;
@@ -35,6 +83,19 @@ void NapiError::SaveError(int32_t ret)
 {
     if (ret < 0) {
         error = MediaLibraryNapiUtils::TransErrorCode(apiName, ret);
+        if (error == JS_E_FILE_EXTENSION) {
+            if (ret == E_CHECK_MEDIATYPE_MATCH_EXTENSION_FAIL) {
+                errorMsg = "File extension does not match the media type";
+            } else if (ret == E_CHECK_MEDIATYPE_FAIL) {
+                errorMsg = "Media type does not match relative path";
+            }   else if (ret == E_CHECK_EXTENSION_FAIL) {
+                errorMsg = "File extension does not match directory type";
+            }
+        } else if (error == JS_INNER_FAIL) {
+            if (ret == E_FILE_OPER_FAIL) {
+                errorMsg = "File operation failed";
+            }
+        }
     }
 }
 
@@ -42,6 +103,19 @@ void NapiError::SaveRealErr(int32_t ret)
 {
     if (ret < 0) {
         realErr = MediaLibraryNapiUtils::TransErrorCode(apiName, ret);
+        if (realErr == JS_E_FILE_EXTENSION && errorMsg.empty()) {
+            if (ret == E_CHECK_MEDIATYPE_MATCH_EXTENSION_FAIL) {
+                errorMsg = "File extension does not match the media type";
+            } else if (ret == E_CHECK_MEDIATYPE_FAIL) {
+                errorMsg = "Media type does not match relative path";
+            }   else if (ret == E_CHECK_EXTENSION_FAIL) {
+                errorMsg = "File extension does not match directory type";
+            }
+        } else if (realErr == JS_INNER_FAIL && errorMsg.empty()) {
+            if (ret == E_FILE_OPER_FAIL) {
+                errorMsg = "File operation failed";
+            }
+        }
     }
     NAPI_ERR_LOG("SaveRealErr errCode:%{public}d realErr:%{public}d", ret, realErr);
 }
@@ -49,7 +123,7 @@ void NapiError::SaveRealErr(int32_t ret)
 void NapiError::HandleError(napi_env env, napi_value &errorObj)
 {
     // deal with context->error
-    MediaLibraryNapiUtils::HandleError(env, error, errorObj, apiName, realErr);
+    MediaLibraryNapiUtils::HandleError(env, error, errorObj, apiName, realErr, errorMsg);
 }
 
 void NapiError::ThrowError(napi_env env, int32_t err, const std::string &errMsg)
@@ -73,6 +147,13 @@ void NapiError::ThrowError(napi_env env, int32_t err, const char *funcName, int3
         message = "operation not support";
         if (jsErrMap.count(err) > 0) {
             message = jsErrMap.at(err);
+        }
+
+        if (err == JS_INNER_FAIL) {
+            std::string specificMsg = GetSpecificErrorMessage(err, funcName);
+            if (!specificMsg.empty()) {
+                message = specificMsg;
+            }
         }
     }
 
