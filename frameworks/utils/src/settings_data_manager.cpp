a395bc8010c16ec8bdbeb963b9e7f3fb60dc35cf
ericyangbit <yangyang581@huawei.com>
2026-04-14 15:27:00 +0800

    multiusesr fix
    Signed-off-by: ericyangbit <yangyang581@huawei.com>

--- frameworks/utils/src/settings_data_manager.cpp
+++ frameworks/utils/src/settings_data_manager.cpp
@@ -22,8 +22,10 @@
 #include "datashare_helper.h"
 #include "datashare_errno.h"
 #include "datashare_result_set.h"
+#include "iservice_registry.h"
 #include "medialibrary_errno.h"
 #include "media_log.h"
+#include "media_uri_utils.h"
 #ifdef MEDIA_LIBRARY_MEMOSPACE_SERVICE_SUPPORT
 #include "hdc_device_id.h"
 #endif
@@ -32,9 +34,16 @@ namespace OHOS::Media {
 inline static const std::string SETTING_DATA_QUERY_URI = "datashareproxy://";
 inline static const std::string SETTING_DATA_COMMON_URI =
     "datashare:///com.ohos.settingsdata/entry/settingsdata/SETTINGSDATA?Proxy=true";
+inline static const std::string PHOTOS_USER_PROPERTY_INIT_URI =
+    "datashare:///com.huawei.hmos.photos.provider.userPropertySettings?init";
 inline static const std::string PHOTOS_SYNC_SWITCH_KEY = "photos_sync_options";
+inline static const std::string PHOTOS_USER_PROPERTY_URI =
+    "datashare:///com.huawei.hmos.photos.provider.userPropertySettings";
+inline static const std::string PHOTOS_SYNC_SWITCH_USER_KEY = "photos_sync_options_user";
 inline static const std::string ALL_PHOTOS_ALBUM_UPLOAD = "photos_all_album_upload";
 inline static const std::string ALL_PHOTOS_ALBUM_UPLOAD_OFF = "0";
+static constexpr int32_t BASE_USER_RANGE = 200000;
+constexpr int PHOTOS_STORAGE_MANAGER_ID = 5003;
 
 class MediaSettingDataHelper {
 public:
@@ -78,6 +87,12 @@ private:
     std::shared_ptr<DataShare::DataShareHelper> dataShareHelper_;
 };
 
+static int32_t GetUserId()
+{
+    int32_t uid = static_cast<int32_t>(getuid());
+    return uid / BASE_USER_RANGE;
+}
+
 static SwitchStatus StringToSwitchStatus(const std::string& value)
 {
     static const std::unordered_map<std::string, SwitchStatus> STRING_SWITCH_STATUS_MAP = {
@@ -90,17 +105,38 @@ static SwitchStatus StringToSwitchStatus(const std::string& value)
     return STRING_SWITCH_STATUS_MAP.at(value);
 }
 
-SwitchStatus SettingsDataManager::GetPhotosSyncSwitchStatus()
+std::optional<SwitchStatus> SettingsDataManager::GetPhotosSyncSwitchUserStatus()
 {
     std::string value;
-    auto ret = QueryParamInSettingData(PHOTOS_SYNC_SWITCH_KEY, value);
-    if (ret != E_OK) {
-        return SwitchStatus::NONE;
+    auto ret = QueryParamInSettingData(PHOTOS_SYNC_SWITCH_USER_KEY, value);
+    if (ret == E_OK) {
+        return std::optional<SwitchStatus>(StringToSwitchStatus(value));
     }
-    return StringToSwitchStatus(value);
+    return std::nullopt;
 }
 
-int32_t SettingsDataManager::QueryParamInSettingData(const std::string &key, std::string &value)
+SwitchStatus SettingsDataManager::GetPhotosSyncSwitchStatus()
+{
+    auto status = GetPhotosSyncSwitchUserStatus();
+    if (status.has_value()) {
+        return status.value();
+    }
+
+    MEDIA_INFO_LOG("query user sync switch failed, notify photos init");
+    int32_t notifyRet = NotifyPhotosSyncSwitchInitByUpdate();
+    if (notifyRet == E_OK) {
+        status = GetPhotosSyncSwitchUserStatus();
+        if (status.has_value()) {
+            return status.value();
+        }
+        MEDIA_WARN_LOG("query user sync switch after init still failed");
+    } else {
+        MEDIA_WARN_LOG("notify photos init by datashare update failed, ret: %{public}d", notifyRet);
+    }
+    return GetPhotosSyncSwitchDeviceStatus();
+}
+
+static int32_t QueryParamInDeviceSettingData(const std::string &key, std::string &value)
 {
     MediaSettingDataHelper dataShareHelper;
     if (!dataShareHelper) {
@@ -108,7 +144,7 @@ int32_t SettingsDataManager::QueryParamInSettingData(const std::string &key, std
         return E_DB_FAIL;
     }
 
-    MEDIA_INFO_LOG("Query key: %{public}s", key.c_str());
+    MEDIA_DEBUG_LOG("Query key: %{public}s", key.c_str());
     DataShare::DataSharePredicates predicates;
     predicates.EqualTo("KEYWORD", key);
     std::vector<std::string> columns = {"value"};
@@ -125,7 +161,80 @@ int32_t SettingsDataManager::QueryParamInSettingData(const std::string &key, std
     CHECK_AND_RETURN_RET_LOG(ret == DataShare::E_OK, ret, "GetColumnIndex failed, err: %{public}d", ret);
     ret = resultSet->GetString(columnIndex, value);
     CHECK_AND_RETURN_RET_LOG(ret == DataShare::E_OK, ret, "GetString failed, err: %{public}d", ret);
-    MEDIA_INFO_LOG("Query success, value: %{public}s", value.c_str());
+    MEDIA_DEBUG_LOG("Query success, value: %{public}s", value.c_str());
+    return E_OK;
+}
+
+SwitchStatus SettingsDataManager::GetPhotosSyncSwitchDeviceStatus()
+{
+    std::string value;
+    auto ret = QueryParamInDeviceSettingData(PHOTOS_SYNC_SWITCH_KEY, value);
+    if (ret != E_OK) {
+        return SwitchStatus::NONE;
+    }
+    return StringToSwitchStatus(value);
+}
+
+int32_t SettingsDataManager::NotifyPhotosSyncSwitchInitByUpdate()
+{
+    auto samgr = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (samgr == nullptr) {
+        MEDIA_ERR_LOG("Samgr is nullptr");
+        return E_DB_FAIL;
+    }
+
+    auto object = samgr->GetSystemAbility(PHOTOS_STORAGE_MANAGER_ID);
+    if (object == nullptr) {
+        MEDIA_ERR_LOG("GetSystemAbility failed");
+        return E_DB_FAIL;
+    }
+
+    auto dataShareHelper = DataShare::DataShareHelper::Creator(object, PHOTOS_USER_PROPERTY_URI);
+    if (dataShareHelper == nullptr) {
+        MEDIA_ERR_LOG("dataShareHelper = nullptr");
+        return E_DB_FAIL;
+    }
+
+    DataShare::DataSharePredicates predicates;
+    DataShare::DataShareValuesBucket valuesBucket;
+    DataShare::DataShareValueObject valueObject(1);
+    valuesBucket.Put("init", valueObject);
+    Uri uri(PHOTOS_USER_PROPERTY_INIT_URI);
+    auto ret = dataShareHelper->UpdateEx(uri, predicates, valuesBucket);
+    dataShareHelper->Release();
+    CHECK_AND_RETURN_RET_LOG(ret.first == DataShare::E_OK, ret.first,
+        "notify photos init failed, err: %{public}d", ret.first);
+    return E_OK;
+}
+
+int32_t SettingsDataManager::QueryParamInSettingData(const std::string &key, std::string &value)
+{
+    MediaSettingDataHelper dataShareHelper;
+    if (!dataShareHelper) {
+        MEDIA_ERR_LOG("failed to init dataShareHelper");
+        return E_DB_FAIL;
+    }
+
+    MEDIA_DEBUG_LOG("Query key: %{public}s", key.c_str());
+    DataShare::DataSharePredicates predicates;
+    predicates.EqualTo("KEYWORD", key);
+    std::vector<std::string> columns = {"value"};
+    int32_t userId = GetUserId();
+    std::string queryUri = "datashare:///com.ohos.settingsdata/entry/settingsdata/USER_SETTINGSDATA_" +
+                           std::to_string(userId) + "?Proxy=true&key=" + key;
+    Uri uri(queryUri);
+
+    std::shared_ptr<DataShare::DataShareResultSet> resultSet = dataShareHelper->Query(uri, predicates, columns);
+    CHECK_AND_RETURN_RET_LOG(resultSet != nullptr, E_DB_FAIL, "resultSet == nullptr");
+    auto ret = resultSet->GoToFirstRow();
+    CHECK_AND_RETURN_RET_LOG(ret == DataShare::E_OK, ret, "GoToFirstRow failed, err: %{public}d", ret);
+
+    int32_t columnIndex = 0;
+    ret = resultSet->GetColumnIndex("value", columnIndex);
+    CHECK_AND_RETURN_RET_LOG(ret == DataShare::E_OK, ret, "GetColumnIndex failed, err: %{public}d", ret);
+    ret = resultSet->GetString(columnIndex, value);
+    CHECK_AND_RETURN_RET_LOG(ret == DataShare::E_OK, ret, "GetString failed, err: %{public}d", ret);
+    MEDIA_DEBUG_LOG("Query success, value: %{public}s", value.c_str());
     return E_OK;
 }
 
@@ -181,7 +290,9 @@ int32_t SettingsDataManager::UpdateParamInSettingData(const std::string &key, co
     DataShare::DataShareValuesBucket valuesBucket;
     valuesBucket.Put("VALUE", value); // static_cast<int32_t>(value)
 
-    std::string updateUri = SETTING_DATA_COMMON_URI + "&key=" + key;
+    int32_t userId = GetUserId();
+    std::string updateUri = "datashare:///com.ohos.settingsdata/entry/settingsdata/USER_SETTINGSDATA_" +
+                            std::to_string(userId) + "?Proxy=true&key=" + key;
     Uri uri(updateUri);
     auto ret = dataShareHelper->UpdateEx(uri, predicates, valuesBucket);
     CHECK_AND_RETURN_RET_LOG(ret.first == DataShare::E_OK, ret.first, "update failed, err: %{public}d", ret.first);
@@ -200,7 +311,9 @@ int32_t SettingsDataManager::InsertParamInSettingData(const std::string &key, co
     valuesBucket.Put("KEYWORD", key);
     valuesBucket.Put("VALUE", value); // static_cast<int32_t>(value)
 
-    std::string insertUri = SETTING_DATA_COMMON_URI + "&key=" + key;
+    int32_t userId = GetUserId();
+    std::string insertUri = "datashare:///com.ohos.settingsdata/entry/settingsdata/USER_SETTINGSDATA_" +
+                            std::to_string(userId) + "?Proxy=true&key=" + key;
     Uri uri(insertUri);
     std::string result;
     auto ret = dataShareHelper->InsertEx(uri, valuesBucket);
