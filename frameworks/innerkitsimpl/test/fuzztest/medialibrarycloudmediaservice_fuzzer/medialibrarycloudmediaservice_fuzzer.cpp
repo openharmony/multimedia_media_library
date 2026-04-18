d4f185eec861926faf83f7c1d458907aca3bc73c
qinchuan-banknote <qinchuanchao@h-partners.com>
2026-04-18 10:32:59 +0800

    MediaLibraryCloudMediaServiceFuzzTest超时18H、MediaLibraryMediaAlbumsControllerService2FuzzTest超时25H
    Signed-off-by: qinchuan-banknote <qinchuanchao@h-partners.com>

--- frameworks/innerkitsimpl/test/fuzztest/medialibrarycloudmediaservice_fuzzer/medialibrarycloudmediaservice_fuzzer.cpp
+++ frameworks/innerkitsimpl/test/fuzztest/medialibrarycloudmediaservice_fuzzer/medialibrarycloudmediaservice_fuzzer.cpp
@@ -20,9 +20,7 @@
 #include <string>
 #include <fstream>
 #include <fuzzer/FuzzedDataProvider.h>
-#define private public
 #include "cloud_media_photos_service.h"
-#undef private
 #include "media_log.h"
 #include "cloud_media_operation_code.h"
 #include "media_upgrade.h"
@@ -33,7 +31,12 @@ using namespace OHOS::Media;
 using namespace OHOS::Media::CloudSync;
 const int32_t FILEID = 1;
 const int32_t NUM_BYTES = 1;
-const int32_t VECTOR_SIZE = 5;
+static const int32_t MXA_CLEAN_TYPE = 1;
+static const int32_t MIN_PHOTO_POSITION_TYPE = -1;
+static const int32_t MAX_PHOTO_POSITION_TYPE = 3;
+static const int32_t MIN_SYNC_STATUS_TYPE = -1;
+static const int32_t MAX_SYNC_STATUS_TYPE = 2;
+static const int32_t MAX_MEDIA_TYPE = 14;
 static const int32_t MAX_THUMB_SATUS = 3;
 static const int32_t MAX_PROPERTIES_ROTATE = 8;
 static const int32_t MAX_DIRTY_TYPE = 8;
@@ -42,7 +45,6 @@ const int32_t SEED_SIZE = 1024;
 const string PHOTOS_TABLE = "Photos";
 FuzzedDataProvider* provider;
 std::shared_ptr<Media::MediaLibraryRdbStore> g_rdbStore;
-shared_ptr<CloudMediaPhotosService> cloudMediaPhotosService = make_shared<CloudMediaPhotosService>();
 
 static inline DirtyType FuzzDirtyType()
 {
@@ -56,22 +58,45 @@ static inline CloudSync::ThumbState FuzzThumbState()
     return static_cast<CloudSync::ThumbState>(value);
 }
 
+static inline MediaType FuzzMediaType()
+{
+    int32_t value = provider->ConsumeIntegralInRange<int32_t>(0, MAX_MEDIA_TYPE);
+    return static_cast<MediaType>(value);
+}
+
+static inline PhotoPositionType FuzzPhotoPositionType()
+{
+    int32_t value = provider->ConsumeIntegralInRange<int32_t>(MIN_PHOTO_POSITION_TYPE, MAX_PHOTO_POSITION_TYPE);
+    return static_cast<PhotoPositionType>(value);
+}
+
+static inline SyncStatusType FuzzSyncStatusType()
+{
+    int32_t value = provider->ConsumeIntegralInRange<int32_t>(MIN_SYNC_STATUS_TYPE, MAX_SYNC_STATUS_TYPE);
+    return static_cast<SyncStatusType>(value);
+}
+
+static inline Media::CleanType FuzzCleanType()
+{
+    int32_t value = provider->ConsumeIntegralInRange<int32_t>(0, MXA_CLEAN_TYPE);
+    return static_cast<CleanType>(value);
+}
+
 static int32_t InsertQueryAsset()
 {
     if (g_rdbStore == nullptr) {
         return E_ERR;
     }
     NativeRdb::ValuesBucket values;
-    values.PutInt(PhotoColumn::MEDIA_TYPE, static_cast<int32_t>(MediaType::MEDIA_TYPE_VIDEO));
-    values.PutInt(PhotoColumn::MEDIA_ID, 1);
-    values.PutInt(PhotoColumn::PHOTO_POSITION, static_cast<int32_t>(PhotoPositionType::CLOUD));
-    values.PutInt(PhotoColumn::PHOTO_SYNC_STATUS, static_cast<int32_t>(SyncStatusType::TYPE_VISIBLE));
+    values.PutInt(PhotoColumn::MEDIA_TYPE, static_cast<int32_t>(FuzzMediaType()));
+    values.PutInt(PhotoColumn::PHOTO_POSITION, static_cast<int32_t>(FuzzPhotoPositionType()));
+    values.PutInt(PhotoColumn::PHOTO_SYNC_STATUS, static_cast<int32_t>(FuzzSyncStatusType()));
     values.PutInt(PhotoColumn::PHOTO_THUMB_STATUS, static_cast<int32_t>(FuzzThumbState()));
-    values.PutInt(PhotoColumn::PHOTO_CLEAN_FLAG, static_cast<int32_t>(CleanType::TYPE_NOT_CLEAN));
+    values.PutInt(PhotoColumn::PHOTO_CLEAN_FLAG, static_cast<int32_t>(FuzzCleanType()));
     values.PutLong(PhotoColumn::MEDIA_DATE_TAKEN, provider->ConsumeIntegral<int64_t>());
-    values.PutString(PhotoColumn::MEDIA_FILE_PATH, "/storage/cloud/files/Photo/16/IMG_1744362716_000.jpg");
     int64_t fileId = 0;
-    g_rdbStore->Insert(fileId, PHOTOS_TABLE, values);
+    int32_t ret = g_rdbStore->Insert(fileId, PHOTOS_TABLE, values);
+    CHECK_AND_RETURN_RET_LOG(ret == NativeRdb::E_OK, E_ERR, "g_rdbStore failed to insert ret=%{public}d", ret);
     return static_cast<int32_t>(fileId);
 }
 
@@ -132,167 +157,51 @@ static CloudMediaPullDataDto FuzzCloudMediaPullDataDto(string &cloudId)
     return pullData;
 }
 
-static void OnRecordFailedFuzzer()
+static void HandleRecordFuzzer()
 {
-    if (cloudMediaPhotosService == nullptr) {
-        MEDIA_ERR_LOG("cloudMediaPhotosService is nuulptr");
-        return;
-    }
-    auto photoRefresh = std::make_shared<AccurateRefresh::AssetAccurateRefresh>();
-    PhotosDto photo;
-    photo.serverErrorCode = ServerErrorCode::NETWORK_ERROR;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    photo.serverErrorCode = ServerErrorCode::UID_EMPTY;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    photo.serverErrorCode = ServerErrorCode::INVALID_LOCK_PARAM;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    photo.serverErrorCode = ServerErrorCode::RESPONSE_TIME_OUT;
-    photo.errorType = ErrorType::TYPE_UNKNOWN;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    photo.errorType = ErrorType::TYPE_NOT_NEED_RETRY;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    photo.serverErrorCode = ServerErrorCode::NO_NETWORK;
-    photo.errorType = ErrorType::TYPE_UNKNOWN;
-    CloudErrorDetail cloudErrorDetail;
-    cloudErrorDetail.detailCode = ErrorDetailCode::SPACE_FULL;
-    photo.errorDetails = { cloudErrorDetail };
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    cloudErrorDetail.detailCode = ErrorDetailCode::BUSINESS_MODEL_CHANGE_DATA_UPLOAD_FORBIDDEN;
-    photo.errorDetails = { cloudErrorDetail };
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    cloudErrorDetail.detailCode = ErrorDetailCode::SAME_FILENAME_NOT_ALLOWED;
-    photo.errorDetails = { cloudErrorDetail };
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    cloudErrorDetail.detailCode = ErrorDetailCode::CONTENT_NOT_FIND;
-    photo.errorDetails = { cloudErrorDetail };
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-
-    cloudErrorDetail.detailCode = ErrorDetailCode::FILE_REFERENCED;
-    photo.errorDetails = { cloudErrorDetail };
-    photo.errorType = ErrorType::TYPE_NOT_NEED_RETRY;
-    cloudMediaPhotosService->OnRecordFailed(photo, photoRefresh);
-}
-
-static void PullDeleteAndUpdateFuzzer()
-{
-    if (cloudMediaPhotosService == nullptr) {
-        MEDIA_ERR_LOG("cloudMediaPhotosService is nuulptr");
-        return;
-    }
-    auto photoRefresh = std::make_shared<AccurateRefresh::AssetAccurateRefresh>();
-    std::string cloudId = provider->ConsumeBytesAsString(NUM_BYTES);
-    CloudMediaPullDataDto pullData = FuzzCloudMediaPullDataDto(cloudId);
-    std::set<string> refreshAlbums;
-    vector<PhotosDto> fdirtyData;
-    vector<int32_t> stats(VECTOR_SIZE, 0);
-    cloudMediaPhotosService->PullDelete(pullData, refreshAlbums, photoRefresh);
-    cloudMediaPhotosService->PullUpdate(pullData, refreshAlbums, fdirtyData, stats, photoRefresh);
-}
-
-static void GetMergeDataMapFuzzer()
-{
-    if (cloudMediaPhotosService == nullptr) {
-        MEDIA_ERR_LOG("cloudMediaPhotosService is nuulptr");
-        return;
-    }
-    vector<CloudMediaPullDataDto> pullDatas;
-    std::string cloudId = provider->ConsumeBytesAsString(NUM_BYTES);
-    CloudMediaPullDataDto pullData = FuzzCloudMediaPullDataDto(cloudId);
-    pullDatas.emplace_back(pullData);
-    std::map<string, KeyData> mergeDataMap;
-    cloudMediaPhotosService->GetMergeDataMap(pullDatas, mergeDataMap);
-}
-
-static void OnRecordFuzzer()
-{
-    if (cloudMediaPhotosService == nullptr) {
-        MEDIA_ERR_LOG("cloudMediaPhotosService is nuulptr");
-        return;
-    }
-    vector<PhotosDto> photos;
-    PhotosDto photo;
-    photos.emplace_back(photo);
-    int32_t failedSize = -1;
-    InsertQueryAsset();
-    cloudMediaPhotosService->OnCreateRecords(photos, failedSize);
-    cloudMediaPhotosService->OnMdirtyRecords(photos, failedSize);
-    cloudMediaPhotosService->OnFdirtyRecords(photos, failedSize);
-    cloudMediaPhotosService->OnDeleteRecords(photos, failedSize);
-    cloudMediaPhotosService->OnCopyRecords(photos, failedSize);
-
-    photos.clear();
-    photo = FuzzPhotosDto();
-    photos.emplace_back(photo);
-    cloudMediaPhotosService->OnCreateRecords(photos, failedSize);
-    cloudMediaPhotosService->OnMdirtyRecords(photos, failedSize);
-    cloudMediaPhotosService->OnFdirtyRecords(photos, failedSize);
-    cloudMediaPhotosService->OnDeleteRecords(photos, failedSize);
-    cloudMediaPhotosService->OnCopyRecords(photos, failedSize);
-}
-
-static void CloudMediadPhotoServiceFuzzer()
-{
-    CHECK_AND_RETURN_LOG(cloudMediaPhotosService != nullptr, "cloudMediaPhotosService is nuulptr");
-
-    MediaOperateResult optRet = {"", 0, ""};
-    cloudMediaPhotosService->OnStartSync();
-    cloudMediaPhotosService->OnCompleteSync();
-    cloudMediaPhotosService->OnCompletePull(optRet);
-    cloudMediaPhotosService->OnCompletePush();
-    cloudMediaPhotosService->OnCompleteCheck();
-    PullDeleteAndUpdateFuzzer();
-    GetMergeDataMapFuzzer();
-
+    CloudMediaPhotosService service;
     string cloudId = "default-album-2";
     vector<string> cloudIds = { cloudId };
-    int32_t recordsSize = provider->ConsumeIntegral<uint32_t>() & 0xf;
-    vector<PhotosPo> photosPo;
-    cloudMediaPhotosService->GetCreatedRecords(recordsSize, photosPo);
-    cloudMediaPhotosService->GetMetaModifiedRecords(recordsSize, photosPo);
-    cloudMediaPhotosService->GetFileModifiedRecords(recordsSize, photosPo);
-    cloudMediaPhotosService->GetCopyRecords(recordsSize, photosPo);
-    cloudMediaPhotosService->GetRetryRecords(cloudIds);
-
-    PhotosDto photo;
-    OnRecordFuzzer();
-    OnRecordFailedFuzzer();
-
-    cloudIds.clear();
     cloudIds.emplace_back(cloudId);
     std::map<string, CloudMediaPullDataDto> cloudIdRelativeMap = { {"default-album-2", CloudMediaPullDataDto()} };
     vector<PhotosDto> newData;
     vector<PhotosDto> fdirtyData;
-    vector<int32_t> stats(VECTOR_SIZE, 0);
+    vector<int32_t> stats(5, 0);
     vector<string> failedRecords;
-    cloudMediaPhotosService->HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
+    service.HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
 
     cloudIdRelativeMap = { {"default-album-2", FuzzCloudMediaPullDataDto(cloudId)} };
-    cloudMediaPhotosService->HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
+    service.HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
 
     CloudMediaPullDataDto pullData = FuzzCloudMediaPullDataDto(cloudId);
     pullData.basicIsDelete = true;
     cloudIdRelativeMap = { {"default-album-2", pullData} };
-    cloudMediaPhotosService->HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
+    service.HandleRecord(cloudIds, cloudIdRelativeMap, newData, fdirtyData, stats, failedRecords);
+}
 
+static void ConvertPullDataToPhotosDtoFuzzer()
+{
+    CloudMediaPhotosService service;
+    PhotosDto photo;
+    string cloudId = provider->ConsumeBytesAsString(NUM_BYTES);
     CloudMediaPullDataDto data = FuzzCloudMediaPullDataDto(cloudId);
-    cloudMediaPhotosService->ConvertPullDataToPhotosDto(data, photo);
-    vector<CloudMediaPullDataDto> pullDatas = { FuzzCloudMediaPullDataDto(cloudId) };
-    cloudMediaPhotosService->OnDentryFileInsert(pullDatas, failedRecords);
+    service.ConvertPullDataToPhotosDto(data, photo);
+}
 
+static void OnCreateRecordSuccessFuzzer()
+{
+    CloudMediaPhotosService service;
     auto photoRefresh = std::make_shared<AccurateRefresh::AssetAccurateRefresh>();
     PhotosDto record = FuzzPhotosDto();
-    cloudMediaPhotosService->OnCreateRecordSuccess(record, photoRefresh);
+    service.OnCreateRecordSuccess(record, photoRefresh);
+}
 
-    record.localId = 1;
-    cloudMediaPhotosService->OnCreateRecordSuccess(record, photoRefresh);
+static void CloudMediadPhotoServiceFuzzer()
+{
+    InsertQueryAsset();
+    HandleRecordFuzzer();
+    ConvertPullDataToPhotosDtoFuzzer();
+    OnCreateRecordSuccessFuzzer();
 }
 
 void SetTables()
@@ -356,11 +265,10 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     /* Run your code on data */
+    CHECK_AND_RETURN_RET_LOG(data != nullptr, OHOS::Media::E_ERR, "data is nullptr");
     FuzzedDataProvider fdp(data, size);
     OHOS::provider = &fdp;
-    if (data == nullptr) {
-        return 0;
-    }
+    CHECK_AND_RETURN_RET_LOG(OHOS::provider != nullptr, OHOS::Media::E_ERR, "provider is nullptr");
 
     OHOS::CloudMediadPhotoServiceFuzzer();
     return 0;
