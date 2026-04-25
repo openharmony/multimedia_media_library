3cb76df9021b006b1cda73fc1ffa4f151d13faa7
cilinf <fumenglei@huawei.com>
2026-04-25 12:10:47 +0800

    fix
    Signed-off-by: cilinf <fumenglei@huawei.com>

--- interfaces/inner_api/media_library_helper/include/media_change_effect.h
+++ interfaces/inner_api/media_library_helper/include/media_change_effect.h
@@ -27,9 +27,9 @@ namespace Media {
 class MediaChangeEffect {
 public:
     static int32_t TakeEffect(const string &inputPath, const string &outputPath, string &editdata, int32_t quality);
-    static int32_t TakeEffectForPicture(std::shared_ptr<Media::Picture> &inPicture,
+    static int32_t TakeEffectForPicture(std::shared_ptr<Media::Picture> &inPicture, string &editdata);
+    static int32_t TakeEffectRevert(const string &inputPath, const string &outputPath,
         string &editdata, int32_t quality);
-    static int32_t TakeEffectRevert(const string &inputPath, const string &outputPath, string &editdata);
 };
  
 } // end of namespace
