74b09cd1944ad666f948e76ff5677f03b9a16f70
lizhen <lizhen236@huawei.com>
2026-05-13 21:08:03 +0800

    稳定性问题修复
    Signed-off-by: lizhen <lizhen236@huawei.com>

--- services/media_file_monitor/common/src/media_lake_clone_event_manager.cpp
+++ services/media_file_monitor/common/src/media_lake_clone_event_manager.cpp
@@ -159,7 +159,7 @@ void MediaLakeCloneEventManager::RunGlobalScanner()
 
     MEDIA_INFO_LOG("LakeClone: Start RunGlobalScanner");
     auto scannerFunc = []() {
-        GlobalScanner::GetInstance().RunLakeScan(std::string(LAKE_ROOT_PATH), false);
+        GlobalScanner::GetInstance().RunLakeScan(std::string(LAKE_ROOT_PATH), false, true);
     };
     Media::thread myThread("LakeClone", scannerFunc);
     if (myThread.is_invalid()) {
