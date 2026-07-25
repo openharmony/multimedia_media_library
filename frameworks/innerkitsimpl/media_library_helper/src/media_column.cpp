68a38169d17c2814ab71b749c6e8fbdb1d407c8c
cilinf <fumenglei@huawei.com>
2026-07-25 11:40:20 +0800

    PHOTO_COLUMNS添加转码文件大小字段
    Signed-off-by: cilinf <fumenglei@huawei.com>

--- frameworks/innerkitsimpl/media_library_helper/src/media_column.cpp
+++ frameworks/innerkitsimpl/media_library_helper/src/media_column.cpp
@@ -272,7 +272,8 @@ const std::set<std::string> PhotoColumn::PHOTO_COLUMNS = {
     PhotoColumn::UNIQUE_ID, PhotoColumn::MOVING_PHOTO_LIVEPHOTO_4D_STATUS,
     PhotoColumn::MOVING_PHOTO_LIVEPHOTO_4D_LATEST_PAIR, PhotoColumn::PHOTO_HIDDEN_TIME, PhotoColumn::LOCAL_ASSET_SIZE,
     PhotoColumn::PHOTO_FILE_HIDDEN, PhotoColumn::PHOTO_NEED_THUMBNAIL, PhotoColumn::ATTACHMENT_SIZE,
-    PhotoColumn::PHOTO_LCD_FILE_SIZE, PhotoColumn::COMPRESSION_QUALITY,
+    PhotoColumn::PHOTO_LCD_FILE_SIZE, PhotoColumn::COMPRESSION_QUALITY, PhotoColumn::PHOTO_TRANS_CODE_FILE_SIZE,
+    PhotoColumn::PHOTO_TRANSCODE_TIME,
 };
 
 bool PhotoColumn::IsPhotoColumn(const std::string &columnName)
