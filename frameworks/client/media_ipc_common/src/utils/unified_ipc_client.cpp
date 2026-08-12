91bf3711c2e9123168772788ac3660b949e999d7
cilinf <fumenglei@huawei.com>
2026-08-12 17:57:40 +0800

    fix: 修复userfileclient 5合1重构的编译问题
    1. 创建unified_ipc_client.cpp合并user_inner_ipc_client.cpp和user_define_ipc_client.cpp的实现
    2. 删除孤立的user_define_ipc_client.cpp和被替代的user_inner_ipc_client.cpp
    3. 移除两个测试BUILD.gn中对已删除userfile_client.cpp的引用，改为依赖media_library_client
    4. 修复media_analysis_data_manager_dto_test中过期的include路径
    5. 修复9个fuzz测试BUILD.gn引用不存在的media_ipc_common构建目标
    
    Signed-off-by: cilinf <fumenglei@huawei.com>

--- frameworks/client/media_ipc_common/src/utils/unified_ipc_client.cpp
+++ frameworks/client/media_ipc_common/src/utils/unified_ipc_client.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,47 +14,55 @@
  */
 #define MLOG_TAG "Media_IPC"
 
-#include "user_define_ipc_client.h"
+#include "unified_ipc_client.h"
 
 #include "iservice_registry.h"
+#include "userfile_client.h"
 
 namespace OHOS::Media::IPC {
 static const std::u16string DESCRIPTOR = u"OHOS.DataShare.IDataShare";
 static const int STORAGE_MANAGER_MANAGER_ID = 5003;
-UserDefineIPCClient &UserDefineIPCClient::SetTraceId(const std::string &traceId)
+
+UnifiedIPCClient &UnifiedIPCClient::SetTraceId(const std::string &traceId)
 {
     this->traceId_ = traceId;
     return *this;
 }
 
-std::string UserDefineIPCClient::GetTraceId() const
+std::string UnifiedIPCClient::GetTraceId() const
 {
     return this->traceId_;
 }
 
-UserDefineIPCClient &UserDefineIPCClient::SetUserId(const int32_t &userId)
+UnifiedIPCClient &UnifiedIPCClient::SetUserId(const int32_t &userId)
 {
     this->userId_ = userId;
     return *this;
 }
 
-int32_t UserDefineIPCClient::GetUserId() const
+int32_t UnifiedIPCClient::GetUserId() const
 {
     return this->userId_;
 }
 
-std::unordered_map<std::string, std::string> UserDefineIPCClient::GetHeader() const
+std::unordered_map<std::string, std::string> UnifiedIPCClient::GetHeader() const
 {
     return this->header_;
 }
 
-UserDefineIPCClient &UserDefineIPCClient::SetHeader(const std::unordered_map<std::string, std::string> &header)
+UnifiedIPCClient &UnifiedIPCClient::SetHeader(const std::unordered_map<std::string, std::string> &header)
 {
     this->header_ = header;
     return *this;
 }
 
-int32_t UserDefineIPCClient::InitClient(const int32_t &userId)
+UnifiedIPCClient &UnifiedIPCClient::SetDataShareHelper(std::shared_ptr<DataShare::DataShareHelper> dataShareHelper)
+{
+    this->dataShareHelper_ = dataShareHelper;
+    return *this;
+}
+
+int32_t UnifiedIPCClient::InitClient(const int32_t &userId)
 {
     userId_ = userId;
     bool errConn = UserFileClient::IsValid(userId);
@@ -69,10 +77,17 @@ int32_t UserDefineIPCClient::InitClient(const int32_t &userId)
     UserFileClient::Init(remoteObj, true, userId);
     return E_OK;
 }
-int32_t UserDefineIPCClient::HeaderMarshalling(MessageParcel &data)
+
+int32_t UnifiedIPCClient::HeaderMarshalling(MessageParcel &data)
 {
     bool errConn = !data.WriteInterfaceToken(DESCRIPTOR);
     CHECK_AND_RETURN_RET_LOG(!errConn, E_FAIL, "WriteInterfaceToken failed");
     return E_OK;
+}
+
+int32_t UnifiedIPCClient::UserDefineFunc(MessageParcel &data, MessageParcel &reply, MessageOption &option)
+{
+    CHECK_AND_RETURN_RET_LOG(this->dataShareHelper_ != nullptr, E_ERR, "dataShareHelper_ is nullptr");
+    return this->dataShareHelper_->UserDefineFunc(data, reply, option);
 }
 }  // namespace OHOS::Media::IPC
