462e877a42bc696a89377aaa960785ffab90469b
Tony Coder <407243179@qq.com>
2026-08-27 16:34:09 +0000

    media_file_scan: parse fileId with from_chars instead of stoi
    SetFileManagerScanFlagBySingle used all_of(::isdigit) then std::stoi.
    Empty string is vacuously true so stoi("") throws; overflow throws.
    Parse via from_chars and keep the existing not-digit return.
    
    Signed-off-by: Tony Coder <407243179@qq.com>

---
+++ services/media_file_scan/common/include/parse_file_id.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PARSE_FILE_ID_H
+#define PARSE_FILE_ID_H
+
+#include <charconv>
+#include <cstdint>
+#include <string>
+#include <system_error>
+
+inline bool ParseFileId(const std::string &s, int32_t &out)
+{
+    if (s.empty()) {
+        return false;
+    }
+    int32_t value = 0;
+    const char *first = s.data();
+    const char *last = first + s.size();
+    auto result = std::from_chars(first, last, value);
+    if (result.ec != std::errc() || result.ptr != last) {
+        return false;
+    }
+    out = value;
+    return true;
+}
+
+#endif
